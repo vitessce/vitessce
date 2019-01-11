5dacfa925e0033cbb6b3039d24e13bba96f86462
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-01-11 16:21:37 -0500

    test.sh

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,21 @@
 import Ajv from 'ajv';
 
-var validate = new Ajv().compile({});
-
-it('works', () => {
-  expect(validate({})).toEqual(true);
+it('ajv works', ()=>{
+  var schema = {
+    'properties': {
+      'a_string': {'type': 'string'},
+      'lt_three': { 'type': 'number', 'maximum': 3 }
+    }
+  };
+  var validate = new Ajv().compile(schema);
+  var good_data = {
+    'a_string': 'asdf',
+    'lt_three': 2
+  };
+  var bad_data = {
+    'a_string': 4,
+    'lt_three': 4
+  };
+  expect(validate(good_data)).toEqual(true);
+  expect(validate(bad_data)).toEqual(false);
 });
