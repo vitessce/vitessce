8a744e2ace442629bd5ec5065264b2c8696c7f91
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-01-11 20:06:50 -0500

    AddCells!

--- src/index.test.js
+++ src/index.test.js
@@ -3,7 +3,7 @@ import Ajv from 'ajv';
 it('ajv works', async (done)=>{
   var schema = require('./schema.json');
   var validate = new Ajv().compile(schema);
-  var good_data = require('./api-fixtures/good.json');
+  var good_data = require('./api-fixtures/AddCells.json');
   var bad_data = require('./api-fixtures/bad.json');
   var expect_true = validate(good_data);
   if (!expect_true) { console.warn(validate.errors); }
