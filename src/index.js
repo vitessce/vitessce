63623a5f734d6dde09b81e338948a537f1844575
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-01-14 19:12:20 -0500

    components connected with pubsub

--- src/index.js
+++ src/index.js
@@ -1,173 +1,70 @@
+import PubSub from 'pubsub-js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Spatial(props) {
-  return (
-    <p>spatial!</p>
-  );
+const CELL = 'cell';
+const CELL_ADD = CELL + '.add';
+
+class FileManager extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const value = event.target.value
+    this.setState({value: value});
+    PubSub.publish(CELL_ADD, value);
+  }
+
+  render() {
+    return (
+      <p>
+        filemanager:
+        <input
+          value={this.state.value}
+          onChange={this.handleChange}
+        ></input>
+      </p>
+    );
+  }
 }
 
-function Heatmap(props) {
-  return (
-    <p>heatmap!</p>
-  );
+class Spatial extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+  }
+
+  componentWillMount() {
+    this.token = PubSub.subscribe(CELL, this.subscriber.bind(this));
+  }
+
+  componentWillUnmount() {
+    PubSub.unsubscribe(this.token);
+  }
+
+  subscriber(msg, data) {
+    console.warn('Spatial component received:', msg, data);
+    this.setState({value: data});
+  }
+
+  render() {
+    return (
+      <p>spatial: {this.state.value}</p>
+    );
+  }
 }
 
-function Tsne(props) {
-  return (
-    <p>tsne!</p>
-  );
-}
-
-// function Square(props) {
-//   return (
-//     <button className="square" onClick={props.onClick}>
-//       {props.value}
-//     </button>
-//   );
-// }
-//
-// class Board extends React.Component {
-//   renderSquare(i) {
-//     return (
-//       <Square
-//         value={this.props.squares[i]}
-//         onClick={() => this.props.onClick(i)}
-//       />
-//     );
-//   }
-//
-//   renderRow(i) {
-//     return (
-//       <div className="board-row">
-//         {this.renderSquare(i)}
-//         {this.renderSquare(i + 1)}
-//         {this.renderSquare(i + 2)}
-//       </div>
-//     )
-//   }
-//
-//   render() {
-//     return (
-//       <div>
-//         {this.renderRow(0)}
-//         {this.renderRow(3)}
-//         {this.renderRow(6)}
-//       </div>
-//     );
-//   }
-// }
-//
-// class Game extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       history: [{
-//         squares: Array(9).fill(null)
-//       }],
-//       stepNumber: 0,
-//       xIsNext: true
-//     };
-//   }
-//
-//   handleClick(i) {
-//     const history = this.state.history.slice(0, this.state.stepNumber + 1);
-//     const current = history[history.length - 1];
-//     const squares = current.squares.slice();
-//     if (calculateWinner(squares) || squares[i]) {
-//       return;
-//     }
-//     squares[i] = this.state.xIsNext ? 'X' : 'O';
-//     this.setState({
-//       history: history.concat([{
-//         squares: squares
-//       }]),
-//       stepNumber: history.length,
-//       xIsNext: !this.state.xIsNext,
-//     });
-//   }
-//
-//   jumpTo(step) {
-//     this.setState({
-//       stepNumber: step,
-//       xIsNext: (step % 2) === 0,
-//     });
-//   }
-//
-//   render() {
-//     const history = this.state.history;
-//     const current = history[this.state.stepNumber];
-//     const winner = calculateWinner(current.squares);
-//
-//     const moves = history.map((step, move) => {
-//       const desc = move ?
-//         'Go to move #' + move :
-//         'Go to game start';
-//       return (
-//         <li key={move}>
-//           <button onClick={() => this.jumpTo(move)}>{desc}</button>
-//         </li>
-//       );
-//     });
-//
-//     let status;
-//     if (winner) {
-//       status = 'Winner: ' + winner;
-//     } else {
-//       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-//     }
-//
-//     return (
-//       <div className="game">
-//         <div className="game-board">
-//           <Board
-//             squares={current.squares}
-//             onClick={(i) => this.handleClick(i)}
-//           />
-//         </div>
-//         <div className="game-info">
-//           <div>{status}</div>
-//           <ol>{moves}</ol>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// ========================================
+ReactDOM.render(
+  <FileManager />,
+  document.getElementById('filemanager')
+);
 
 ReactDOM.render(
   <Spatial />,
   document.getElementById('spatial')
 );
-
-ReactDOM.render(
-  <Heatmap />,
-  document.getElementById('heatmap')
-);
-
-ReactDOM.render(
-  <Tsne />,
-  document.getElementById('tsne')
-);
-
-// function calculateWinner(squares) {
-//   const lines = [
-//     [0, 1, 2],
-//     [3, 4, 5],
-//     [6, 7, 8],
-//     [0, 3, 6],
-//     [1, 4, 7],
-//     [2, 5, 8],
-//     [0, 4, 8],
-//     [2, 4, 6],
-//   ];
-//   for (let i = 0; i < lines.length; i++) {
-//     const [a, b, c] = lines[i];
-//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-//       return squares[a];
-//     }
-//   }
-//   return null;
-// }
