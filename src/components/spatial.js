9505924fe04c6ba3231b05fb5f841305cfb992ae
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-01-23 14:53:45 -0500

    Make Spatial into a stateless function

--- src/components/spatial.js
+++ src/components/spatial.js
@@ -35,37 +35,30 @@ export class SpatialSubscriber extends React.Component {
   }
 
   imageAddSubscriber(msg, file) {
-    console.warn('add image');
     this.setState({baseImgUrl: URL.createObjectURL(file)});
   }
 
   render() {
-    console.warn('spatialSub render');
     return (
       <Spatial baseImgUrl={this.state.baseImgUrl}/>
     );
   }
 }
 
-export class Spatial extends React.Component {
-  constructor(props) {
-    super(props);
-    console.warn('Spatial', props.baseImgUrl);
-    //this.state = {baseImgUrl: undefined};
-  }
+function renderLayers(props) {
+  const {
+    scatterplot_data = [[-74, 57, 1], [-65, 41, 2], [-73, 32, 1], [-74, 40, 2]],
+    radius = 5,
+    maleColor = MALE_COLOR,
+    femaleColor = FEMALE_COLOR,
+    baseImgUrl = undefined
+  } = props;
 
-  _renderLayers() {
-    const {
-      scatterplot_data = [[-74, 57, 1], [-65, 41, 2], [-73, 32, 1], [-74, 40, 2]],
-      radius = 5,
-      maleColor = MALE_COLOR,
-      femaleColor = FEMALE_COLOR,
-      baseImgUrl = undefined
-    } = this.props;
+  const polygon_data = [ { contour: [[-20, -20], [-65, -10], [-80, 0], [-70, 40]] } ];
 
-    const polygon_data = [ { contour: [[-20, -20], [-65, -10], [-80, 0], [-70, 40]] } ];
-
-    return [
+  var layers = [];
+  if (baseImgUrl) {
+    layers.push(
       new BitmapLayer({
         id: 'bitmap-layer',
         coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
@@ -77,57 +70,61 @@ export class Spatial extends React.Component {
         }],
         tintColor: [300, 300, 300], // https://github.com/uber/deck.gl/issues/2585
         modelMatrix: new Matrix4().scale([400,200,200])
-      }),
-      new PolygonLayer({
-        id: 'polygon-layer',
-        coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
-        data: polygon_data,
-        pickable: true,
-        stroked: true,
-        filled: true,
-        wireframe: true,
-        lineWidthMinPixels: 1,
-        getPolygon: d => d.contour,
-        getElevation: d => 0,
-        getFillColor: d => [255, 0, 0],
-        getLineColor: [80, 80, 80],
-        getLineWidth: 1,
-        onHover: ({object, x, y}) => {
-          //const tooltip = `${object.zipcode}\nPopulation: ${object.population}`;
-          /* Update tooltip
-             http://deck.gl/#/documentation/developer-guide/adding-interactivity?section=example-display-a-tooltip-for-hovered-object
-          */
-        }
-      }),
-      new ScatterplotLayer({
-        id: 'scatter-plot',
-        coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
-        data: scatterplot_data,
-        radiusScale: radius,
-        radiusMinPixels: 0.25,
-        getPosition: d => [d[0], d[1], 0],
-        getColor: d => (d[2] === 1 ? maleColor : femaleColor),
-        getRadius: 1,
-        updateTriggers: {
-          getColor: [maleColor, femaleColor]
-        }
       })
-    ];
-  }
-
-  render() {
-    console.warn('spatial render');
-    const {viewState, controller = true} = this.props;
-
-    return (
-      <DeckGL
-        views={[new OrthographicView()]}
-        layers={this._renderLayers()}
-        initialViewState={INITIAL_VIEW_STATE}
-        viewState={viewState}
-        controller={controller}
-      >
-      </DeckGL>
     );
   }
+  layers.push(
+    new PolygonLayer({
+      id: 'polygon-layer',
+      coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
+      data: polygon_data,
+      pickable: true,
+      stroked: true,
+      filled: true,
+      wireframe: true,
+      lineWidthMinPixels: 1,
+      getPolygon: d => d.contour,
+      getElevation: d => 0,
+      getFillColor: d => [255, 0, 0],
+      getLineColor: [80, 80, 80],
+      getLineWidth: 1,
+      onHover: ({object, x, y}) => {
+        //const tooltip = `${object.zipcode}\nPopulation: ${object.population}`;
+        /* Update tooltip
+           http://deck.gl/#/documentation/developer-guide/adding-interactivity?section=example-display-a-tooltip-for-hovered-object
+        */
+      }
+    })
+  );
+  layers.push(
+    new ScatterplotLayer({
+      id: 'scatter-plot',
+      coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
+      data: scatterplot_data,
+      radiusScale: radius,
+      radiusMinPixels: 0.25,
+      getPosition: d => [d[0], d[1], 0],
+      getColor: d => (d[2] === 1 ? maleColor : femaleColor),
+      getRadius: 1,
+      updateTriggers: {
+        getColor: [maleColor, femaleColor]
+      }
+    })
+  );
+  return layers;
+}
+
+export function Spatial(props) {
+  const {viewState, controller = true} = props;
+
+  return (
+    <DeckGL
+      views={[new OrthographicView()]}
+      layers={renderLayers(props)}
+      initialViewState={INITIAL_VIEW_STATE}
+      viewState={viewState}
+      controller={controller}
+    >
+    </DeckGL>
+  );
 }
