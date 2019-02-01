1fdffac4a24fa35fb3339ca1a174c42f6e926398
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-02-01 15:48:57 -0500

    Builds

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import React, {Component} from 'react'
-
-// TODO: This came with nwb... how should I use it for this project?
-
-export default class Vitessce extends Component {
-  render() {
-    return <div>
-      <h2>TODO: Vitessce Stub</h2>
-    </div>
-  }
-}
+export { HeatmapSubscriber } from './components/heatmap/'
+export { SpatialSubscriber } from './components/spatial/'
+export { TsneSubscriber } from './components/tsne/'
+export { FileManagerPublisher } from './components/fileManager'
+export { MessagesSubscriber } from './components/messages'
