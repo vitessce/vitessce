8fa2ae4b3f69fa973467266d6a69f0f2a5d70848
mccalluc <mccalluc@users.noreply.github.com>
2019-02-04 12:39:38 -0500

    Mccalluc/demo cleanup (#17)
    * Checkpoint: Simplify demo. localhost works
    
    * Local version works
    
    * Version bump: demo works locally, hopefully on ghpages, too

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,21 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import renderApp from '../../src/app';
 
-import './css/file-drop.css';
-import './css/index.css';
-
-import { FileManagerPublisher } from '../../src/components/fileManager';
-import { MessagesSubscriber } from '../../src/components/messages';
-import { TsneSubscriber } from '../../src/components/tsne/';
-import { HeatmapSubscriber } from '../../src/components/heatmap/';
-import { SpatialSubscriber } from '../../src/components/spatial/';
-
-function render(react, id) {
-  ReactDOM.render(react, document.getElementById(id));
-}
-
-render(<FileManagerPublisher />, 'filemanager');
-render(<MessagesSubscriber />, 'messages');
-render(<TsneSubscriber />, 'tsne');
-render(<HeatmapSubscriber />, 'heatmap');
-render(<SpatialSubscriber />, 'spatial');
+renderApp('app');
