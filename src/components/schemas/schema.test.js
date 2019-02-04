31359e51f8835e610110e4e6d4971f6b9335db3e
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-02-04 17:07:20 -0500

    And give an example of validation failing

--- src/components/schemas/schema.test.js
+++ src/components/schemas/schema.test.js
@@ -4,13 +4,33 @@ import expect from 'expect'
 describe('cells.schema.json', ()=>{
   var schema = require('./cells.schema.json');
   var validate = new Ajv().compile(schema);
-  var fixtures = ['good.cells.json'];
-  fixtures.forEach((f) => {
+  var goodFixtures = ['good.cells.json'];
+  goodFixtures.forEach((f) => {
     it(`handles ${f}`, ()=>{
       var data = require(`./fixtures/${f}`);
       var valid = validate(data);
       if (!valid) { console.warn(validate.errors); }
       expect(valid).toEqual(true);
+    })
+  });
+
+  var badFixtures = [['bad.cells.tsne.json', 'bad.cells.tsne.message.json']];
+  badFixtures.forEach(([input, output]) => {
+    it(`handles ${input}`, ()=>{
+      var data = require(`./fixtures/${input}`);
+      var message = require(`./fixtures/${output}`);
+      var valid = validate(data);
+      expect(valid).toEqual(false);
+      expect(validate.errors).toEqual(message);
+
+      // {
+      //   keyword: 'type',
+      //   dataPath: '['778'].tsne[1]',
+      //   schemaPath: '#/definitions/coord/items/type',
+      //   params: Object{type: ...},
+      //   message: 'should be number'
+      // }]
+
     })
   })
 });
