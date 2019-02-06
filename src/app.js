06460552ad31549d32b87ff23152ab6eef490e94
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-02-06 14:45:26 -0500

    even padding everywhere

--- src/app.js
+++ src/app.js
@@ -18,11 +18,11 @@ export default function renderApp(id) {
   const card = 'card card-body bg-light my-2';
   const [lLg, lMd, lSm] = [4, 5, 6];
   const [rLg, rMd, rSm] = [12 - lLg, 12 - lMd, 12 - lSm];
-  const col = 'd-flex flex-column pr-2';
+  const col = 'd-flex flex-column px-2';
   const left = `${col} col-lg-${lLg} col-md-${lMd} col-sm-${lSm}`;
   const right = `${col} col-lg-${rLg} col-md-${rMd} col-sm-${rSm}`;
   document.getElementById(id).innerHTML = `
-    <div class="container-fluid d-flex h-100 px-0">
+    <div class="container-fluid d-flex h-100 px-2 py-2">
       <div class="${left}">
         <div id="filemanager" class="${card}"></div>
         <div id="status" class="${card}"></div>
