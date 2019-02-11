6f9907d9c778cdeb2588c6f7f339de5bbbd09f0b
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-02-11 12:39:01 -0500

    Toggle button state

--- src/components/toolpicker.js
+++ src/components/toolpicker.js
@@ -1,13 +1,77 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export function ToolPicker(props) {
+function IconButton(props) {
+  const {src, alt, onClick, isActive=false} = props;
+  const inactive = 'btn btn-outline-secondary mr-2 icon'
+  const active = inactive +  ' active';
   return (
-    <div>
-      <button type='button' className='btn btn-outline-secondary mr-2 icon'>
-        <img src="inkscape/tool_pointer.svg" alt="pointer tool"/>
-      </button>
-      <button type='button' className='btn btn-outline-secondary mr-2 icon'>
-        <img src="inkscape/snap_bounding_box_center.svg" alt="region selection tool"/>
-      </button>
-    </div>);
+    <button
+      className={isActive ? active : inactive}
+      onClick={onClick}
+    >
+      <img src={src} alt={alt}/>
+    </button>
+  );
+}
+
+IconButton.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  onClick: PropTypes.func,
+  isActive: PropTypes.bool
+}
+
+const POINT = 'point';
+const RECT = 'rect';
+
+export class ToolPicker extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {activeTool: POINT};
+    this.activatePointMode = this.activatePointMode.bind(this);
+    this.activateRectangleMode = this.activateRectangleMode.bind(this);
+  }
+
+  // componentWillMount() {
+  //   this.warnToken = PubSub.subscribe(STATUS_WARN, this.warnSubscriber.bind(this));
+  // }
+
+  // componentWillUnmount() {
+  //   PubSub.unsubscribe(this.warnToken);
+  // }
+
+  // warnSubscriber(msg, data) {
+  //   this.setState({warn: true, message: data});
+  // }
+
+  activatePointMode() {
+    this.setState({activeTool: POINT});
+    console.log('point!');
+  }
+
+  activateRectangleMode() {
+    this.setState({activeTool: RECT});
+    console.log('rect!');
+  }
+
+  render() {
+    return (
+      <div>
+        <IconButton
+          src="inkscape/tool_pointer.svg"
+          alt="pointer tool"
+          onClick={this.activatePointMode}
+          isActive={this.state.activeTool === POINT}
+        />
+        <IconButton
+          src="inkscape/snap_bounding_box_center.svg"
+          alt="select rectangle"
+          onClick={this.activateRectangleMode}
+          isActive={this.state.activeTool === RECT}
+        />
+      </div>
+    );
+  }
 }
