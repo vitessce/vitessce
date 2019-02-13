4fba7e36b4d5e818a5514354ffc244a131249f6b
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-02-13 14:18:35 -0500

    Checkpoint: Works!

--- src/components/spatial/Spatial.js
+++ src/components/spatial/Spatial.js
@@ -51,7 +51,7 @@ const INITIAL_VIEW_STATE = {
 export default class Spatial extends React.Component {
   constructor(props) {
     super(props);
-    this.onDrag = this.onDrag.bind(this);
+    // this.onDrag = this.onDrag.bind(this);
     this.onDragStart = this.onDragStart.bind(this);
     this.onDragEnd = this.onDragEnd.bind(this);
   }
@@ -63,28 +63,36 @@ export default class Spatial extends React.Component {
     }
   }
   onDragEnd(event) {
-    const dragEndCoordinate = event.coordinate;
-    const xMin = Math.min(this.dragStartCoordinate[0], dragEndCoordinate[0]);
-    const yMin = Math.min(this.dragStartCoordinate[1], dragEndCoordinate[1]);
-    const xMax = Math.max(this.dragStartCoordinate[0], dragEndCoordinate[0]);
-    const yMax = Math.max(this.dragStartCoordinate[1], dragEndCoordinate[1]);
-    // The built-in pickObjects is limited in the size of the region that can be selected.
-    // https://github.com/uber/deck.gl/issues/2658#issuecomment-463293063
+    if (this.props.isRectangleSelection) {
+      const dragEndCoordinate = event.coordinate;
+      const xMin = Math.min(this.dragStartCoordinate[0], dragEndCoordinate[0]);
+      const yMin = Math.min(this.dragStartCoordinate[1], dragEndCoordinate[1]);
+      const xMax = Math.max(this.dragStartCoordinate[0], dragEndCoordinate[0]);
+      const yMax = Math.max(this.dragStartCoordinate[1], dragEndCoordinate[1]);
+      // The built-in pickObjects is limited in the size of the region that can be selected.
+      // https://github.com/uber/deck.gl/issues/2658#issuecomment-463293063
 
-    // TODO: Implement quadtree? But it's probably fast enough.
-    // TODO: I don't trust xy from raw data... but clean it up, and then use it, instead.
-    const selectedCellIds = Object.entries(this.props.cells).filter(
-      ([id, cell]) =>
-        cell.poly[0][0] > xMin
-        && cell.poly[0][0] < xMax
-        && cell.poly[0][1] > yMin
-        && cell.poly[0][1] < yMax
-    ).map(([id, cell]) => id);
-    this.props.updateCellsSelection(selectedCellIds);
+      // TODO: Implement quadtree? But it's probably fast enough.
+      // TODO: I don't trust xy from raw data... but clean it up, and then use it, instead.
+      const selectedCellIds = Object.entries(this.props.cells).filter(
+        ([id, cell]) =>
+          cell.poly[0][0] > xMin
+          && cell.poly[0][0] < xMax
+          && cell.poly[0][1] > yMin
+          && cell.poly[0][1] < yMax
+      ).map(([id, cell]) => id);
+      var selectedCellIdsSet = {};
+      for (const id of selectedCellIds) {
+        selectedCellIdsSet[id] = true;
+      }
+      this.props.updateCellsSelection(selectedCellIdsSet);
+    }
   }
-  onDrag(event) {
-    // TODO: Draw marquee?
-  }
+  // onDrag(event) {
+  //   if (this.props.isRectangleSelection) {
+  //     //  TODO: Draw marquee?
+  //   }
+  // }
 
   renderLayers() {
     const {
