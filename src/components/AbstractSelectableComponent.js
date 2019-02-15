f3c7528edec672fe37db5aafe84f4017eaf3ac14
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-02-15 11:42:40 -0500

    Log on drag

--- src/components/AbstractSelectableComponent.js
+++ src/components/AbstractSelectableComponent.js
@@ -8,7 +8,19 @@ export default class AbstractSelectableComponent extends React.Component {
     super(props);
     // this.onDrag = this.onDrag.bind(this);
     this.onDragStart = this.onDragStart.bind(this);
+    this.onDrag = this.onDrag.bind(this);
     this.onDragEnd = this.onDragEnd.bind(this);
+  }
+
+  getDragRectangle(event) {
+    const start = this.dragStartCoordinate
+    const end = event.coordinate;
+    return {
+      xMin: Math.min(start[0], end[0]),
+      yMin: Math.min(start[1], end[1]),
+      xMax: Math.max(start[0], end[0]),
+      yMax: Math.max(start[1], end[1])
+    }
   }
 
   onDragStart(event) {
@@ -17,13 +29,16 @@ export default class AbstractSelectableComponent extends React.Component {
     }
   }
 
+  onDrag(event) {
+    if (this.props.isRectangleSelection) {
+      const {xMin, yMin, xMax, yMax} = this.getDragRectangle(event);
+      console.log({xMin, yMin, xMax, yMax});
+    }
+  }
+
   onDragEnd(event) {
     if (this.props.isRectangleSelection) {
-      const dragEndCoordinate = event.coordinate;
-      const xMin = Math.min(this.dragStartCoordinate[0], dragEndCoordinate[0]);
-      const yMin = Math.min(this.dragStartCoordinate[1], dragEndCoordinate[1]);
-      const xMax = Math.max(this.dragStartCoordinate[0], dragEndCoordinate[0]);
-      const yMax = Math.max(this.dragStartCoordinate[1], dragEndCoordinate[1]);
+      const {xMin, yMin, xMax, yMax} = this.getDragRectangle(event);
       // The built-in pickObjects is limited in the size of the region that can be selected.
       // https://github.com/uber/deck.gl/issues/2658#issuecomment-463293063
 
