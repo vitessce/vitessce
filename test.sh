40e130041690a10959a72f0828805064f64c5404
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-02-26 15:34:18 -0500

    build and checkin jsdocs, and link from github pages

--- test.sh
+++ test.sh
@@ -10,8 +10,21 @@ die() { set +v; echo "$*" 1>&2 ; sleep 1; exit 1; }
 export CI=true
 
 start lint
-./node_modules/eslint/bin/eslint.js src
+node_modules/eslint/bin/eslint.js src
 end lint
+
+start jsdoc
+# TODO: Make this part of the real build process,
+# and host jsdocs at unpkg, or something like that.
+NEW_DOCS=/tmp/jsdocs
+OLD_DOCS=docs/jsdocs
+node_modules/documentation/bin/documentation.js \
+  build src/index.js \
+  --format html --sort-order alpha --output $NEW_DOCS
+rm -rf $NEW_DOCS/assets/fonts
+diff -r $OLD_DOCS $NEW_DOCS > /dev/null \
+  || die "JSDocs not up-to-date: 'rm -rf $OLD_DOCS && cp -a $NEW_DOCS $OLD_DOCS'"
+end jsdoc
 
 start test
 npm test
