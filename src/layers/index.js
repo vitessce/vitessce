8d9ba9e15327b17f03416c38e888bde98fc67ad8
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-02-27 10:01:06 -0500

    MatrixLayer is todo

--- src/layers/index.js
+++ src/layers/index.js
@@ -1,3 +1,3 @@
 export SelectablePolygonLayer from './SelectablePolygonLayer';
 export SelectableScatterplotLayer from './SelectableScatterplotLayer';
-export MatrixLayer from './MatrixLayer';
+// TODO: export MatrixLayer from './MatrixLayer';
