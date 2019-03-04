f7181e6c759e4ff6cd9a81f1e4bd2b7275c64202
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-03-04 13:46:56 -0500

    More tests

--- src/components/spatial/spatial.test.js
+++ src/components/spatial/spatial.test.js
@@ -2,8 +2,10 @@
 import expect from 'expect';
 import { square } from './Spatial';
 
-describe('square()', () => {
-  it('gives the right coordinates', () => {
-    expect(square(0, 0)).toEqual([[0, 5], [5, 0], [0, -5], [-5, 0]]);
+describe('Spatial', () => {
+  describe('square()', () => {
+    it('gives the right coordinates', () => {
+      expect(square(0, 0)).toEqual([[0, 5], [5, 0], [0, -5], [-5, 0]]);
+    });
   });
 });
--- src/schemas/schema.test.js
+++ src/schemas/schema.test.js
@@ -6,52 +6,54 @@ import expect from 'expect';
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
 
-describe('cells.schema.json', () => {
-  const schema = require('./cells.schema.json');
-  const validate = new Ajv().compile(schema);
-  const goodFixtures = ['good.cells.json'];
-  goodFixtures.forEach((f) => {
-    it(`handles ${f}`, () => {
-      const data = require(`./fixtures/${f}`);
-      const valid = validate(data);
-      if (!valid) { console.warn(validate.errors); }
-      expect(valid).toEqual(true);
+describe('schemas', () => {
+  describe('cells.schema.json', () => {
+    const schema = require('./cells.schema.json');
+    const validate = new Ajv().compile(schema);
+    const goodFixtures = ['good.cells.json'];
+    goodFixtures.forEach((f) => {
+      it(`handles ${f}`, () => {
+        const data = require(`./fixtures/${f}`);
+        const valid = validate(data);
+        if (!valid) { console.warn(validate.errors); }
+        expect(valid).toEqual(true);
+      });
+    });
+
+    const badFixtures = [['bad.cells.tsne.json', 'bad.cells.tsne.message.json']];
+    badFixtures.forEach(([input, output]) => {
+      it(`handles ${input}`, () => {
+        const data = require(`./fixtures/${input}`);
+        const message = require(`./fixtures/${output}`);
+        const valid = validate(data);
+        expect(valid).toEqual(false);
+        expect(validate.errors).toEqual(message);
+      });
     });
   });
 
-  const badFixtures = [['bad.cells.tsne.json', 'bad.cells.tsne.message.json']];
-  badFixtures.forEach(([input, output]) => {
-    it(`handles ${input}`, () => {
-      const data = require(`./fixtures/${input}`);
-      const message = require(`./fixtures/${output}`);
-      const valid = validate(data);
-      expect(valid).toEqual(false);
-      expect(validate.errors).toEqual(message);
+  describe('molecules.schema.json', () => {
+    const schema = require('./molecules.schema.json');
+    const validate = new Ajv().compile(schema);
+    const goodFixtures = ['good.molecules.json'];
+    goodFixtures.forEach((f) => {
+      it(`handles ${f}`, () => {
+        const data = require(`./fixtures/${f}`);
+        const valid = validate(data);
+        if (!valid) { console.warn(validate.errors); }
+        expect(valid).toEqual(true);
+      });
     });
-  });
-});
 
-describe('molecules.schema.json', () => {
-  const schema = require('./molecules.schema.json');
-  const validate = new Ajv().compile(schema);
-  const goodFixtures = ['good.molecules.json'];
-  goodFixtures.forEach((f) => {
-    it(`handles ${f}`, () => {
-      const data = require(`./fixtures/${f}`);
-      const valid = validate(data);
-      if (!valid) { console.warn(validate.errors); }
-      expect(valid).toEqual(true);
-    });
-  });
-
-  const badFixtures = [['bad.molecules.json', 'bad.molecules.message.json']];
-  badFixtures.forEach(([input, output]) => {
-    it(`handles ${input}`, () => {
-      const data = require(`./fixtures/${input}`);
-      const message = require(`./fixtures/${output}`);
-      const valid = validate(data);
-      expect(valid).toEqual(false);
-      expect(validate.errors).toEqual(message);
+    const badFixtures = [['bad.molecules.json', 'bad.molecules.message.json']];
+    badFixtures.forEach(([input, output]) => {
+      it(`handles ${input}`, () => {
+        const data = require(`./fixtures/${input}`);
+        const message = require(`./fixtures/${output}`);
+        const valid = validate(data);
+        expect(valid).toEqual(false);
+        expect(validate.errors).toEqual(message);
+      });
     });
   });
 });
