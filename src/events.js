ee7a75338385b7d652a91de6c7aa1a9d9716e361
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-03-13 16:49:27 -0400

    more events

--- src/events.js
+++ src/events.js
@@ -2,18 +2,24 @@ const ADD = '.add';
 
 export const CLEAR_PLEASE_WAIT = 'clear_please_wait';
 
+export const CELLS = 'cells';
+export const CELLS_ADD = CELLS + ADD;
+export const CELLS_SELECTION = `${CELLS}.selection`;
+
+export const FACTORS = 'factors';
+export const FACTORS_ADD = FACTORS + ADD;
+
 export const GENES = 'genes';
 export const GENES_ADD = GENES + ADD;
 
 export const IMAGES = 'images';
 export const IMAGES_ADD = IMAGES + ADD;
 
-export const CELLS = 'cells';
-export const CELLS_ADD = CELLS + ADD;
-export const CELLS_SELECTION = `${CELLS}.selection`;
-
 export const MOLECULES = 'molecules';
 export const MOLECULES_ADD = MOLECULES + ADD;
+
+export const NEIGHBORHOODS = 'neighborhoods';
+export const NEIGHBORHOODS_ADD = NEIGHBORHOODS + ADD;
 
 export const STATUS = 'status';
 export const STATUS_WARN = `${STATUS}.warn`;
