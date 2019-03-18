f2cedebdbee25986bae19aa99a6b60153e198fb5
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-03-18 18:21:18 -0400

    viridis heatmap

--- src/components/heatmap/HeatmapCanvas.js
+++ src/components/heatmap/HeatmapCanvas.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { interpolateViridis } from 'd3-scale-chromatic';
+
+import { rgb } from '../utils';
 
 export default class HeatmapCanvas extends React.Component {
   shouldComponentUpdate(nextProps) {
@@ -7,10 +10,27 @@ export default class HeatmapCanvas extends React.Component {
 
   componentDidUpdate() {
     const ctx = this.canvasRef.getContext('2d');
-    ctx.fillStyle = 'green';
-    ctx.fillRect(0, 0, 4839, 33);
-    ctx.fillStyle = 'red';
-    ctx.fillRect(100, 10, 4639, 13);
+    ctx.imageSmoothingEnabled = false;
+
+    const { clusters } = this.props;
+    const width = clusters.cols.length;
+    const height = clusters.rows.length;
+
+    const imageData = ctx.createImageData(width, height);
+    clusters.matrix.forEach((row, y) => {
+      row.forEach((value, x) => {
+        const offset = (y * width + x) * 4;
+        // Math.sqrt is arbitrary, but I wanted to improve the contrast.
+        const rgbTriple = rgb(interpolateViridis(Math.sqrt(value)));
+        /* eslint-disable prefer-destructuring */
+        imageData.data[offset + 0] = rgbTriple[0];
+        imageData.data[offset + 1] = rgbTriple[1];
+        imageData.data[offset + 2] = rgbTriple[2];
+        /* eslint-enable */
+        imageData.data[offset + 3] = 255;
+      });
+    });
+    ctx.putImageData(imageData, 0, 0);
   }
 
   render() {
@@ -20,6 +40,7 @@ export default class HeatmapCanvas extends React.Component {
     }
     return (
       <canvas
+        style={{ height: '100%' }}
         ref={(c) => { this.canvasRef = c; }}
         width={clusters.cols.length}
         height={clusters.rows.length}
