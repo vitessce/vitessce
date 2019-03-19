e2ca2d13e891439f5f63bd0942ce208565e744c0
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-03-18 23:00:42 -0400

    Cell selection markers on heatmap work

--- src/components/heatmap/HeatmapCellSelectionCanvas.js
+++ src/components/heatmap/HeatmapCellSelectionCanvas.js
@@ -8,20 +8,21 @@ export default class HeatmapCellSelectionCanvas extends React.Component {
   componentDidUpdate() {
     const ctx = this.canvasRef.getContext('2d');
 
-    const { clusters } = this.props;
+    const { clusters, selectedCellIds } = this.props;
     const width = clusters.cols.length;
     const height = 1;
 
     const imageData = ctx.createImageData(width, height);
     clusters.cols.forEach((cellId, x) => {
       const offset = x * 4;
+      const selected = selectedCellIds[cellId];
 
       /* eslint-disable prefer-destructuring */
       imageData.data[offset + 0] = 128;
       imageData.data[offset + 1] = 128;
       imageData.data[offset + 2] = 128;
       /* eslint-enable */
-      imageData.data[offset + 3] = 255;
+      imageData.data[offset + 3] = selected ? 255 : 0;
     });
     ctx.putImageData(imageData, 0, 0);
   }
