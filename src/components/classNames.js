d8bd45efe164123584ccefe29990cd6e06906887
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-03-25 13:41:32 -0400

    Darker grey background

--- src/components/classNames.js
+++ src/components/classNames.js
@@ -1,5 +1,5 @@
 const CARD = 'card card-body my-2';
 export const BLACK_CARD = `${CARD} bg-black`;
 export const LIGHT_CARD = `${CARD} bg-light`;
-export const TITLE_CARD = 'card bg-light px-3';
+export const TITLE_CARD = 'title';
 export const SCROLL_CARD = `${LIGHT_CARD} scroll`;
