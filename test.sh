f82900d45d587babc672ced06e35cb246bec212e
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-04-01 16:33:39 -0400

    Uncomment

--- test.sh
+++ test.sh
@@ -11,23 +11,23 @@ export CI=true
 
 PATH=$PATH:`npm bin`
 
-# start lint
-# eslint src
-# end lint
-#
-# start jsdoc
-# NEW_DOCS=/tmp/jsdocs
-# OLD_DOCS=docs/jsdocs
-# documentation build src/index.js \
-#   --format html --sort-order alpha --output $NEW_DOCS
-# rm -rf $NEW_DOCS/assets/fonts
-# diff -r $OLD_DOCS $NEW_DOCS > /dev/null \
-#   || die "JSDocs not up-to-date: 'rm -rf $OLD_DOCS && cp -a $NEW_DOCS $OLD_DOCS'"
-# end jsdoc
-#
-# start test
-# npm test
-# end test
+start lint
+eslint src
+end lint
+
+start jsdoc
+NEW_DOCS=/tmp/jsdocs
+OLD_DOCS=docs/jsdocs
+documentation build src/index.js \
+  --format html --sort-order alpha --output $NEW_DOCS
+rm -rf $NEW_DOCS/assets/fonts
+diff -r $OLD_DOCS $NEW_DOCS > /dev/null \
+  || die "JSDocs not up-to-date: 'rm -rf $OLD_DOCS && cp -a $NEW_DOCS $OLD_DOCS'"
+end jsdoc
+
+start test
+npm test
+end test
 
 start cypress
 npm start & wait-on http://localhost:3000/
@@ -35,6 +35,6 @@ cypress run
 echo 'NOTE: Server is still running.'
 end cypress
 
-# start build
-# npm run build
-# end build
+start build
+npm run build
+end build
