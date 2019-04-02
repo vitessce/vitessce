4e7d514f2c9954102c2212e98c3b90f3f67cf76a
Chuck McCallum <mccalluc@users.noreply.github.com>
2019-04-02 13:13:26 -0400

    Mccalluc/cypress.io (#155)
    * Checkpoint: cypress and cypress-eslint are installed; demo tests run locally
    
    * Actual cypress test
    
    * Uncomment
    
    * caching
    
    * More comments on caches
    
    * Two passing cypress tests
    
    * Mock some responses from AWS
    
    * Remove fixtures file to avoid confusion
    
    * All fixtures except for cells work
    
    * More cypress assertions
    
    * Fix the problem loading the cells fixture

---
+++ cypress/integration/vitessce.spec.js
@@ -0,0 +1,72 @@
+Cypress.on('window:before:load', (win) => {
+  // Forces fallback to XHR, so cypress can mock response.
+  //
+  // Using this work-around:
+  // https://github.com/cypress-io/cypress-example-recipes/tree/master/examples/stubbing-spying__window-fetch
+  //
+  // until this feature is implemented:
+  // https://github.com/cypress-io/cypress/issues/95
+
+  delete win.fetch; // eslint-disable-line no-param-reassign
+});
+
+describe('Vitessce', () => {
+  beforeEach(() => {
+    const base = 'https://s3.amazonaws.com/vitessce-data/0.0.12/linnarsson-2018';
+    cy.server();
+    ['cells', 'molecules', 'images', 'clusters', 'genes', 'factors', 'neighborhoods'].forEach(
+      (type) => {
+        cy.route(
+          `${base}/linnarsson.${type}.json`,
+          `fixture:../../src/schemas/fixtures/${type}.good.json`,
+        );
+      },
+    );
+  });
+
+  it('has title, blurb, and link to "Please wait"', () => {
+    cy.visit('/');
+    cy.contains('Vitessce');
+    cy.contains('This is a demo');
+    cy.contains('Linnarsson - osmFISH')
+      .click();
+    cy.contains('Please wait');
+    cy.get('.modal-body').should('be.visible');
+  });
+
+  it('loads details', () => {
+    cy.visit('/?dataset=linnarsson-2018');
+    cy.contains('Please wait');
+    cy.get('.modal-body').should('be.visible');
+
+    // Data Set:
+    cy.contains('Linnarsson - osmFISH');
+    cy.contains('Spatial organization of the');
+
+    // Status:
+    // Contents will depend on load order, so not sure how to make a good test
+    // that won't have race condition problems.
+
+    // Spatial:
+    cy.contains('1 cells');
+    cy.contains('1 molecules');
+    cy.contains('3 locations');
+
+    // Heatmap:
+    cy.contains('3 cells × 3 genes');
+    cy.contains('with 0 cells selected');
+
+    // Factors:
+    cy.contains('2 factors');
+    cy.contains('subcluster');
+
+    // Expression Levels:
+    cy.contains('2 genes');
+    cy.contains('Slc32a1');
+
+    // t-SNE:
+    // Has same number of cells as Spatial.
+
+    cy.get('.modal-body').should('not.be.visible');
+  });
+});
--- test.sh
+++ test.sh
@@ -9,15 +9,16 @@ die() { set +v; echo "$*" 1>&2 ; sleep 1; exit 1; }
 
 export CI=true
 
+PATH=$PATH:`npm bin`
+
 start lint
-node_modules/eslint/bin/eslint.js src
+eslint src
 end lint
 
 start jsdoc
 NEW_DOCS=/tmp/jsdocs
 OLD_DOCS=docs/jsdocs
-node_modules/documentation/bin/documentation.js \
-  build src/index.js \
+documentation build src/index.js \
   --format html --sort-order alpha --output $NEW_DOCS
 rm -rf $NEW_DOCS/assets/fonts
 diff -r $OLD_DOCS $NEW_DOCS > /dev/null \
@@ -27,6 +28,12 @@ end jsdoc
 start test
 npm test
 end test
+
+start cypress
+npm start & wait-on http://localhost:3000/
+cypress run
+echo 'NOTE: Server is still running.'
+end cypress
 
 start build
 npm run build
