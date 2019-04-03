3d249d8de81c58602f05dba3bc40cc3a8fd6fc4b
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-04-03 13:07:22 -0400

    Continue earlier rename

--- src/app.test.js
+++ src/app.test.js
@@ -2,20 +2,20 @@ import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
 import expect from 'expect';
-import { DatasetPicker } from './app';
+import { DatasetList } from './app';
 
 configure({ adapter: new Adapter() });
 
 describe('app.js', () => {
   describe('<DatasetPicker />', () => {
     it('is empty if datasets is empty', () => {
-      const wrapper = shallow(<DatasetPicker datasets={{}} />);
+      const wrapper = shallow(<DatasetList datasets={{}} />);
       expect(wrapper.find('a').length).toEqual(0);
     });
 
     it('has one if datasets has one', () => {
       const datasets = { fake: { name: 'NAME', description: 'DESCRIPTION' } };
-      const wrapper = shallow(<DatasetPicker datasets={datasets} />);
+      const wrapper = shallow(<DatasetList datasets={datasets} />);
       expect(wrapper.find('a').length).toEqual(1);
     });
   });
