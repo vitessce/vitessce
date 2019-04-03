bf013b0b18397ef2247fdd9093e4843e4ca4565d
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-04-03 13:02:28 -0400

    fewer warnings

--- src/components/layermanager/LayerManagerPublisher.js
+++ src/components/layermanager/LayerManagerPublisher.js
@@ -88,7 +88,7 @@ export default class LayerManagerPublisher extends React.Component {
       const waitingOn = Object.entries(prevState.pleaseWaits)
         .filter(entry => entry[1])
         .map(entry => entry[0]);
-      console.warn('cleared:', layerName, '-> still waiting on:', waitingOn);
+      if (waitingOn.length) console.warn('cleared:', layerName, '-> still waiting on:', waitingOn);
       return prevState;
     });
   }
