2ab35985e66b33d255bf996da53aa710d09b5b92
Chuck McCallum <mccalluc@users.noreply.github.com>
2019-04-09 11:21:09 -0400

    Mccalluc/responsive tweaks (#172)
    * Public flag; make responsive even more responsive.
    
    * Fix #171: status overflow hide

--- src/app/api.js
+++ src/app/api.js
@@ -28,13 +28,21 @@ const linnarssonBase = {
 };
 
 const configs = {
-  'linnarsson-2018-responsive': {
+  'linnarsson-2018': {
     ...linnarssonBase,
     name: 'Linnarsson (responsive layout)',
+    public: true,
     responsiveLayout: {
       columns: {
-        1000: [0, 3, 9, 12],
-        800: [0, 4, 8, 12],
+        // First two columns are equal,
+        // third column is constant;
+        // Grid cell width stays roughly constant,
+        // but more columns are available in a wider window.
+        1400: [0, 6, 12, 14],
+        1200: [0, 5, 10, 12],
+        1000: [0, 4, 8, 10],
+        800: [0, 3, 6, 8],
+        600: [0, 2, 4, 8],
       },
       layout: {
         description: { x: 0, y: 0 },
@@ -65,11 +73,15 @@ const configs = {
 };
 
 export function listConfig() {
-  return Object.entries(configs).map(([id, config]) => ({
-    id,
-    name: config.name,
-    description: config.description,
-  }));
+  return Object.entries(configs).filter(
+    entry => entry[1].public,
+  ).map(
+    ([id, config]) => ({
+      id,
+      name: config.name,
+      description: config.description,
+    }),
+  );
 }
 
 export function getConfig(id) {
