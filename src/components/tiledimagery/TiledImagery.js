eb4afae13699f7adc5cf96354cf629c5faf89900
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-04-25 17:20:54 -0400

    a lot of pruning

--- src/components/tiledimagery/TiledImagery.js
+++ src/components/tiledimagery/TiledImagery.js
@@ -6,21 +6,11 @@ import OpenSeadragon from 'openseadragon';
 import { BLACK_CARD } from '../classNames';
 import TitleInfo from '../TitleInfo';
 
-// helper function to load image using promises
-const loadImage = src => new Promise((resolve, reject) => {
-  const img = document.createElement('img');
-  img.addEventListener('load', () => { resolve(img); })
-  img.addEventListener('error', (err) => { reject(404); })
-  img.src = src;
-});
-
 class OpenSeaDragon extends React.Component {
   render() {
     const { id } = this.props;
     return (
-      <div className="ocd-div" ref={(node) => { this.el = node; }} >
-        <div className="openseadragon" id={id} style={{ height: '100px', width: '100px' }} />
-      </div>
+      <div id={id} style={{ height: '100%', width: '100%' }} />
     );
   }
 
@@ -29,51 +19,13 @@ class OpenSeaDragon extends React.Component {
 
     OpenSeadragon({
       id,
-      // visibilityRatio: 1.0,
-      // constrainDuringPan: false,
-      // defaultZoomLevel: 1,
-      // minZoomLevel: 1,
-      // maxZoomLevel: 10,
-      // zoomInButton: 'zoom-in',
-      // zoomOutButton: 'zoom-out',
-      // homeButton: 'reset',
-      // fullPageButton: 'full-page',
-      // nextButton: 'next',
-      // previousButton: 'previous',
-      // showNavigator: false,
-      // navigatorId: 'navigator',
+      showNavigationControl: false,
       tileSources: {
         type: 'image',
         url: 'http://gehlenborglab.org/assets/img/site/hero_backbay.jpg',
         buildPyramid: false,
-        // levels: [{ url: image, height: 100 /*data.naturalHeight*/, width: 100 /*data.naturalWidth*/ }],
       },
     });
-
-    // loadImage(image).then((data) => {
-    //   this.viewer = OpenSeadragon({
-    //     id,
-    //     // visibilityRatio: 1.0,
-    //     // constrainDuringPan: false,
-    //     // defaultZoomLevel: 1,
-    //     // minZoomLevel: 1,
-    //     // maxZoomLevel: 10,
-    //     // zoomInButton: 'zoom-in',
-    //     // zoomOutButton: 'zoom-out',
-    //     // homeButton: 'reset',
-    //     // fullPageButton: 'full-page',
-    //     // nextButton: 'next',
-    //     // previousButton: 'previous',
-    //     // showNavigator: false,
-    //     // navigatorId: 'navigator',
-    //     tileSources: {
-    //       type: 'image',
-    //       url: 'http://gehlenborglab.org/assets/img/site/hero_backbay.jpg',
-    //       buildPyramid: false,
-    //       // levels: [{ url: image, height: 100 /*data.naturalHeight*/, width: 100 /*data.naturalWidth*/ }],
-    //     },
-    //   });
-    // });
   }
 
   componentDidMount() {
