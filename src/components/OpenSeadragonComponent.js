55eb915a71e1bf8ca86c587f3424a6eb95b879f5
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-04-25 20:37:06 -0400

    Pull out OpenSeadragonComponent

---
+++ src/components/OpenSeadragonComponent.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import OpenSeadragon from 'openseadragon';
+
+export default class OpenSeadragonComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.id = `id-${Math.random()}`;
+    this.myRef = React.createRef();
+  }
+
+  render() {
+    return (
+      <div id={this.id} style={{ height: '100%', width: '100%' }} ref={this.myRef} />
+    );
+  }
+
+  initSeaDragon() {
+    const {
+      tileSources,
+      x, y, width, height,
+    } = this.props;
+    this.viewer = OpenSeadragon({
+      id: this.id,
+      showNavigationControl: false,
+      tileSources,
+    });
+    this.viewer.addHandler('open', () => {
+      // Callback is necessary: If invoked immediately, it doesn't work.
+      const rect = this.viewer.viewport.imageToViewportRectangle(x, y, width, height);
+      this.viewer.viewport.fitBounds(rect, false);
+    });
+  }
+
+  componentDidMount() {
+    this.initSeaDragon();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const {
+      x, y, width, height,
+    } = nextProps;
+    const rect = this.viewer.viewport.imageToViewportRectangle(x, y, width, height);
+    this.viewer.viewport.fitBounds(rect, false);
+    return false;
+  }
+}
--- src/components/tiledimagery/TiledImagery.js
+++ src/components/tiledimagery/TiledImagery.js
@@ -1,45 +1,8 @@
 import React from 'react';
-// import PubSub from 'pubsub-js';
-import OpenSeadragon from 'openseadragon';
-// import ReactOpenSeadragon from 'react-openseadragon'
+import OpenSeadragonComponent from '../OpenSeadragonComponent';
 
 import { BLACK_CARD } from '../classNames';
 import TitleInfo from '../TitleInfo';
-
-class OpenSeaDragon extends React.Component {
-  constructor(props) {
-    super(props);
-    this.id = `id-${Math.random()}`;
-  }
-
-  render() {
-    return (
-      <div id={this.id} style={{ height: '100%', width: '100%' }} />
-    );
-  }
-
-  initSeaDragon() {
-    const { tileSources } = this.props;
-    const viewer = OpenSeadragon({
-      id: this.id,
-      showNavigationControl: false,
-      tileSources,
-    });
-    viewer.addHandler('open', () => {
-      // Callback is necessary: If invoked immediately, it doesn't work.
-      const rect = viewer.viewport.imageToViewportRectangle(500, 200, 100, 100);
-      viewer.viewport.fitBounds(rect, false);
-    });
-  }
-
-  componentDidMount() {
-    this.initSeaDragon();
-  }
-
-  shouldComponentUpdate() {
-    return false;
-  }
-}
 
 export default function TiledImagery(props) { // eslint-disable-line no-unused-vars
   return (
@@ -49,12 +12,16 @@ export default function TiledImagery(props) { // eslint-disable-line no-unused-v
         info=""
       />
       <div className={BLACK_CARD}>
-        <OpenSeaDragon
+        <OpenSeadragonComponent
           tileSources={{
             type: 'image',
             url: 'http://gehlenborglab.org/assets/img/site/hero_backbay.jpg',
             buildPyramid: false,
           }}
+          x={800}
+          y={200}
+          width={100}
+          height={100}
         />
       </div>
     </React.Fragment>
