aa44e7409303ca52313591089639c1e906eeb5f0
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-05-02 12:54:07 -0400

    use variable

--- push-demo.sh
+++ push-demo.sh
@@ -28,4 +28,4 @@ DATE_TIME=`date "+%Y-%m-%d %H:%M:%S"`
 echo "- $DATE_TIME: [`$BRANCH/$HASH`]($TARGET_URL)" >> docs/index.md
 
 aws s3 cp --recursive demo/dist s3://$URL_PATH
-open https://s3.amazonaws.com/$URL_PATH/docs/index
+open "$TARGET_URL"
