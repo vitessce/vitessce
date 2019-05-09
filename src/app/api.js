8617f70107908233c4b3f49b6c00ce53d4d1dd0c
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-05-09 16:00:12 -0400

    less whitespace

--- src/app/api.js
+++ src/app/api.js
@@ -25,6 +25,7 @@ const linnarssonBase = {
 
 
 /* eslint-disable object-property-newline */
+/* eslint-disable object-curly-newline */
 const configs = {
   'linnarsson-2018': {
     ...linnarssonBase,
@@ -43,49 +44,33 @@ const configs = {
         600: [0, 2, 4, 8],
       },
       layout: [
-        {
-          component: 'Description',
+        { component: 'Description',
           props: {
             description: 'Linnarsson: Spatial organization of the somatosensory cortex revealed by cyclic smFISH',
           },
-          x: 0, y: 0,
-        },
-        {
-          component: 'StatusSubscriber',
-          x: 0, y: 1,
-        },
-        {
-          component: 'ScatterplotSubscriber',
+          x: 0, y: 0 },
+        { component: 'StatusSubscriber',
+          x: 0, y: 1 },
+        { component: 'ScatterplotSubscriber',
           props: { mapping: 'tsne' },
-          x: 0, y: 2, h: 2,
-        },
-        {
-          component: 'SpatialSubscriber',
+          x: 0, y: 2, h: 2 },
+        { component: 'SpatialSubscriber',
           props: {
             view: {
               zoom: -6.5,
               offset: [200, 200],
             },
           },
-          x: 1, y: 0, h: 2,
-        },
-        {
-          component: 'ScatterplotSubscriber',
+          x: 1, y: 0, h: 2 },
+        { component: 'ScatterplotSubscriber',
           props: { mapping: 'pca' },
-          x: 1, y: 2, h: 2,
-        },
-        {
-          component: 'FactorsSubscriber',
-          x: 2, y: 0, h: 2,
-        },
-        {
-          component: 'GenesSubscriber',
-          x: 2, y: 2, h: 2,
-        },
-        {
-          component: 'HeatmapSubscriber',
-          x: 0, y: 4, w: 3,
-        },
+          x: 1, y: 2, h: 2 },
+        { component: 'FactorsSubscriber',
+          x: 2, y: 0, h: 2 },
+        { component: 'GenesSubscriber',
+          x: 2, y: 2, h: 2 },
+        { component: 'HeatmapSubscriber',
+          x: 0, y: 4, w: 3 },
       ],
     },
   },
@@ -93,44 +78,30 @@ const configs = {
     ...linnarssonBase,
     name: 'Linnarsson (static layout)',
     staticLayout: [
-      {
-        component: 'Description',
+      { component: 'Description',
         props: {
           description: 'Linnarsson (static layout): Spatial organization of the somatosensory cortex revealed by cyclic smFISH',
         },
-        x: 0, y: 0, w: 3, h: 1,
-      },
-      {
-        component: 'StatusSubscriber',
-        x: 0, y: 1, w: 3, h: 1,
-      },
-      {
-        component: 'ScatterplotSubscriber',
+        x: 0, y: 0, w: 3, h: 1 },
+      { component: 'StatusSubscriber',
+        x: 0, y: 1, w: 3, h: 1 },
+      { component: 'ScatterplotSubscriber',
         props: { mapping: 'tsne' },
-        x: 0, y: 2, w: 3, h: 2,
-      },
-      {
-        component: 'SpatialSubscriber',
+        x: 0, y: 2, w: 3, h: 2 },
+      { component: 'SpatialSubscriber',
         props: {
           view: {
             zoom: -6.5,
             offset: [200, 200],
           },
         },
-        x: 3, y: 0, w: 6, h: 4,
-      },
-      {
-        component: 'FactorsSubscriber',
-        x: 9, y: 0, w: 3, h: 2,
-      },
-      {
-        component: 'GenesSubscriber',
-        x: 9, y: 2, w: 3, h: 2,
-      },
-      {
-        component: 'HeatmapSubscriber',
-        x: 0, y: 4, w: 12, h: 1,
-      },
+        x: 3, y: 0, w: 6, h: 4 },
+      { component: 'FactorsSubscriber',
+        x: 9, y: 0, w: 3, h: 2 },
+      { component: 'GenesSubscriber',
+        x: 9, y: 2, w: 3, h: 2 },
+      { component: 'HeatmapSubscriber',
+        x: 0, y: 4, w: 12, h: 1 },
     ],
   },
   'higlass-component-demo': {
@@ -138,14 +109,12 @@ const configs = {
     layers: [],
     name: 'HiGlass component demo',
     staticLayout: [
-      {
-        component: 'HiGlassComponent',
+      { component: 'HiGlassComponent',
         props: {
           options: { bounded: true },
           viewConfig: higlassViewConf,
         },
-        x: 1, y: 0, w: 10, h: 1,
-      },
+        x: 1, y: 0, w: 10, h: 1 },
     ],
   },
 };
