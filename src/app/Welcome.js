42ce1d2ac96d2ed6fe86c0736895326eafd3237d
Chuck McCallum <mccalluc@users.noreply.github.com>
2019-06-01 13:21:36 -0400

    Mccalluc/url option (#215)
    * HTML for URL submission
    
    * Split into two fragments for readability
    
    * Bootstrap tweaks
    
    * I have not seen this work... but I have seen it fail in lots of good ways :)
    
    * works; add to manual testing doc

--- src/app/Welcome.js
+++ src/app/Welcome.js
@@ -26,45 +26,73 @@ function DatasetList(props) {
   );
 }
 
+function Form(props) {
+  const { configs } = props;
+  return (
+    <form method="GET">
+      <h1><span role="img" aria-label="fast train!">🚄 </span> Vitessce</h1>
+      <div>URL of dataset:</div>
+
+      <div className="input-group mb-3">
+        <input type="text" name="url" className="form-control" style={{ background: 'lightgrey' }} />
+        <div className="input-group-append">
+          <button className="btn btn-outline-secondary" type="submit">Load</button>
+        </div>
+      </div>
+
+      <div>or select a dataset:</div>
+      <DatasetList configs={configs} />
+    </form>
+  );
+}
+
+function Info() {
+  return (
+    <React.Fragment>
+      <p>
+        This is a demo of key concepts for
+        a <b>v</b>isual <b>i</b>ntegration <b>t</b>ool
+        for <b>e</b>xploration of
+        (<b>s</b>patial) <b>s</b>ingle-<b>c</b>ell <b>e</b>xperiment data.
+        This demo focusses on scalable, linked visualizations that support both
+        spatial and non-spatial representation of cell-level and molecule-level data.
+      </p>
+      <p>
+        Vitessce is supported by the NIH Common Fund, through
+        the <a href="https://commonfund.nih.gov/HuBMAP">Human BioMolecular Atlas Program (HuBMAP)</a>,
+        Integration, Visualization & Engagement (HIVE) Initiative,
+        RFA-RM-18-001.
+      </p>
+      <p>
+        More information:
+      </p>
+      <ul>
+        <li><a href="prod-docs/index.html">Documentation</a></li>
+        <li><a href="https://github.com/hms-dbmi/vitessce">GitHub</a></li>
+        <li><a href="https://www.npmjs.com/package/vitessce">NPM</a></li>
+      </ul>
+      <p>
+        This demployment: branch={version.branch} | hash={version.hash} | date={version.date}
+      </p>
+    </React.Fragment>
+  );
+}
+
 export default function Welcome(props) {
   const { configs } = props;
   return (
-    <div className="container-fluid d-flex flex-column align-items-end">
-      <div className={LIGHT_CARD} style={{ width: '100%', maxWidth: '330px', margin: 'auto' }}>
-        <form method="GET">
-          <h1><span role="img" aria-label="fast train!">🚄 </span> Vitessce</h1>
-          <div>
-            Select a data set:
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-5">
+          <div className={LIGHT_CARD}>
+            <Form configs={configs} />
           </div>
-          <DatasetList configs={configs} />
-        </form>
-      </div>
-      <div className={LIGHT_CARD} style={{ width: '100%' }}>
-        <p>
-          This is a demo of key concepts for
-          a <b>v</b>isual <b>i</b>ntegration <b>t</b>ool
-          for <b>e</b>xploration of
-          (<b>s</b>patial) <b>s</b>ingle-<b>c</b>ell <b>e</b>xperiment data.
-          This demo focusses on scalable, linked visualizations that support both
-          spatial and non-spatial representation of cell-level and molecule-level data.
-        </p>
-        <p>
-          Vitessce is supported by the NIH Common Fund, through
-          the <a href="https://commonfund.nih.gov/HuBMAP">Human BioMolecular Atlas Program (HuBMAP)</a>,
-          Integration, Visualization & Engagement (HIVE) Initiative,
-          RFA-RM-18-001.
-        </p>
-        <p>
-          More information:
-          <ul>
-            <li><a href="prod-docs/index.html">Documentation</a></li>
-            <li><a href="https://github.com/hms-dbmi/vitessce">GitHub</a></li>
-            <li><a href="https://www.npmjs.com/package/vitessce">NPM</a></li>
-          </ul>
-        </p>
-        <p>
-          This demployment: branch={version.branch} | hash={version.hash} | date={version.date}
-        </p>
+        </div>
+        <div className="col-7">
+          <div className={LIGHT_CARD}>
+            <Info />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Ajv from 'ajv';
+
+import datasetSchema from '../schemas/dataset.schema.json';
+
+import { LIGHT_CARD } from '../components/classNames';
 
 import '../css/index.css';
 import '../../node_modules/react-grid-layout/css/styles.css';
@@ -15,19 +20,89 @@ function renderComponent(react, id) {
   ReactDOM.render(react, document.getElementById(id));
 }
 
-export default function renderApp(id) {
-  const datasetId = new URLSearchParams(window.location.search).get('dataset');
-  if (datasetId) {
-    const config = getConfig(datasetId);
+function Warning(props) {
+  const { title, preformatted, unformatted } = props;
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <div className={LIGHT_CARD}>
+            <h1>{title}</h1>
+            <pre>{preformatted}</pre>
+            <div>{unformatted}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function preformattedDetails(response) {
+  return `
+    ok: ${response.ok}
+    status: ${response.status}
+    statusText: ${response.statusText}
+    redirected: ${response.redirected}
+    type: ${response.type}
+    url: ${response.url}`; // TODO: headers
+}
+
+function renderResponse(response, id) {
+  if (!response.ok) {
     renderComponent(
-      <PubSubVitessceGrid config={config} />,
-      id,
+      <Warning
+        title="Fetch response not OK"
+        preformatted={preformattedDetails(response)}
+      />, id,
     );
   } else {
+    response.text().then((text) => {
+      try {
+        const config = JSON.parse(text);
+        const validate = new Ajv().compile(datasetSchema);
+        const valid = validate(config);
+        if (!valid) {
+          const failureReason = JSON.stringify(validate.errors, null, 2);
+          renderComponent(
+            <Warning
+              title="Config validation failed"
+              preformatted={failureReason}
+            />, id,
+          );
+        } else {
+          renderComponent(<PubSubVitessceGrid config={config} />, id);
+        }
+      } catch (e) {
+        renderComponent(
+          <Warning
+            title="Error parsing JSON"
+            preformatted={preformattedDetails(response)}
+            unformatted={`${e.message}: ${text}`}
+          />, id,
+        );
+      }
+    });
+  }
+}
+
+export default function renderApp(id) {
+  const datasetId = new URLSearchParams(window.location.search).get('dataset');
+  const datasetUrl = new URLSearchParams(window.location.search).get('url');
+
+  if (datasetId) {
+    const config = getConfig(datasetId);
+    renderComponent(<PubSubVitessceGrid config={config} />, id);
+  } else if (datasetUrl) {
+    fetch(datasetUrl)
+      .then(response => renderResponse(response, id))
+      .catch(error => renderComponent(
+        <Warning
+          title="Error fetching"
+          unformatted={error.message}
+        />, id,
+      ));
+  } else {
     const configs = listConfigs();
-    renderComponent(
-      <Welcome configs={configs} />,
-      id,
-    );
+    renderComponent(<Welcome configs={configs} />, id);
   }
 }
