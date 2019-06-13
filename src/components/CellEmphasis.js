ea538ca5132d2bcaf5a0b4f64dfa29d5566dcd0f
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2019-06-13 13:46:38 -0400

    Linter

--- src/components/CellEmphasis.js
+++ src/components/CellEmphasis.js
@@ -15,7 +15,8 @@ export default function CellEmphasis(props) {
     const y = projectedXY[1];
     // Only show the tooltip element if the hovered cell
     // is within the current DeckGL zoom boundaries
-    if (hoveredCellInfo.uuid !== uuid && x >= 0 && x <= viewInfo.width && y >= 0 && y <= viewInfo.height) {
+    if (hoveredCellInfo.uuid !== uuid
+        && x >= 0 && x <= viewInfo.width && y >= 0 && y <= viewInfo.height) {
       // Position a circle-shaped <div> element on top of the hovered cell
       return (
         <div
