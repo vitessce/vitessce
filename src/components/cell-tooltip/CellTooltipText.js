a24dc0a82b7bd0d488b7d3763da1628a9c55cc44
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2019-06-24 17:47:33 -0400

    Status tooltip (#246)
    * Status tooltip with collision detection
    
    * Put different factors on different lines using <table> element
    
    * Comments
    
    * Comments
    
    * Make CellTooltipText more general (not specific to deck.gl context)
    
    * Remove unnecessary css
    
    * Change ref variable
    
    * Clean up collision detection code based on feedback
    
    * Remove restrictive width constraints for tooltip text component
    
    * Merge
    
    * Add tbody to fix table bug in tooltip text componenet

---
+++ src/components/cell-tooltip/CellTooltipText.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef } from 'react';
+
+export default function CellTooltipText(props) {
+  const {
+    x,
+    y,
+    factors,
+  } = props;
+
+  const ref = useRef(null);
+  const offsetPercentage = 10;
+
+  // Do collision detection based on the bounds of the parent (.card-body) element.
+  useEffect(() => {
+    const el = ref.current;
+    const rect = el.parentNode.getBoundingClientRect();
+    const { width, height } = rect;
+    const translateX = (x > width / 2) ? -(100 + offsetPercentage) : offsetPercentage;
+    const translateY = (y > height / 2) ? -(100 + offsetPercentage) : offsetPercentage;
+    el.style.transform = `translateX(${translateX}%) translateY(${translateY}%)`;
+  });
+
+  return (
+    <div
+      ref={ref}
+      className="cell-tooltip bg-light"
+      style={{
+        left: `${x}px`,
+        top: `${y}px`,
+      }}
+    >
+      <table>
+        <tbody>
+          {Object.keys(factors).map(key => (
+            <tr key={key}>
+              <th>{key}</th>
+              <td>{factors[key]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
