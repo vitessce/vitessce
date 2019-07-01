5f983ee411c130ef576d806da254dd2b5959c6fc
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2019-07-01 12:00:30 -0400

    Class naming change

--- src/components/classNames.js
+++ src/components/classNames.js
@@ -1,6 +1,6 @@
-const CARD = 'card card-body my-2';
+export const TOOLTIP_ANCESTOR = 'tooltip-ancestor';
+const CARD = `card card-body my-2 ${TOOLTIP_ANCESTOR}`;
 export const BLACK_CARD = `${CARD} bg-black`;
 export const LIGHT_CARD = `${CARD} bg-light`;
 export const TITLE_CARD = 'title';
 export const SCROLL_CARD = `${LIGHT_CARD} scroll`;
-export const TOOLTIP_ANCESTOR = 'tooltip-ancestor';
