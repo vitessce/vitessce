bcb85cf8c6e883bf62223d536dcc2c69b376498b
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2019-07-01 11:54:34 -0400

    Update scale and offset of tooltip based on ancestor width

--- src/components/cell-tooltip/CellTooltipText.js
+++ src/components/cell-tooltip/CellTooltipText.js
@@ -10,13 +10,14 @@ export default function CellTooltipText(props) {
   } = props;
 
   const ref = useRef(null);
-  const offsetPercentage = 10;
   // Do collision detection based on the bounds of the tooltip ancestor element.
   useEffect(() => {
     const el = ref.current;
+    const offsetPercentage = (parentWidth < 200) ? -5 : 10;
     const translateX = (x > parentWidth / 2) ? -(100 + offsetPercentage) : offsetPercentage;
     const translateY = (y > parentHeight / 2) ? -(100 + offsetPercentage) : offsetPercentage;
-    el.style.transform = `translateX(${translateX}%) translateY(${translateY}%)`;
+    const scale = (parentWidth < 200) ? 0.75 : 1.0;
+    el.style.transform = `translateX(${translateX}%) translateY(${translateY}%) scale(${scale})`;
   });
 
   return (
