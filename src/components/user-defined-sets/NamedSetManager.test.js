4759a41d22875bb4786d379fdfd9f11a664fb638
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2019-07-10 15:44:45 -0400

    Add basic tests for NamedSetManager component

---
+++ src/components/user-defined-sets/NamedSetManager.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+import expect from 'expect';
+import NamedSetManager from './NamedSetManager';
+import * as Sets from './sets';
+
+configure({ adapter: new Adapter() });
+
+describe('NamedSetManager.js', () => {
+  describe('<NamedSetManager />', () => {
+    it('renders name and edit button when not editing', () => {
+      let setsState = Sets.initialState;
+      setsState = Sets.setNamedSet(setsState, 'test', new Set([1, 2, 3]));
+      const wrapper = shallow(<NamedSetManager sets={setsState} name="test" />);
+      expect(wrapper.find('input').length).toEqual(0);
+      expect(wrapper.find('button').length).toEqual(1);
+    });
+
+    it('renders input when editing', () => {
+      let setsState = Sets.initialState;
+      setsState = Sets.setNamedSet(setsState, 'test', new Set([1, 2, 3]));
+      const wrapper = shallow(<NamedSetManager sets={setsState} name="test" />);
+      const button = wrapper.find('button');
+      button.simulate('click', {});
+      wrapper.update();
+      expect(wrapper.find('input').length).toEqual(1);
+    });
+  });
+});
