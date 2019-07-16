c7b399fc963cfc633aaa4fdf287205af86c77d8a
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-07-16 15:26:46 -0400

    Fix test

--- src/components/status/Status.test.js
+++ src/components/status/Status.test.js
@@ -4,6 +4,8 @@ import { shallow, configure } from 'enzyme';
 import expect from 'expect';
 import Status from './Status';
 
+import componentCss from '../../css/component.module.scss';
+
 configure({ adapter: new Adapter() });
 
 function assertElementHasText(wrapper, query, text) {
@@ -19,14 +21,14 @@ describe('Status.js', () => {
 
     it('shows info', () => {
       const wrapper = shallow(<Status info="INFO" />);
-      assertElementHasText(wrapper, '[className="details"]', 'INFO');
+      assertElementHasText(wrapper, `[className="${componentCss.details}"]`, 'INFO');
       // The warning also has the "details" class, among others,
       // so ".details" is not sufficient.
     });
 
     it('shows both', () => {
       const wrapper = shallow(<Status info="INFO" warn="WARN" />);
-      assertElementHasText(wrapper, '[className="details"]', 'INFO');
+      assertElementHasText(wrapper, `[className="${componentCss.details}"]`, 'INFO');
       assertElementHasText(wrapper, '.alert-warning', 'WARN');
     });
   });
