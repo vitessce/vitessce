3592349bb128595d70d0b2cc87eff6fd82173a80
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2019-07-26 12:33:26 -0400

    Un-css-module. Fixes #323

--- src/components/classNames.js
+++ src/components/classNames.js
@@ -1,8 +1,6 @@
-import componentCss from '../css/component.module.scss';
-
 export const TOOLTIP_ANCESTOR = 'tooltip-ancestor';
 const CARD = `card card-body my-2 ${TOOLTIP_ANCESTOR}`;
-export const BLACK_CARD = `${CARD} ${componentCss.bgBlack}`;
-export const LIGHT_CARD = `${CARD} ${componentCss.bgLight}`;
-export const TITLE_CARD = componentCss.title;
-export const SCROLL_CARD = `${LIGHT_CARD} ${componentCss.scroll}`;
+export const BLACK_CARD = `${CARD} bg-black`;
+export const LIGHT_CARD = `${CARD} bg-light`;
+export const TITLE_CARD = 'title';
+export const SCROLL_CARD = `${LIGHT_CARD} scroll`;
