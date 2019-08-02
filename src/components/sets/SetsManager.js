63618cfe2d920b44871a237ac5b6076c4c7266fd
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2019-08-02 14:27:16 -0400

    Remove unnecessary dependency

--- src/components/sets/SetsManager.js
+++ src/components/sets/SetsManager.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Tabs } from 'antd';
 import SetsManagerTab from './SetsManagerTab';
 import SetsManagerActionBar from './SetsManagerActionBar';
