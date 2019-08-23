13ea9ea2e327fe1cb6f465fec17530f35576dd50
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-08-23 15:41:41 -0400

    Rephrase comment and split on to two lines

--- src/app/api.js
+++ src/app/api.js
@@ -93,7 +93,8 @@ const configs = {
         { component: 'scatterplot',
           props: {
             mapping: 'PCA',
-            // This intentionally does not have a  "view" prop, so that we can exercise using the default.
+            // This intentionally does not have a  "view" prop,
+            // in order to demonstrate that the default works.
           },
           x: 2, y: 0, h: 2 },
         { component: 'scatterplot',
