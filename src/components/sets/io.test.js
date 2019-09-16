974c07775ef6983e2175c40c02ce11f5bc6b5380
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-09-16 16:27:00 -0400

    URI decode the expected outputs to make more maintainable.

--- src/components/sets/io.test.js
+++ src/components/sets/io.test.js
@@ -67,13 +67,31 @@ const jsonString = JSON.stringify({
   setsTree: correctProcessedJSON,
 });
 
-const correctExportedJSON = 'data:text/json;charset=utf-8,%7B%22datasetId%22%3A%22linnarsson-2018%22%2C%22setsType%22%3A%22cell%22%2C%22version%22%3A%220.0.17%22%2C%22setsTree%22%3A%5B%7B%22key%22%3A%22other-selection%22%2C%22name%22%3A%22Other%20selection%22%2C%22color%22%3A%5B0%2C0%2C0%5D%2C%22set%22%3A%5B%22cell_9%22%2C%22cell_10%22%5D%7D%2C%7B%22key%22%3A%22neurons%22%2C%22name%22%3A%22Neurons%22%2C%22color%22%3A%5B0%2C0%2C0%5D%2C%22set%22%3A%5B%22cell_3%22%5D%7D%2C%7B%22key%22%3A%22neurons%5Ctexcitatory%22%2C%22name%22%3A%22Excitatory%20Neurons%22%2C%22color%22%3A%5B1%2C1%2C1%5D%2C%22set%22%3A%5B%22cell_6%22%2C%22cell_7%22%2C%22cell_8%22%5D%7D%2C%7B%22key%22%3A%22neurons%5Ctinhibitory%22%2C%22name%22%3A%22Inhibitory%20Neurons%22%2C%22color%22%3A%5B1%2C0%2C0%5D%2C%22set%22%3A%5B%22cell_4%22%2C%22cell_5%22%5D%7D%2C%7B%22key%22%3A%22empty-selection%22%2C%22name%22%3A%22Empty%20selection%22%2C%22color%22%3A%5B0%2C0%2C0%5D%7D%5D%7D';
+const jsonPrefix = 'data:text/json;charset=utf-8,';
+const correctExportedJSON = jsonPrefix + encodeURIComponent(
+  // Note the '//t' below: We want a literal '/t', so we escape the '/'.
+  '{"datasetId":"linnarsson-2018","setsType":"cell","version":"0.0.17","setsTree":[{"key":"other-selection","name":"Other selection","color":[0,0,0],"set":["cell_9","cell_10"]},{"key":"neurons","name":"Neurons","color":[0,0,0],"set":["cell_3"]},{"key":"neurons\\texcitatory","name":"Excitatory Neurons","color":[1,1,1],"set":["cell_6","cell_7","cell_8"]},{"key":"neurons\\tinhibitory","name":"Inhibitory Neurons","color":[1,0,0],"set":["cell_4","cell_5"]},{"key":"empty-selection","name":"Empty selection","color":[0,0,0]}]}',
+);
+const correctExportedEmptyJSON = jsonPrefix + encodeURIComponent(
+  '{"datasetId":"linnarsson-2018","setsType":"cell","version":"0.0.17","setsTree":[]}',
+);
 
-const correctExportedEmptyJSON = 'data:text/json;charset=utf-8,%7B%22datasetId%22%3A%22linnarsson-2018%22%2C%22setsType%22%3A%22cell%22%2C%22version%22%3A%220.0.17%22%2C%22setsTree%22%3A%5B%5D%7D';
-
-const correctExportedTSV = 'data:text/tsv;charset=utf-8,%22Item%20ID%22%09%22Set%20Key%22%09%22Set%20Name%22%09%22Set%20Color%22%0A%22cell_9%22%09%22other-selection%22%09%22Other%20selection%22%09%22%23000000%22%0A%22cell_10%22%09%22other-selection%22%09%22Other%20selection%22%09%22%23000000%22%0A%22cell_3%22%09%22neurons%22%09%22Neurons%22%09%22%23000000%22%0A%22cell_6%22%09%22neurons%09excitatory%22%09%22Neurons%3BExcitatory%20Neurons%22%09%22%23010101%22%0A%22cell_7%22%09%22neurons%09excitatory%22%09%22Neurons%3BExcitatory%20Neurons%22%09%22%23010101%22%0A%22cell_8%22%09%22neurons%09excitatory%22%09%22Neurons%3BExcitatory%20Neurons%22%09%22%23010101%22%0A%22cell_4%22%09%22neurons%09inhibitory%22%09%22Neurons%3BInhibitory%20Neurons%22%09%22%23010000%22%0A%22cell_5%22%09%22neurons%09inhibitory%22%09%22Neurons%3BInhibitory%20Neurons%22%09%22%23010000%22%0A%22NA%22%09%22empty-selection%22%09%22Empty%20selection%22%09%22%23000000%22';
-
-const correctExportedEmptyTSV = 'data:text/tsv;charset=utf-8,%22Item%20ID%22%09%22Set%20Key%22%09%22Set%20Name%22%09%22Set%20Color%22';
+const tsvPrefix = 'data:text/tsv;charset=utf-8,';
+const correctExportedTSV = tsvPrefix + encodeURIComponent(
+  `"Item ID"	"Set Key"	"Set Name"	"Set Color"
+"cell_9"	"other-selection"	"Other selection"	"#000000"
+"cell_10"	"other-selection"	"Other selection"	"#000000"
+"cell_3"	"neurons"	"Neurons"	"#000000"
+"cell_6"	"neurons	excitatory"	"Neurons;Excitatory Neurons"	"#010101"
+"cell_7"	"neurons	excitatory"	"Neurons;Excitatory Neurons"	"#010101"
+"cell_8"	"neurons	excitatory"	"Neurons;Excitatory Neurons"	"#010101"
+"cell_4"	"neurons	inhibitory"	"Neurons;Inhibitory Neurons"	"#010000"
+"cell_5"	"neurons	inhibitory"	"Neurons;Inhibitory Neurons"	"#010000"
+"NA"	"empty-selection"	"Empty selection"	"#000000"`,
+);
+const correctExportedEmptyTSV = tsvPrefix + encodeURIComponent(
+  '"Item ID"	"Set Key"	"Set Name"	"Set Color"',
+);
 
 describe('io.js', () => {
   describe('importing sets', () => {
