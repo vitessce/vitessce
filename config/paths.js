6fe842751c020ddd9d598e8c52f7a67526f4fa24
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-03-19 15:59:32 -0400

    Use relative script/stylesheet urls to support arbitrary demo paths

--- config/paths.js
+++ config/paths.js
@@ -23,11 +23,9 @@ const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 // like /todos/42/static/js/bundle.7289d.js. We have to know the root.
 const publicUrlOrPath = getPublicUrlOrPath(
   process.env.NODE_ENV === 'development',
-  require(resolveApp('package.json')).homepage,
+  ".",
   process.env.PUBLIC_URL
 );
-
-
 
 // Resolve file paths in the same order as webpack
 const resolveModule = (resolveFn, filePath) => {
