065ea9fd47630593c31b9af540e31e7c9513df2f
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-03-19 16:00:51 -0400

    comment

--- config/paths.js
+++ config/paths.js
@@ -15,15 +15,9 @@ const utils = require('../scripts/utils');
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
-// We use `PUBLIC_URL` environment variable or "homepage" field to infer
-// "public path" at which the app is served.
-// webpack needs to know it to put the right <script> hrefs into HTML even in
-// single-page apps that may serve index.html for nested URLs like /todos/42.
-// We can't use a relative path in HTML because we don't want to load something
-// like /todos/42/static/js/bundle.7289d.js. We have to know the root.
 const publicUrlOrPath = getPublicUrlOrPath(
   process.env.NODE_ENV === 'development',
-  ".",
+  ".", // https://create-react-app.dev/docs/deployment/#serving-the-same-build-from-different-paths
   process.env.PUBLIC_URL
 );
 
