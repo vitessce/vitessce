7dd02f2193bea15445cb6c6b17072e31b79fa67f
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-03-19 16:05:31 -0400

    comment

--- config/paths.js
+++ config/paths.js
@@ -1,6 +1,8 @@
 /*
  * This file has been ejected from create-react-app v3.4.0.
  * The paths have been modified, and the `libBuild` and `libIndexJs` paths have been added.
+ * Rather than using the `homepage` property from `package.json` we now use "." to ensure 
+ * that relative paths are used for <script> and <link> tag urls.
  */
 
 'use strict';
@@ -15,9 +17,10 @@ const utils = require('../scripts/utils');
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
+// https://create-react-app.dev/docs/deployment/#serving-the-same-build-from-different-paths
 const publicUrlOrPath = getPublicUrlOrPath(
   process.env.NODE_ENV === 'development',
-  ".", // https://create-react-app.dev/docs/deployment/#serving-the-same-build-from-different-paths
+  ".",
   process.env.PUBLIC_URL
 );
 
