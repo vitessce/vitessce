3b033cc6a623b46248bc71327281b69c3072a94b
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-04-30 12:08:59 -0400

    Comments

--- src/components/higlass/HiGlassSubscriber.js
+++ src/components/higlass/HiGlassSubscriber.js
@@ -7,6 +7,17 @@ import 'higlass/dist/hglib.css';
 
 const HiGlassComponent = loadable(() => import('higlass').then(d => new Promise(resolve => resolve(d.HiGlassComponent))));
 
+/**
+ * A wrapper around HiGlass (http://higlass.io/).
+ * The HiGlassComponent react component is loaded lazily.
+ * @prop {object} hgViewConfig A HiGlass viewconfig object to pass
+ * to the HiGlassComponent viewConfig prop.
+ * @prop {object} hgOptions An optional HiGlass object to pass
+ * to the HiGlassComponent hgOptions prop.
+ * @prop {function} removeGridComponent A grid component removal handler
+ * to pass to the TitleInfo component.
+ * @prop {function} onReady A callback function to signal that the component is ready.
+ */
 export default function HiGlassSubscriber(props) {
   const {
     hgViewConfig,
