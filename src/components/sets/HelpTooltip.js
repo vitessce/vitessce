320c1f282d1ac1951e8fe249218feb77345fa6be
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-05-14 21:01:17 -0400

    Remove unnecessary zoom animation css

--- src/components/sets/HelpTooltip.js
+++ src/components/sets/HelpTooltip.js
@@ -4,12 +4,12 @@ import RcTooltip from 'rc-tooltip';
 /**
  * This is a small wrapper around the Tooltip component from the rc-tooltip library,
  * which is required to be able to apply theme styles to the tooltip.
- * This is because the default `getTooltipContainer` function used by antd
- * just returns `document.body` (see https://ant.design/components/tooltip/#API),
- * but theme styles are applied using a sibling class on `.vitessce-container`
+ * This is because the default `getTooltipContainer` function used by rc-tooltip
+ * just returns `document.body` (see https://github.com/react-component/tooltip#props),
+ * but theme styles need to be applied using a sibling class on `.vitessce-container`
  * (which is a child of `body`).
  * https://github.com/hubmapconsortium/vitessce/pull/494#discussion_r395957914
- * @param {*} props Props are passed through to the <RcTooltip/> from the library.
+ * @param {object} props Props are passed through to the <RcTooltip/> from the library.
  */
 export default function HelpTooltip(props) {
   const { title, content, overlayClassName } = props;
