bb77d1ef2b5fa603ae6a7f06d42da6b32d7e5909
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-06-08 10:03:46 -0400

    Exports in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { Heatmap } from './components/heatmap';
 import { Spatial } from './components/spatial';
 import { Scatterplot } from './components/scatterplot';
 import PubSubVitessceGrid from './app/PubSubVitessceGrid';
-import { createApp, Vitessce } from './app';
+import { createApp, Vitessce, VitessceGrid } from './app';
 
 export {
   Heatmap,
@@ -11,4 +11,5 @@ export {
   PubSubVitessceGrid,
   createApp,
   Vitessce,
+  VitessceGrid,
 };
