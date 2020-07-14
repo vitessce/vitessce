a6cc824fce35fdc193b7c381b91108922b40d83b
ilan-gold <ilanbassgold@gmail.com>
2020-07-14 14:41:12 -0400

    Refactor heatmap and add urls

--- src/components/heatmap/HeatmapSubscriber.js
+++ src/components/heatmap/HeatmapSubscriber.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import PubSub from 'pubsub-js';
 
 import TitleInfo from '../TitleInfo';
@@ -8,92 +8,98 @@ import {
 } from '../../events';
 import Heatmap from './Heatmap';
 
-export default class HeatmapSubscriber extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cells: {}, clusters: null, selectedCellIds: new Set(), cellColors: null,
+export default function HeatmapSubscriber(props) {
+  const {
+    children,
+    uuid,
+    removeGridComponent,
+    onReady,
+  } = props;
+  const [cells, setCells] = useState({});
+  const [clusters, setClusters] = useState({});
+  const [cellColors, setCellColors] = useState({});
+  const [selectedCellIds, setSelectedCellIds] = useState({});
+  const [urls, setUrls] = useState([]);
+
+  const onReadyCallback = useCallback(onReady, []);
+
+  useEffect(() => {
+    function clustersAddSubscriber(msg, { data, url }) {
+      setClusters(data);
+      setUrls((prevUrls) => {
+        const newUrls = [...prevUrls].concat({ url, name: 'Cells' });
+        return newUrls;
+      });
+    }
+    function cellsAddSubscriber(msg, { data, url }) {
+      setCells(data);
+      setUrls((prevUrls) => {
+        const newUrls = [...prevUrls].concat({ url, name: 'Cells' });
+        return newUrls;
+      });
+    }
+    function cellsSelectionSubscriber(msg, cellIds) {
+      setSelectedCellIds(cellIds);
+    }
+    function cellsColorSubscriber(msg, newCellColors) {
+      setCellColors(newCellColors);
+    }
+    const clustersAddToken = PubSub.subscribe(
+      CLUSTERS_ADD, clustersAddSubscriber,
+    );
+    const cellsAddToken = PubSub.subscribe(
+      CELLS_ADD, cellsAddSubscriber,
+    );
+    const cellsColorToken = PubSub.subscribe(
+      CELLS_COLOR, cellsColorSubscriber,
+    );
+    const cellsSelectionToken = PubSub.subscribe(
+      CELLS_SELECTION, cellsSelectionSubscriber,
+    );
+    const cellSetsViewToken = PubSub.subscribe(
+      CELL_SETS_VIEW, cellsSelectionSubscriber,
+    );
+    onReadyCallback();
+    return () => {
+      PubSub.unsubscribe(clustersAddToken);
+      PubSub.unsubscribe(cellsAddToken);
+      PubSub.unsubscribe(cellsColorToken);
+      PubSub.unsubscribe(cellsSelectionToken);
+      PubSub.unsubscribe(cellSetsViewToken);
     };
-    this.componentWillUnmount = this.componentWillUnmount.bind(this);
-  }
-
-  // eslint-disable-next-line camelcase
-  UNSAFE_componentWillMount() {
-    this.clustersAddToken = PubSub.subscribe(
-      CLUSTERS_ADD, this.clustersAddSubscriber.bind(this),
-    );
-    this.cellsAddToken = PubSub.subscribe(
-      CELLS_ADD, this.cellsAddSubscriber.bind(this),
-    );
-    this.cellsColorToken = PubSub.subscribe(
-      CELLS_COLOR, this.cellsColorSubscriber.bind(this),
-    );
-    this.cellsSelectionToken = PubSub.subscribe(
-      CELLS_SELECTION, this.cellsSelectionSubscriber.bind(this),
-    );
-    this.cellSetsViewToken = PubSub.subscribe(
-      CELL_SETS_VIEW, this.cellsSelectionSubscriber.bind(this),
-    );
-  }
-
-  componentDidMount() {
-    const { onReady } = this.props;
-    onReady();
-  }
-
-  componentWillUnmount() {
-    PubSub.unsubscribe(this.clustersAddToken);
-    PubSub.unsubscribe(this.cellsAddToken);
-    PubSub.unsubscribe(this.cellsColorToken);
-    PubSub.unsubscribe(this.cellsSelectionToken);
-    PubSub.unsubscribe(this.cellSetsViewToken);
-  }
-
-  clustersAddSubscriber(msg, { data: clusters }) {
-    this.setState({ clusters });
-  }
-
-  cellsAddSubscriber(msg, { data: cells }) {
-    this.setState({ cells });
-  }
-
-  cellsSelectionSubscriber(msg, cellIds) {
-    this.setState({ selectedCellIds: cellIds });
-  }
-
-  cellsColorSubscriber(msg, cellColors) {
-    this.setState({ cellColors });
-  }
-
-  render() {
-    const {
-      cells, clusters, selectedCellIds, cellColors,
-    } = this.state;
-    const cellsCount = clusters ? clusters.cols.length : 0;
-    const genesCount = clusters ? clusters.rows.length : 0;
-    const selectedCount = selectedCellIds ? selectedCellIds.size : 0;
-    const { children, uuid, removeGridComponent } = this.props;
-    return (
-      <TitleInfo
-        title="Heatmap"
-        info={`${cellsCount} cells × ${genesCount} genes,
-               with ${selectedCount} cells selected`}
-        removeGridComponent={removeGridComponent}
-      >
-        {children}
-        <Heatmap
-          uuid={uuid}
-          cells={cells}
-          clusters={clusters}
-          selectedCellIds={selectedCellIds}
-          cellColors={cellColors}
-          updateCellsHover={hoverInfo => PubSub.publish(CELLS_HOVER, hoverInfo)}
-          updateStatus={message => PubSub.publish(STATUS_INFO, message)}
-          clearPleaseWait={
-            layerName => PubSub.publish(CLEAR_PLEASE_WAIT, layerName)
-          }
-        />
-      </TitleInfo>
-    );
-  }
+  }, [onReadyCallback]);
+  // eslint-disable-next-line
+  const cellsCount = clusters.cols ? clusters.cols.length : 0;
+  const genesCount = clusters.rows ? clusters.rows.length : 0;
+  const selectedCount = selectedCellIds ? selectedCellIds.size : 0;
+  const allReady = cellsCount && genesCount && selectedCount;
+  return (
+    <TitleInfo
+      title="Heatmap"
+      info={`${cellsCount} cells × ${genesCount} genes,
+              with ${selectedCount} cells selected`}
+      removeGridComponent={removeGridComponent}
+      urls={urls}
+    >
+      {children}
+      {
+        allReady
+          ? (
+            <Heatmap
+              uuid={uuid}
+              cells={cells}
+              clusters={clusters}
+              selectedCellIds={selectedCellIds}
+              cellColors={cellColors}
+              updateCellsHover={hoverInfo => PubSub.publish(CELLS_HOVER, hoverInfo)}
+              updateStatus={message => PubSub.publish(STATUS_INFO, message)}
+              clearPleaseWait={
+                layerName => PubSub.publish(CLEAR_PLEASE_WAIT, layerName)
+              }
+            />
+          )
+          : null
+      }
+    </TitleInfo>
+  );
 }
