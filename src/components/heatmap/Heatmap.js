3db6a913e885599dc13f0a1d2f8c87bdace65de9
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-07-30 16:39:31 -0400

    Revert "Fix typo in heatmap props"
    This reverts commit eef211ab5b90b0c9dfc408bf6c2f87ef41985b75.

--- src/components/heatmap/Heatmap.js
+++ src/components/heatmap/Heatmap.js
@@ -61,8 +61,8 @@ import {
  * object { uuid, project() } where project is a function that maps (cellId, geneId)
  * to canvas (x,y) coordinates. Used to show tooltips. Optional.
  * @param {boolean} props.transpose By default, false.
- * @param {string} props.variablesTitle By default, 'Genes'.
- * @param {string} props.observationsTitle By default, 'Cells'.
+ * @param {string} variableTitle By default, 'Genes'.
+ * @param {string} observationTitle By default, 'Cells'.
  */
 const Heatmap = forwardRef((props, deckRef) => {
   const {
@@ -83,12 +83,12 @@ const Heatmap = forwardRef((props, deckRef) => {
     updateStatus = createDefaultUpdateStatus('Heatmap'),
     updateViewInfo = createDefaultUpdateViewInfo('Heatmap'),
     transpose = false,
-    variablesTitle = 'Genes',
-    observationsTitle = 'Cells',
+    variableTitle = 'Genes',
+    observationTitle = 'Cells',
   } = props;
 
-  const axisLeftTitle = (transpose ? variablesTitle : observationsTitle);
-  const axisTopTitle = (transpose ? observationsTitle : variablesTitle);
+  const axisLeftTitle = (transpose ? variableTitle : observationTitle);
+  const axisTopTitle = (transpose ? observationTitle : variableTitle);
 
   useEffect(() => {
     if (clearPleaseWait && expression) {
