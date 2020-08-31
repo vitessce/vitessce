2b4ac5fd7852e6550acbd74a03f4f73021bd4dd9
ilan-gold <ilanbassgold@gmail.com>
2020-08-31 17:44:50 -0400

    Update homepage.

--- src/app/Welcome.js
+++ src/app/Welcome.js
@@ -36,7 +36,7 @@ function Form(props) {
   const { configs, theme } = props;
   return (
     <form method="GET">
-      <h1><span role="img" aria-label="fast train!">🚄 </span> Vitessce</h1>
+      <h1> Vitessce</h1>
       <div>Select a dataset:</div>
       <DatasetList configs={configs} theme={theme} />
 
@@ -56,35 +56,49 @@ function Form(props) {
 function Info() {
   return (
     <>
-      <p>
-        This is a demo of key concepts for a
-        {
-          'visual integration tool for exploration of spatial single cell experiments'.split(' ')
-            .map(
-              (word, i) => (['for', 'of'].includes(word)
-              /* eslint-disable react/no-array-index-key */
-                ? <span key={i}> {word}</span>
-                : <span key={i}> <b>{word[0]}</b>{word.slice(1)}</span>),
-              /* eslint-enable */
-            )
-        }.
-        This demo focusses on scalable, linked visualizations that support both
-        spatial and non-spatial representation of cell-level and molecule-level data.
+      <p className="info-paragraph">
+        Vitessce is a visual integration tool for exploration of spatial single cell experiments.
+        Its modular design is optimized for scalable, linked visualizations that support the
+        spatial and non-spatial representation of tissue-, cell- and molecule-level data.
+        Vitessce integrates the <a href="https://github.com/hms-dbmi/viv">Viv library</a> to visualize
+        highly multiplexed, high-resolution, high-bit depth image data directly from
+        OME-TIFF files and Bio-Formats-compatible Zarr stores.
       </p>
-      <p>
-        Vitessce is supported by the NIH Common Fund, through
-        the <a href="https://commonfund.nih.gov/HuBMAP">Human BioMolecular Atlas Program (HuBMAP)</a>,
-        Integration, Visualization & Engagement (HIVE) Initiative,
-        RFA-RM-18-001.
-      </p>
-      <p>
-        More information:
-      </p>
+      <h5 className="info-section-text">
+        Contributors
+      </h5>
+      <ul>
+        <li><a href="https://github.com/keller-mark">Mark Keller</a></li>
+        <li><a href="https://github.com/mccalluc">Chuck McCallum</a></li>
+        <li><a href="https://github.com/ilan-gold">Ilan Gold</a></li>
+        <li><a href="https://github.com/manzt">Trevor Manz</a></li>
+        <li><a href="https://github.com/thomaslchan">Tos Chan</a></li>
+        <li><a href="https://github.com/jkmarx">Jennifer Marx</a></li>
+        <li><a href="https://github.com/pkharchenko">Peter Kharchenko</a></li>
+        <li><a href="https://github.com/ngehlenborg">Nils Gehlenborg</a></li>
+      </ul>
+      <h5 className="info-section-text">
+        Source Code
+      </h5>
       <ul>
         <li><a href="https://github.com/hubmapconsortium/vitessce">GitHub</a></li>
         <li><a href="https://www.npmjs.com/package/vitessce">NPM</a></li>
       </ul>
-      <p>
+      <h5 className="info-section-text">
+        Funding
+      </h5>
+      <ul>
+        <li>
+          NIH/OD Human BioMolecular Atlas Program (HuBMAP)
+          (OT2OD026677, PI: Nils Gehlenborg).
+        </li>
+        <li>
+          NIH/NLM Biomedical Informatics and Data Science Research Training Program
+          (T15LM007092, PI: Nils Gehlenborg)
+        </li>
+        <li>Harvard Stem Cell Institute (CF-0014-17-03, PI: Nils Gehlenborg)</li>
+      </ul>
+      <p className="info-section-text">
         This deployment: branch={version.branch} | hash={version.hash} | date={version.date}
       </p>
     </>
