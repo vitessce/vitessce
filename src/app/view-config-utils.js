572db0fea82964ce3294b6a04399ae7d4e0fe610
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-08-31 10:30:53 -0400

    Comments

--- src/app/view-config-utils.js
+++ src/app/view-config-utils.js
@@ -7,6 +7,10 @@ import {
 /**
  * Generate a new scope name which does not
  * conflict / overlap with a previous scope name.
+ * Really these just need to be unique within the coordination object.
+ * So in theory they could be String(Math.random()) or uuidv4() or something.
+ * However it may be good to make them more human-readable and memorable
+ * since eventually we will want to expose a UI to update the coordination.
  * @param {string[]} prevScopes Previous scope names.
  * @returns {string} The new scope name.
  */
