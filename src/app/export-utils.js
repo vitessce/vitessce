39e74f24ce9424623eebc1d5cb055e1a47488857
ilan-gold <ilanbassgold@gmail.com>
2020-11-12 11:59:24 -0500

    Update tests.

--- src/app/export-utils.js
+++ src/app/export-utils.js
@@ -9,11 +9,11 @@ const VERSION_QUERY_STRING = 'version';
  * @param {Object} conf Previous scope names.
  * @param {Object} confParameter The parameter to use instead of vitessce_conf for
  * the view config in the URL (optional).
- * @returns {string} The URL params as a string like vitessce_conf=fksdfasdfjkl&version=0.0.1
+ * @returns {string} The URL params as a string like version=0.0.1#vitessce_conf=fksdfasdfjkl
  */
 export function encodeConfAsURLParams(conf, confParameter) {
   const compressedConf = compressToEncodedURIComponent(JSON.stringify(conf));
-  const params = `${confParameter || VITESSCE_CONF_QUERY_STRING}=${compressedConf}&${VERSION_QUERY_STRING}=${CURRENT_VERSION}`;
+  const params = `${VERSION_QUERY_STRING}=${CURRENT_VERSION}#${confParameter || VITESSCE_CONF_QUERY_STRING}=${compressedConf}`;
   return params;
 }
 
@@ -21,13 +21,13 @@ export function encodeConfAsURLParams(conf, confParameter) {
  * Decode URL params to a Vitessce configuration.
  * The URL params must have version and vitessce_conf params,
  * like vitessce_conf=fksdfasdfjkl&version=0.0.1.
- * @param {Object} queryString The URL params, like vitessce_conf=fksdfasdfjkl&version=0.0.1.
+ * @param {Object} queryString The URL params, like version=0.0.1#vitessce_conf=fksdfasdfjkl.
  * @param {Object} confParameter The parameter to use instead of vitessce_conf
  * for the view config in the URL (optional).
  * @returns {string} A vitessce configuration.
  */
 export function decodeURLParamsToConf(queryString, confParameter) {
-  const params = new URLSearchParams(queryString);
+  const params = new URLSearchParams(queryString.replace('#', '&'));
   const compressedConfString = params.get(confParameter || VITESSCE_CONF_QUERY_STRING);
   const version = params.get(VERSION_QUERY_STRING);
   if (version === CURRENT_VERSION) {
--- src/app/export-utils.test.js
+++ src/app/export-utils.test.js
@@ -38,34 +38,34 @@ describe('src/app/export-utils.js', () => {
   describe('encodeConfAsURLParams', () => {
     it('encodes a view config for a URL', () => {
       const urlParams1 = encodeConfAsURLParams({});
-      expect(urlParams1).toEqual('vitessce_conf=N4XyA&version=0.0.1');
+      expect(urlParams1).toEqual('version=0.0.1#vitessce_conf=N4XyA');
 
       const urlParams2 = encodeConfAsURLParams(fakeConfig);
-      expect(urlParams2).toEqual('vitessce_conf=N4IgbgpgTgzglgewHYgFwgAwDoCMWMgA0IADgK4BGANnAMZoBmAhlTBMVUwJ7QxoDaoJEwC2ENCFoQqrIiAAuXEuPQBhAKIAZTQGU5DOFQgAVJSsnTWWAFYxkcslCoSAFvPklUAei8QAHqIkRli0CCJeUjIwNnYoAL4AusTCYhIAUmQw8gAEMLRM7tBBCPJyWQV0mtwIZKWogpJhJMgQSHUgeQXyRVQlciRQCCR8qKAiTCQkcEgA5hLyALQ6AHLqcmBwEADuaKAAXghhaNgA7ACsxPJMUDMQdfwYhI8YCXFxxH7HxFxfIDuoOAATMQXGhAYk4kA&version=0.0.1');
+      expect(urlParams2).toEqual('version=0.0.1#vitessce_conf=N4IgbgpgTgzglgewHYgFwgAwDoCMWMgA0IADgK4BGANnAMZoBmAhlTBMVUwJ7QxoDaoJEwC2ENCFoQqrIiAAuXEuPQBhAKIAZTQGU5DOFQgAVJSsnTWWAFYxkcslCoSAFvPklUAei8QAHqIkRli0CCJeUjIwNnYoAL4AusTCYhIAUmQw8gAEMLRM7tBBCPJyWQV0mtwIZKWogpJhJMgQSHUgeQXyRVQlciRQCCR8qKAiTCQkcEgA5hLyALQ6AHLqcmBwEADuaKAAXghhaNgA7ACsxPJMUDMQdfwYhI8YCXFxxH7HxFxfIDuoOAATMQXGhAYk4kA');
     });
 
     it('decodes a view config for a URL', () => {
-      const viewConfig1 = decodeURLParamsToConf('vitessce_conf=N4XyA&version=0.0.1');
+      const viewConfig1 = decodeURLParamsToConf('version=0.0.1#vitessce_conf=N4XyA');
       expect(viewConfig1).toEqual({});
 
-      const viewConfig2 = decodeURLParamsToConf('vitessce_conf=N4IgbgpgTgzglgewHYgFwgAwDoCMWMgA0IADgK4BGANnAMZoBmAhlTBMVUwJ7QxoDaoJEwC2ENCFoQqrIiAAuXEuPQBhAKIAZTQGU5DOFQgAVJSsnTWWAFYxkcslCoSAFvPklUAei8QAHqIkRli0CCJeUjIwNnYoAL4AusTCYhIAUmQw8gAEMLRM7tBBCPJyWQV0mtwIZKWogpJhJMgQSHUgeQXyRVQlciRQCCR8qKAiTCQkcEgA5hLyALQ6AHLqcmBwEADuaKAAXghhaNgA7ACsxPJMUDMQdfwYhI8YCXFxxH7HxFxfIDuoOAATMQXGhAYk4kA&version=0.0.1');
+      const viewConfig2 = decodeURLParamsToConf('version=0.0.1#vitessce_conf=N4IgbgpgTgzglgewHYgFwgAwDoCMWMgA0IADgK4BGANnAMZoBmAhlTBMVUwJ7QxoDaoJEwC2ENCFoQqrIiAAuXEuPQBhAKIAZTQGU5DOFQgAVJSsnTWWAFYxkcslCoSAFvPklUAei8QAHqIkRli0CCJeUjIwNnYoAL4AusTCYhIAUmQw8gAEMLRM7tBBCPJyWQV0mtwIZKWogpJhJMgQSHUgeQXyRVQlciRQCCR8qKAiTCQkcEgA5hLyALQ6AHLqcmBwEADuaKAAXghhaNgA7ACsxPJMUDMQdfwYhI8YCXFxxH7HxFxfIDuoOAATMQXGhAYk4kA');
       expect(viewConfig2).toEqual(fakeConfig);
     });
   });
   describe('encodeConfAsURLParams with custom param', () => {
     it('encodes a view config for a URL', () => {
       const urlParams1 = encodeConfAsURLParams({}, 'foo');
-      expect(urlParams1).toEqual('foo=N4XyA&version=0.0.1');
+      expect(urlParams1).toEqual('version=0.0.1#foo=N4XyA');
 
       const urlParams2 = encodeConfAsURLParams(fakeConfig, 'foo');
-      expect(urlParams2).toEqual('foo=N4IgbgpgTgzglgewHYgFwgAwDoCMWMgA0IADgK4BGANnAMZoBmAhlTBMVUwJ7QxoDaoJEwC2ENCFoQqrIiAAuXEuPQBhAKIAZTQGU5DOFQgAVJSsnTWWAFYxkcslCoSAFvPklUAei8QAHqIkRli0CCJeUjIwNnYoAL4AusTCYhIAUmQw8gAEMLRM7tBBCPJyWQV0mtwIZKWogpJhJMgQSHUgeQXyRVQlciRQCCR8qKAiTCQkcEgA5hLyALQ6AHLqcmBwEADuaKAAXghhaNgA7ACsxPJMUDMQdfwYhI8YCXFxxH7HxFxfIDuoOAATMQXGhAYk4kA&version=0.0.1');
+      expect(urlParams2).toEqual('version=0.0.1#foo=N4IgbgpgTgzglgewHYgFwgAwDoCMWMgA0IADgK4BGANnAMZoBmAhlTBMVUwJ7QxoDaoJEwC2ENCFoQqrIiAAuXEuPQBhAKIAZTQGU5DOFQgAVJSsnTWWAFYxkcslCoSAFvPklUAei8QAHqIkRli0CCJeUjIwNnYoAL4AusTCYhIAUmQw8gAEMLRM7tBBCPJyWQV0mtwIZKWogpJhJMgQSHUgeQXyRVQlciRQCCR8qKAiTCQkcEgA5hLyALQ6AHLqcmBwEADuaKAAXghhaNgA7ACsxPJMUDMQdfwYhI8YCXFxxH7HxFxfIDuoOAATMQXGhAYk4kA');
     });
 
     it('decodes a view config for a URL', () => {
-      const viewConfig1 = decodeURLParamsToConf('foo=N4XyA&version=0.0.1', 'foo');
+      const viewConfig1 = decodeURLParamsToConf('version=0.0.1#foo=N4XyA', 'foo');
       expect(viewConfig1).toEqual({});
 
-      const viewConfig2 = decodeURLParamsToConf('foo=N4IgbgpgTgzglgewHYgFwgAwDoCMWMgA0IADgK4BGANnAMZoBmAhlTBMVUwJ7QxoDaoJEwC2ENCFoQqrIiAAuXEuPQBhAKIAZTQGU5DOFQgAVJSsnTWWAFYxkcslCoSAFvPklUAei8QAHqIkRli0CCJeUjIwNnYoAL4AusTCYhIAUmQw8gAEMLRM7tBBCPJyWQV0mtwIZKWogpJhJMgQSHUgeQXyRVQlciRQCCR8qKAiTCQkcEgA5hLyALQ6AHLqcmBwEADuaKAAXghhaNgA7ACsxPJMUDMQdfwYhI8YCXFxxH7HxFxfIDuoOAATMQXGhAYk4kA&version=0.0.1', 'foo');
+      const viewConfig2 = decodeURLParamsToConf('version=0.0.1#foo=N4IgbgpgTgzglgewHYgFwgAwDoCMWMgA0IADgK4BGANnAMZoBmAhlTBMVUwJ7QxoDaoJEwC2ENCFoQqrIiAAuXEuPQBhAKIAZTQGU5DOFQgAVJSsnTWWAFYxkcslCoSAFvPklUAei8QAHqIkRli0CCJeUjIwNnYoAL4AusTCYhIAUmQw8gAEMLRM7tBBCPJyWQV0mtwIZKWogpJhJMgQSHUgeQXyRVQlciRQCCR8qKAiTCQkcEgA5hLyALQ6AHLqcmBwEADuaKAAXghhaNgA7ACsxPJMUDMQdfwYhI8YCXFxxH7HxFxfIDuoOAATMQXGhAYk4kA', 'foo');
       expect(viewConfig2).toEqual(fakeConfig);
     });
   });
