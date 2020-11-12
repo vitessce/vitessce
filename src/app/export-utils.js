a8dc9f0d06995bd43759f962333d23ac7113f5a9
ilan-gold <ilanbassgold@gmail.com>
2020-11-12 15:49:41 -0500

    Browser sniff for max length.

--- src/app/export-utils.js
+++ src/app/export-utils.js
@@ -1,3 +1,4 @@
+import Bowser from 'bowser';
 import { compressToEncodedURIComponent, decompressFromEncodedURIComponent } from 'lz-string';
 
 const CURRENT_VERSION = '0.0.1';
@@ -5,17 +6,54 @@ const VITESSCE_CONF_QUERY_STRING = 'vitessce_conf';
 const VERSION_QUERY_STRING = 'version';
 const LENGTH_QUERY_STRING = 'conf_length';
 
+function sniffBrowser() {
+  const { browser } = Bowser.parse(window.navigator.userAgent);
+  return browser.name;
+}
+
+// https://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url-in-different-browsers
+const MAX_BROWSER_URL_LENGTHS = {
+  Chrome: 32779,
+  'Internet Explorer': 2047,
+  Edge: 2047,
+  Safari: 65000,
+  Firefox: 65000,
+
+};
+
 /**
  * Encode a configuration as url params with a version and an lz-compressed conf.
- * @param {Object} conf Previous scope names.
- * @param {Object} confParameter The parameter to use instead of vitessce_conf for
+ * @param {Object} params
+ * @param {Object} params.conf Previous scope names.
+ * @param {Object} params.baseUrl The URL to which the params should be appended.
+ * @param {function} params.onOverMaximumUrlLength Callback for when new url is over max length for your browser (optional).
+ * @param {Object} params.confParameter The parameter to use instead of vitessce_conf for
  * the view config in the URL (optional).
  * @returns {string} The URL params as a string like version=0.0.1#vitessce_conf=fksdfasdfjkl
  */
-export function encodeConfAsURLParams(conf, confParameter) {
+export function encodeConfInUrl({
+  conf,
+  baseUrl,
+  onOverMaximumUrlLength = () => { },
+  confParameter,
+}) {
   const compressedConf = compressToEncodedURIComponent(JSON.stringify(conf));
-  const params = `${LENGTH_QUERY_STRING}=${compressedConf.length}&${VERSION_QUERY_STRING}=${CURRENT_VERSION}#${confParameter || VITESSCE_CONF_QUERY_STRING}=${compressedConf}`;
-  return params;
+  const newUrl = `${baseUrl}?${LENGTH_QUERY_STRING}=${compressedConf.length}&${VERSION_QUERY_STRING}=${CURRENT_VERSION}#${confParameter || VITESSCE_CONF_QUERY_STRING}=${compressedConf}`;
+  const browser = sniffBrowser();
+  const maxLength = MAX_BROWSER_URL_LENGTHS[browser];
+  if (newUrl.length > maxLength) {
+    let message;
+    if (newUrl.length < MAX_BROWSER_URL_LENGTHS.Firefox) {
+      message = `URL is too long to be used in current browser ${browser} but will work in Firefox or Safari.`;
+    } else if (newUrl.length < MAX_BROWSER_URL_LENGTHS.Chrome) {
+      message = `URL is too long to be used in current browser ${browser} but will work in Firefox, Chrome, or Safari.`;
+    } else if (newUrl.length > MAX_BROWSER_URL_LENGTHS.Firefox) {
+      message = 'URL is too long to be used reliably in modern browsers.';
+    }
+    console.error(message);
+    onOverMaximumUrlLength(message);
+  }
+  return newUrl;
 }
 
 /**
--- src/app/export-utils.test.js
+++ src/app/export-utils.test.js
@@ -1,6 +1,6 @@
 import expect from 'expect';
 import {
-  encodeConfAsURLParams,
+  encodeConfInUrl,
   decodeURLParamsToConf,
 } from './export-utils';
 
@@ -34,38 +34,60 @@ const fakeConfig = {
   ],
 };
 
-describe('src/app/export-utils.js', () => {
-  describe('encodeConfAsURLParams', () => {
-    it('encodes a view config for a URL', () => {
-      const urlParams1 = encodeConfAsURLParams({});
-      expect(urlParams1).toEqual('version=0.0.1#vitessce_conf=N4XyA');
+function makeLongString(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
-      const urlParams2 = encodeConfAsURLParams(fakeConfig);
-      expect(urlParams2).toEqual('version=0.0.1#vitessce_conf=N4IgbgpgTgzglgewHYgFwgAwDoCMWMgA0IADgK4BGANnAMZoBmAhlTBMVUwJ7QxoDaoJEwC2ENCFoQqrIiAAuXEuPQBhAKIAZTQGU5DOFQgAVJSsnTWWAFYxkcslCoSAFvPklUAei8QAHqIkRli0CCJeUjIwNnYoAL4AusTCYhIAUmQw8gAEMLRM7tBBCPJyWQV0mtwIZKWogpJhJMgQSHUgeQXyRVQlciRQCCR8qKAiTCQkcEgA5hLyALQ6AHLqcmBwEADuaKAAXghhaNgA7ACsxPJMUDMQdfwYhI8YCXFxxH7HxFxfIDuoOAATMQXGhAYk4kA');
+describe('src/app/export-utils.js', () => {
+  describe('encodeConfInUrl', () => {
+    it('encodes a view config for a URL', () => {
+      const urlParams1 = encodeConfInUrl({ conf: {}, baseUrl: 'https://example.com' });
+      expect(urlParams1).toEqual('https://example.com?conf_length=5&version=0.0.1#vitessce_conf=N4XyA');
+
+      const urlParams2 = encodeConfInUrl({ conf: fakeConfig, baseUrl: 'https://example.com' });
+      expect(urlParams2).toEqual('https://example.com?conf_length=327&version=0.0.1#vitessce_conf=N4IgbgpgTgzglgewHYgFwgAwDoCMWMgA0IADgK4BGANnAMZoBmAhlTBMVUwJ7QxoDaoJEwC2ENCFoQqrIiAAuXEuPQBhAKIAZTQGU5DOFQgAVJSsnTWWAFYxkcslCoSAFvPklUAei8QAHqIkRli0CCJeUjIwNnYoAL4AusTCYhIAUmQw8gAEMLRM7tBBCPJyWQV0mtwIZKWogpJhJMgQSHUgeQXyRVQlciRQCCR8qKAiTCQkcEgA5hLyALQ6AHLqcmBwEADuaKAAXghhaNgA7ACsxPJMUDMQdfwYhI8YCXFxxH7HxFxfIDuoOAATMQXGhAYk4kA');
+    });
+
+    it('Calls onOverMaximumUrlLength for conf too long', () => {
+      let message;
+      // eslint-disable-next-line no-return-assign
+      const onOverMaximumUrlLength = warning => message = warning;
+      encodeConfInUrl({ conf: { foo: makeLongString(65000) }, baseUrl: 'https://example.com', onOverMaximumUrlLength });
+      expect(message).toEqual('URL is too long to be used reliably in modern browsers.');
+
+      encodeConfInUrl({ conf: { foo: makeLongString(37000) }, baseUrl: 'https://example.com', onOverMaximumUrlLength });
+      expect(message).toEqual('URL is too long to be used in current browser Chrome but will work in Firefox or Safari.');
     });
 
     it('decodes a view config for a URL', () => {
-      const viewConfig1 = decodeURLParamsToConf('version=0.0.1#vitessce_conf=N4XyA');
+      const viewConfig1 = decodeURLParamsToConf('conf_length=5&version=0.0.1#vitessce_conf=N4XyA');
       expect(viewConfig1).toEqual({});
 
-      const viewConfig2 = decodeURLParamsToConf('version=0.0.1#vitessce_conf=N4IgbgpgTgzglgewHYgFwgAwDoCMWMgA0IADgK4BGANnAMZoBmAhlTBMVUwJ7QxoDaoJEwC2ENCFoQqrIiAAuXEuPQBhAKIAZTQGU5DOFQgAVJSsnTWWAFYxkcslCoSAFvPklUAei8QAHqIkRli0CCJeUjIwNnYoAL4AusTCYhIAUmQw8gAEMLRM7tBBCPJyWQV0mtwIZKWogpJhJMgQSHUgeQXyRVQlciRQCCR8qKAiTCQkcEgA5hLyALQ6AHLqcmBwEADuaKAAXghhaNgA7ACsxPJMUDMQdfwYhI8YCXFxxH7HxFxfIDuoOAATMQXGhAYk4kA');
+      const viewConfig2 = decodeURLParamsToConf('conf_length=327&version=0.0.1#vitessce_conf=N4IgbgpgTgzglgewHYgFwgAwDoCMWMgA0IADgK4BGANnAMZoBmAhlTBMVUwJ7QxoDaoJEwC2ENCFoQqrIiAAuXEuPQBhAKIAZTQGU5DOFQgAVJSsnTWWAFYxkcslCoSAFvPklUAei8QAHqIkRli0CCJeUjIwNnYoAL4AusTCYhIAUmQw8gAEMLRM7tBBCPJyWQV0mtwIZKWogpJhJMgQSHUgeQXyRVQlciRQCCR8qKAiTCQkcEgA5hLyALQ6AHLqcmBwEADuaKAAXghhaNgA7ACsxPJMUDMQdfwYhI8YCXFxxH7HxFxfIDuoOAATMQXGhAYk4kA');
       expect(viewConfig2).toEqual(fakeConfig);
     });
   });
-  describe('encodeConfAsURLParams with custom param', () => {
+  describe('encodeConfInUrl with custom param', () => {
     it('encodes a view config for a URL', () => {
-      const urlParams1 = encodeConfAsURLParams({}, 'foo');
-      expect(urlParams1).toEqual('version=0.0.1#foo=N4XyA');
+      const urlParams1 = encodeConfInUrl({ conf: {}, baseUrl: 'https://example.com', confParameter: 'foo' });
+      expect(urlParams1).toEqual("https://example.com?conf_length=5&version=0.0.1#foo=N4XyA");
 
-      const urlParams2 = encodeConfAsURLParams(fakeConfig, 'foo');
-      expect(urlParams2).toEqual('version=0.0.1#foo=N4IgbgpgTgzglgewHYgFwgAwDoCMWMgA0IADgK4BGANnAMZoBmAhlTBMVUwJ7QxoDaoJEwC2ENCFoQqrIiAAuXEuPQBhAKIAZTQGU5DOFQgAVJSsnTWWAFYxkcslCoSAFvPklUAei8QAHqIkRli0CCJeUjIwNnYoAL4AusTCYhIAUmQw8gAEMLRM7tBBCPJyWQV0mtwIZKWogpJhJMgQSHUgeQXyRVQlciRQCCR8qKAiTCQkcEgA5hLyALQ6AHLqcmBwEADuaKAAXghhaNgA7ACsxPJMUDMQdfwYhI8YCXFxxH7HxFxfIDuoOAATMQXGhAYk4kA');
+      const urlParams2 = encodeConfInUrl({ conf: fakeConfig, baseUrl: 'https://example.com', confParameter: 'foo' });
+      expect(urlParams2).toEqual('https://example.com?conf_length=327&version=0.0.1#foo=N4IgbgpgTgzglgewHYgFwgAwDoCMWMgA0IADgK4BGANnAMZoBmAhlTBMVUwJ7QxoDaoJEwC2ENCFoQqrIiAAuXEuPQBhAKIAZTQGU5DOFQgAVJSsnTWWAFYxkcslCoSAFvPklUAei8QAHqIkRli0CCJeUjIwNnYoAL4AusTCYhIAUmQw8gAEMLRM7tBBCPJyWQV0mtwIZKWogpJhJMgQSHUgeQXyRVQlciRQCCR8qKAiTCQkcEgA5hLyALQ6AHLqcmBwEADuaKAAXghhaNgA7ACsxPJMUDMQdfwYhI8YCXFxxH7HxFxfIDuoOAATMQXGhAYk4kA');
     });
 
     it('decodes a view config for a URL', () => {
-      const viewConfig1 = decodeURLParamsToConf('version=0.0.1#foo=N4XyA', 'foo');
+      const viewConfig1 = decodeURLParamsToConf('conf_length=5&version=0.0.1#foo=N4XyA', 'foo');
       expect(viewConfig1).toEqual({});
 
-      const viewConfig2 = decodeURLParamsToConf('version=0.0.1#foo=N4IgbgpgTgzglgewHYgFwgAwDoCMWMgA0IADgK4BGANnAMZoBmAhlTBMVUwJ7QxoDaoJEwC2ENCFoQqrIiAAuXEuPQBhAKIAZTQGU5DOFQgAVJSsnTWWAFYxkcslCoSAFvPklUAei8QAHqIkRli0CCJeUjIwNnYoAL4AusTCYhIAUmQw8gAEMLRM7tBBCPJyWQV0mtwIZKWogpJhJMgQSHUgeQXyRVQlciRQCCR8qKAiTCQkcEgA5hLyALQ6AHLqcmBwEADuaKAAXghhaNgA7ACsxPJMUDMQdfwYhI8YCXFxxH7HxFxfIDuoOAATMQXGhAYk4kA', 'foo');
+      const viewConfig2 = decodeURLParamsToConf('conf_length=327&version=0.0.1#foo=N4IgbgpgTgzglgewHYgFwgAwDoCMWMgA0IADgK4BGANnAMZoBmAhlTBMVUwJ7QxoDaoJEwC2ENCFoQqrIiAAuXEuPQBhAKIAZTQGU5DOFQgAVJSsnTWWAFYxkcslCoSAFvPklUAei8QAHqIkRli0CCJeUjIwNnYoAL4AusTCYhIAUmQw8gAEMLRM7tBBCPJyWQV0mtwIZKWogpJhJMgQSHUgeQXyRVQlciRQCCR8qKAiTCQkcEgA5hLyALQ6AHLqcmBwEADuaKAAXghhaNgA7ACsxPJMUDMQdfwYhI8YCXFxxH7HxFxfIDuoOAATMQXGhAYk4kA', 'foo');
       expect(viewConfig2).toEqual(fakeConfig);
     });
   });
