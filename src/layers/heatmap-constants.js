35cf21bae1e064c98be2541672b102d765a3707e
ilan-gold <ilanbassgold@gmail.com>
2020-11-21 12:54:36 -0500

    Remove shader aggregation

--- src/layers/heatmap-constants.js
+++ src/layers/heatmap-constants.js
@@ -1,8 +1,6 @@
 import GL from '@luma.gl/constants';
 
 export const TILE_SIZE = 2048;
-export const MIN_ROW_AGG = 1;
-export const MAX_ROW_AGG = 16;
 
 export const COLOR_BAR_SIZE = 20;
 export const AXIS_LABEL_TEXT_SIZE = 9;
@@ -17,8 +15,9 @@ export const THEME_TO_TEXT_COLOR = {
 export const AXIS_FONT_FAMILY = "-apple-system, 'Helvetica Neue', Arial, sans-serif";
 
 export const PIXELATED_TEXTURE_PARAMETERS = {
-  // NEAREST for integer data to prevent interpolation.
-  [GL.TEXTURE_MIN_FILTER]: GL.NEAREST,
+  // NEAREST afor integer data to prevent interpolation nd NEAREST_MIPMAP_LINEAR
+  // to give LINEAR downsampled MIPMAP(basically an image pyramid linearly downsampled).
+  [GL.TEXTURE_MIN_FILTER]: GL.NEAREST_MIPMAP_LINEAR,
   [GL.TEXTURE_MAG_FILTER]: GL.NEAREST,
   // CLAMP_TO_EDGE to remove tile artifacts.
   [GL.TEXTURE_WRAP_S]: GL.CLAMP_TO_EDGE,
