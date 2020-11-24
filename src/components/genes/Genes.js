33298f68acd807ce99e2dd7a26d4f91044a1b638
ilan-gold <ilanbassgold@gmail.com>
2020-11-24 12:51:21 -0500

    Add searchable genes.

--- src/components/genes/Genes.js
+++ src/components/genes/Genes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SelectableTable } from '../selectable-table/index';
 
 export default function Genes(props) {
@@ -9,28 +9,50 @@ export default function Genes(props) {
     setGeneSelection,
   } = props;
 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState(geneList);
+
+  useEffect(() => {
+    const results = geneList
+      .filter(gene => gene.toLowerCase().includes(searchTerm.toLowerCase()));
+    setSearchResults(results);
+  }, [searchTerm, geneList]);
+
   function onChange(selection) {
     if (setGeneSelection && selection && selection.name) {
       setGeneSelection([selection.name]);
     }
   }
 
-  const data = geneList
+  const data = searchResults
     .filter(gene => (geneFilter ? geneFilter.includes(gene) : true))
     .sort((a, b) => a.localeCompare(b))
     .map(
       gene => ({ name: gene, value: (geneSelection ? geneSelection.includes(gene) : false) }),
     );
 
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
-    <SelectableTable
-      columns={['name']}
-      data={data}
-      idKey="name"
-      valueKey="value"
-      onChange={onChange}
-      allowUncheck={false}
-      showTableHead={false}
-    />
+    <>
+      <input
+        className="search-bar"
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <SelectableTable
+        columns={['name']}
+        data={data}
+        idKey="name"
+        valueKey="value"
+        onChange={onChange}
+        allowUncheck={false}
+        showTableHead={false}
+      />
+    </>
   );
 }
