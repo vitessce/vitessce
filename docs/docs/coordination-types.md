dcc1162cddc40786c8f58be98df8c154ecc6bacd
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-12-18 16:50:08 -0500

    Docs

--- docs/docs/coordination-types.md
+++ docs/docs/coordination-types.md
@@ -4,3 +4,45 @@ title: Coordination Types
 sidebar_label: Coordination Types
 slug: /coordination-types
 ---
+
+## Overview
+
+Vitessce defines several coordination types upon which views can be coordinated via the view config.
+
+### `dataset`
+### `embeddingType`
+### `embeddingZoom`
+### `embeddingRotation`
+### `embeddingTargetX`
+### `embeddingTargetY`
+### `embeddingTargetZ`
+### `embeddingCellSetPolygonsVisible`
+### `embeddingCellSetLabelsVisible`
+### `embeddingCellSetLabelSize`
+### `embeddingCellRadius`
+### `spatialZoom`
+### `spatialRotation`
+### `spatialTargetX`
+### `spatialTargetY`
+### `spatialTargetZ`
+### `heatmapZoomX`
+### `heatmapZoomY`
+### `heatmapTargetX`
+### `heatmapTargetY`
+### `cellFilter`
+### `cellHighlight`
+### `cellSetSelection`
+### `cellSetHighlight`
+### `cellSetColor`
+### `geneFilter`
+### `geneHighlight`
+### `geneSelection`
+### `geneExpressionColormap`
+### `geneExpressionColormapRange`
+### `cellColorEncoding`
+### `spatialLayers`
+### `genomicZoom`
+### `genomicTargetX`
+### `genomicTargetY`
+### `additionalCellSets`
+### `moleculeHighlight`
--- docs/src/pages/app.js
+++ docs/src/pages/app.js
@@ -1,20 +1,69 @@
-import React from 'react';
-import clsx from 'clsx';
+import React, { useCallback, useState } from 'react';
 import Layout from '@theme/Layout';
+import useThemeContext from '@theme/hooks/useThemeContext';
+import { useDropzone } from 'react-dropzone';
+import { ControlledEditor } from '@monaco-editor/react';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 
+const baseConfig = `{
+  "name": "My config",
+  "version": "1.0.0",
+  "initStrategy": "auto",
+  "datasets": [],
+  "layout": [],
+  "coordinationSpace": {}
+}`;
+
+function ThemedControlledEditor(props) {
+  const { isDarkTheme } = useThemeContext();
+  return <ControlledEditor
+    {...props}
+    theme={(isDarkTheme ? "dark" : "light")}
+  />
+}
+
 function App() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const [validConfig, setValidConfig] = useState(null);
+  const [pendingConfig, setPendingConfig] = useState(baseConfig);
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+    console.log(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop, maxFiles: 1});
+
   return (
     <Layout
       title="App"
-      description="Description will go into a meta tag in <head />">
+      description="Use Vitessce with a custom configuration.">
       <main>
-        <section>App</section>
+        <div {...getRootProps()} className={styles.dropzone}>
+          <input {...getInputProps()} className={styles.dropzoneInfo} />
+          {
+            isDragActive ?
+              <p>Drop the file here ...</p> :
+              <p>Drag &amp; drop a view config as a JSON file</p>
+          }
+        </div>
+        <div className={styles.dropzoneOr}>OR</div>
+        <div className={styles.viewConfigEditor}>
+          <p className={styles.viewConfigEditorInfo}>Use the JSON view config editor below</p>
+          <ThemedControlledEditor
+            value={pendingConfig}
+            onChange={(event, value) => setPendingConfig(value)}
+            height="500px"
+            language="json"
+            options={{
+              fontSize: 14,
+              minimap: {
+                enabled: false,
+              },
+            }}
+          />
+          <button className={styles.viewConfigGo}>Go</button>
+        </div>
       </main>
     </Layout>
   );
