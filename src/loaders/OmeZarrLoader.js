b314cb197f48d87f1edb74f52e7818ffacd85d66
William Moore <w.moore@dundee.ac.uk>
2020-12-23 20:53:16 +0000

    Use code from vizarr to load OME Zarr

--- src/loaders/OmeZarrLoader.js
+++ src/loaders/OmeZarrLoader.js
@@ -1,25 +1,79 @@
-import { createZarrLoader } from '@hms-dbmi/viv';
+import { ZarrLoader } from '@hms-dbmi/viv';
+import { openArray, HTTPStore } from 'zarr';
 import rasterSchema from '../schemas/raster.schema.json';
 import AbstractLoader from './AbstractLoader';
 import { LoaderFetchError, AbstractLoaderError } from './errors';
 
-async function initLoader(imageData) {
-  const {
-    url, metadata,
-  } = imageData;
+async function openMultiResolutionData(store, rootAttrs) {
+  let resolutions = ['0'];
+  if ('multiscales' in rootAttrs) {
+    const { datasets } = rootAttrs.multiscales[0];
+    resolutions = datasets.map(d => d.path);
+  }
+  const promises = resolutions.map(path => openArray({ store, path }));
+  const data = await Promise.all(promises);
+  return data;
+}
 
-  const { dimensions, isPyramid, transform } = metadata || {};
-  const { scale = 0, translate = { x: 0, y: 0 } } = transform;
-  const loader = await createZarrLoader({
-    url, dimensions, isPyramid, scale, translate,
-  });
-  return loader;
+function createLoader(dataArr, imageData) {
+  // TODO: There should be a much better way to do this.
+  // If base image is small, we don't need to fetch data for the
+  // top levels of the pyramid. For large images, the tile sizes (chunks)
+  // will be the same size for x/y. We check the chunksize here for this edge case.
+  const base = dataArr[0];
+  const { chunks } = base;
+  const [tileHeight, tileWidth] = chunks.slice(-2);
+  // TODO: Need function to trim pyramidal levels that aren't chunked w/ even tile sizes.
+  // Lowest resolution doesn't need to have square chunks, but all others do.
+  const data = dataArr.length === 1 || tileHeight !== tileWidth ? base : dataArr;
+  // need to make dimensions to use ZarrLoader, but not necessary
+  const dimensions = [
+    {
+      field: 't',
+      type: 'ordinal',
+      values: ['0'],
+    },
+    {
+      field: 'channel',
+      type: 'nominal',
+      values: imageData.metadata.channelNames,
+    },
+    {
+      field: 'z',
+      type: 'ordinal',
+      values: ['0'],
+    },
+    {
+      field: 'y',
+      type: 'quantitative',
+      values: [],
+    },
+    {
+      field: 'x',
+      type: 'quantitative',
+      values: [],
+    },
+  ];
+  return new ZarrLoader({ data, dimensions });
 }
 
 export default class OmeZarrLoader extends AbstractLoader {
   constructor(params) {
     super(params);
     this.schema = rasterSchema;
+  }
+
+  async initLoader(imageData) {
+    const {
+      url,
+    } = imageData;
+
+    this.store = new HTTPStore(url);
+    const rootAttrs = (await this.getJson('.zattrs'));
+    const data = await openMultiResolutionData(this.store, rootAttrs);
+    const loader = createLoader(data, imageData);
+
+    return loader;
   }
 
   async loadZattrs() {
@@ -50,8 +104,6 @@ export default class OmeZarrLoader extends AbstractLoader {
       return Promise.reject(payload);
     }
 
-    const channelNames = payload.data.omero.channels.map(ch => ch.label);
-
     const renderLayers = undefined;
     const urls = [[this.url, 'Image']];
 
@@ -60,33 +112,7 @@ export default class OmeZarrLoader extends AbstractLoader {
       url: this.url,
       type: 'ome-zarr',
       metadata: {
-        dimensions: [
-          {
-            field: 't',
-            type: 'ordinal',
-            values: ['0'],
-          },
-          {
-            field: 'channel',
-            type: 'nominal',
-            values: channelNames,
-          },
-          {
-            field: 'z',
-            type: 'ordinal',
-            values: ['0'],
-          },
-          {
-            field: 'y',
-            type: 'quantitative',
-            values: null,
-          },
-          {
-            field: 'x',
-            type: 'quantitative',
-            values: null,
-          },
-        ],
+        channelNames: payload.data.omero.channels.map(ch => ch.label),
         isPyramid: true,
         transform: {
           translate: {
@@ -102,7 +128,7 @@ export default class OmeZarrLoader extends AbstractLoader {
       {
         ...image,
         loaderCreator: async () => {
-          const loader = await initLoader(image);
+          const loader = await this.initLoader(image);
           return loader;
         },
       },
