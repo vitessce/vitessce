64566b03a53e9a3fa99e793d388726fee79ebff7
mkeller <7525285+keller-mark@users.noreply.github.com>
2021-01-08 00:28:16 -0500

    Comments

--- docs/src/theme/Toggle/index.js
+++ docs/src/theme/Toggle/index.js
@@ -1,15 +1,7 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
 import React from 'react';
 import clsx from 'clsx';
 import Toggle from 'react-toggle';
-import { useThemeConfig } from '@docusaurus/theme-common';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-
 
 import styles from './styles.module.css';
 
