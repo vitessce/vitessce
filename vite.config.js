dd9393bc7c2dbbf6914e89457d4b925e4a210108
mkeller <7525285+keller-mark@users.noreply.github.com>
2021-02-12 16:55:11 -0500

    Remove unnecessary vite plugin functions. GLSL shader imports not working

--- vite.config.js
+++ vite.config.js
@@ -3,39 +3,6 @@ import svgr from '@svgr/rollup';
 import inject from '@rollup/plugin-inject';
 import path from 'path';
 
-/* 
-* Custom Rollup Plugin for installing geotiff.js
-*
-* vizarr doesn't use geotiff component of viv.
-* This plugin just creates an empty shim for 
-* top-level imports in viv during install by snowpack.
-*/
-
-function resolveGeotiff() {
-  return {
-    name: 'resolve-empty-geotiff',
-    async load(id) {
-      if (!id.includes('geotiff.js')) return;
-      return `
-      export const fromBlob = '';
-      export const fromUrl = '';
-      `;
-    },
-  }
-}
-
-function resolveLoaders() {
-  return {
-    name: 'resolve-empty-loaders-gl',
-    async load(id) {
-      if (!id.includes('require-utils.node')) return;
-      return `
-      export const node = '';
-      `;
-    }
-  }
-}
-
 
 /**
  * https://vitejs.dev/config/
@@ -46,8 +13,6 @@ export default {
     inject({
       global: path.resolve( 'global.js' )
     }),
-    resolveLoaders(),
-    resolveGeotiff(),
     svgr(),
     reactRefresh()
   ]
