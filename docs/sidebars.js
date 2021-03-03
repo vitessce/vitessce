a5d6ddea0d4dcddd00ee70a32ead696f26ae8f51
mkeller <7525285+keller-mark@users.noreply.github.com>
2021-03-03 17:06:01 -0500

    Rearrange pages in sidebar

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,10 +1,9 @@
 module.exports = {
   someSidebar: {
-    'Getting Started': ['introduction', 'installation'],
-    'Features': ['components', 'coordination', 'coordination-types', 'data-types-file-types'],
+    'Getting Started': ['introduction', 'components', 'coordination', 'coordination-types', 'data-types-file-types'],
     'View Configs': ['view-config-json', 'view-config-js'],
     'Data Preparation': ['data-file-types', 'data-hosting'],
-    'React Components': ['component-overview', 'component-vitessce', 'component-scatterplot', 'develop-component'],
     'Tutorials': ['tutorial-overview', 'tutorial-visium'],
+    'React Components': ['component-overview', 'installation', 'component-vitessce', 'develop-component'],
   },
 };
