77598c51be8ce9e84c72de115c5bf3808375fbe9
mkeller <7525285+keller-mark@users.noreply.github.com>
2021-03-03 16:47:06 -0500

    Remove Vitessce component from home page of docs

--- docs/src/pages/_Home.js
+++ docs/src/pages/_Home.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import clsx from 'clsx';
-import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import { linnarssonWithRorb } from '../../../src/demo/view-configs/codeluppi';
-import ThemedVitessce from './_ThemedVitessce';
 import ScreenshotImage from './_ScreenshotImage';
 import styles from './styles.module.css';
-
-const configJson = linnarssonWithRorb;
 
 const features = [
   {
@@ -55,41 +50,34 @@ function Feature({imageUrl, title, description}) {
 export default function Home() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
-  const height = 600;
 
   return (
     <>
-        <header className={clsx('hero hero--primary', styles.heroBanner)}>
-            <div className={clsx("container", styles.heroContainer)}>
-                <h1 className="hero__title">{siteConfig.title}</h1>
-                <p className="hero__subtitle">{siteConfig.tagline}</p>
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className={clsx("container", styles.heroContainer)}>
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+        </div>
+      </header>
+      <main>
+        <section className={styles.homeScreenshots}>
+          <ScreenshotImage filename="web.png" alt="Vitessce as a web application" />
+          <ScreenshotImage filename="hubmap.png" alt="Vitessce as an embedded web component (HuBMAP Portal)" />
+          <ScreenshotImage filename="jupyterlab.png" alt="Vitessce as an ipywidget in JupyterLab" />
+          <ScreenshotImage filename="rstudio.png" alt="Vitessce as an htmlwidget in RStudio" />
+        </section>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
             </div>
-        </header>
-        <main>
-            <section className={styles.homeComponent} style={{ height: `${height}px` }}>
-                <ThemedVitessce
-                    height={height}
-                    config={configJson}
-                />
-            </section>
-            <section className={styles.homeScreenshots}>
-              <ScreenshotImage filename="web.png" alt="Vitessce as a web application" />
-              <ScreenshotImage filename="hubmap.png" alt="Vitessce as an embedded web component (HuBMAP Portal)" />
-              <ScreenshotImage filename="jupyterlab.png" alt="Vitessce as an ipywidget in JupyterLab" />
-              <ScreenshotImage filename="rstudio.png" alt="Vitessce as an htmlwidget in RStudio" />
-            </section>
-            {features && features.length > 0 && (
-                <section className={styles.features}>
-                    <div className="container">
-                    <div className="row">
-                        {features.map((props, idx) => (
-                        <Feature key={idx} {...props} />
-                        ))}
-                    </div>
-                    </div>
-                </section>
-            )}
-        </main>
+          </section>
+        )}
+      </main>
     </>
   );
 }
