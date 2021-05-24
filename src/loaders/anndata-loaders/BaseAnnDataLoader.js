c993ace09754f8aee7700511ce27f716eed6b4c5
ilan-gold <ilanbassgold@gmail.com>
2021-05-24 15:38:28 -0400

    Move out of class

--- src/loaders/anndata-loaders/BaseAnnDataLoader.js
+++ src/loaders/anndata-loaders/BaseAnnDataLoader.js
@@ -11,41 +11,40 @@ const readFloat32FromUint8 = (bytes) => {
 const HEADER_LENGTH = 4;
 
 /**
+   * Method for decoding text arrays from zarr.
+   * Largerly a port of https://github.com/zarr-developers/numcodecs/blob/2c1aff98e965c3c4747d9881d8b8d4aad91adb3a/numcodecs/vlen.pyx#L135-L178
+   * @returns {string[]} An array of strings.
+   */
+function parseVlenUtf8(buffer) {
+  const decoder = new TextDecoder();
+  let data = 0;
+  const dataEnd = data + buffer.length;
+  const length = readFloat32FromUint8(buffer.slice(data, HEADER_LENGTH));
+  if (buffer.length < HEADER_LENGTH) {
+    throw new Error('corrupt buffer, missing or truncated header');
+  }
+  data += HEADER_LENGTH;
+  const output = new Array(length);
+  for (let i = 0; i < length; i += 1) {
+    if (data + 4 > dataEnd) {
+      throw new Error('corrupt buffer, data seem truncated');
+    }
+    const l = readFloat32FromUint8(buffer.slice(data, data + 4));
+    data += 4;
+    if (data + l > dataEnd) {
+      throw new Error('corrupt buffer, data seem truncated');
+    }
+    output[i] = decoder.decode(buffer.slice(data, data + l));
+    data += l;
+  }
+  return output;
+}
+
+/**
  * A base AnnData loader which has all shared methods for more comlpex laoders,
  * like loading cell names and ids. It inherits from AbstractLoader.
  */
 export default class BaseAnnDataLoader extends AbstractZarrLoader {
-  /**
-   * Class method for decoding text arrays from zarr.
-   * Largerly a port of https://github.com/zarr-developers/numcodecs/blob/2c1aff98e965c3c4747d9881d8b8d4aad91adb3a/numcodecs/vlen.pyx#L135-L178
-   * @returns {string[]} An array of strings.
-   */
-  // eslint-disable-next-line class-methods-use-this
-  parseVlenUtf8(buffer) {
-    const decoder = new TextDecoder();
-    let data = 0;
-    const dataEnd = data + buffer.length;
-    const length = readFloat32FromUint8(buffer.slice(data, HEADER_LENGTH));
-    if (buffer.length < HEADER_LENGTH) {
-      throw new Error('corrupt buffer, missing or truncated header');
-    }
-    data += HEADER_LENGTH;
-    const output = new Array(length);
-    for (let i = 0; i < length; i += 1) {
-      if (data + 4 > dataEnd) {
-        throw new Error('corrupt buffer, data seem truncated');
-      }
-      const l = readFloat32FromUint8(buffer.slice(data, data + 4));
-      data += 4;
-      if (data + l > dataEnd) {
-        throw new Error('corrupt buffer, data seem truncated');
-      }
-      output[i] = decoder.decode(buffer.slice(data, data + l));
-      data += l;
-    }
-    return output;
-  }
-
   /**
    * Class method for loading cell set ids.
    * Takes the location as an argument because this is shared across objects,
@@ -115,7 +114,7 @@ export default class BaseAnnDataLoader extends AbstractZarrLoader {
           // eslint-disable-next-line no-await-in-loop
           const dbytes = await z.compressor.decode(buf);
           if (z.meta.filters[0].id === 'vlen-utf8') {
-            const text = this.parseVlenUtf8(dbytes);
+            const text = parseVlenUtf8(dbytes);
             data = data.concat(text);
           } else {
             data = data.concat(dbytes);
