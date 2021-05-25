5174a23119d83a365818c92938d4a5fab78ed231
ilan-gold <ilanbassgold@gmail.com>
2021-05-25 17:49:14 -0400

    Fix VLenUTF8 Parsing (#948)
    * Fix VLenUTF8 Parsing.
    
    * cell set polygon hack
    
    * Move out of class

--- src/loaders/anndata-loaders/BaseAnnDataLoader.js
+++ src/loaders/anndata-loaders/BaseAnnDataLoader.js
@@ -1,31 +1,50 @@
 import { openArray, KeyError } from 'zarr';
 import AbstractZarrLoader from '../AbstractZarrLoader';
 
+const readFloat32FromUint8 = (bytes) => {
+  if (bytes.length !== 4) {
+    throw new Error('readFloat32 only takes in length 4 byte buffers');
+  }
+  return new Int32Array(bytes.buffer)[0];
+};
+
+const HEADER_LENGTH = 4;
+
+/**
+   * Method for decoding text arrays from zarr.
+   * Largerly a port of https://github.com/zarr-developers/numcodecs/blob/2c1aff98e965c3c4747d9881d8b8d4aad91adb3a/numcodecs/vlen.pyx#L135-L178
+   * @returns {string[]} An array of strings.
+   */
+function parseVlenUtf8(buffer) {
+  const decoder = new TextDecoder();
+  let data = 0;
+  const dataEnd = data + buffer.length;
+  const length = readFloat32FromUint8(buffer.slice(data, HEADER_LENGTH));
+  if (buffer.length < HEADER_LENGTH) {
+    throw new Error('corrupt buffer, missing or truncated header');
+  }
+  data += HEADER_LENGTH;
+  const output = new Array(length);
+  for (let i = 0; i < length; i += 1) {
+    if (data + 4 > dataEnd) {
+      throw new Error('corrupt buffer, data seem truncated');
+    }
+    const l = readFloat32FromUint8(buffer.slice(data, data + 4));
+    data += 4;
+    if (data + l > dataEnd) {
+      throw new Error('corrupt buffer, data seem truncated');
+    }
+    output[i] = decoder.decode(buffer.slice(data, data + l));
+    data += l;
+  }
+  return output;
+}
+
 /**
  * A base AnnData loader which has all shared methods for more comlpex laoders,
  * like loading cell names and ids. It inherits from AbstractLoader.
  */
 export default class BaseAnnDataLoader extends AbstractZarrLoader {
-  /**
-   * Class method for decoding text arrays from zarr.
-   * @returns {string[]} An array of strings.
-   */
-  // eslint-disable-next-line class-methods-use-this
-  decodeTextArray(buffer) {
-    return (
-      new TextDecoder()
-        // Remove header: https://github.com/zarr-developers/numcodecs/blob/2c1aff98e965c3c4747d9881d8b8d4aad91adb3a/numcodecs/vlen.pyx#L34
-        // Should we validate? Seems unnecessary, but maybe?
-        .decode(buffer.slice(4))
-        // https://stackoverflow.com/questions/11159118/incorrect-string-value-xef-xbf-xbd-for-column
-        // for information on the right hand side of the | in the regex.
-        // eslint-disable-next-line no-control-regex
-        .replace(/[\u0000-\u001c]|�c/g, ',')
-        .split(',')
-        .filter(Boolean)
-    );
-  }
-
   /**
    * Class method for loading cell set ids.
    * Takes the location as an argument because this is shared across objects,
@@ -40,16 +59,12 @@ export default class BaseAnnDataLoader extends AbstractZarrLoader {
     }
     this.cellSets = Promise.all(
       cellSetZarrLocation.map(async (setName) => {
-        const { categories } = await this.getJson(
-          `${setName}/.zattrs`,
-        );
+        const { categories } = await this.getJson(`${setName}/.zattrs`);
         let categoriesValues;
         if (categories) {
-          const { dtype } = await this.getJson(
-            `/obs/${categories}/.zarray`,
-          );
+          const { dtype } = await this.getJson(`/obs/${categories}/.zarray`);
           if (dtype === '|O') {
-            categoriesValues = await this.getFlatTextArr(`/obs/${categories}`);
+            categoriesValues = await this.getFlatArrDecompressed(`/obs/${categories}`);
           }
         }
         const cellSetsArr = await openArray({
@@ -89,7 +104,7 @@ export default class BaseAnnDataLoader extends AbstractZarrLoader {
       path,
       mode: 'r',
     }).then(async (z) => {
-      let data = new Uint8Array();
+      let data = [];
       let item = 0;
       // eslint-disable-next-line no-constant-condition
       while (true) {
@@ -98,12 +113,12 @@ export default class BaseAnnDataLoader extends AbstractZarrLoader {
           const buf = await store.getItem(`${z.keyPrefix}${String(item)}`);
           // eslint-disable-next-line no-await-in-loop
           const dbytes = await z.compressor.decode(buf);
-          const tmp = new Uint8Array(
-            dbytes.buffer.byteLength + data.buffer.byteLength,
-          );
-          tmp.set(new Uint8Array(data.buffer), 0);
-          tmp.set(dbytes, data.buffer.byteLength);
-          data = tmp;
+          if (z.meta.filters[0].id === 'vlen-utf8') {
+            const text = parseVlenUtf8(dbytes);
+            data = data.concat(text);
+          } else {
+            data = data.concat(dbytes);
+          }
           item += 1;
         } catch (err) {
           if (err instanceof KeyError) {
@@ -112,14 +127,13 @@ export default class BaseAnnDataLoader extends AbstractZarrLoader {
           throw err;
         }
       }
-      return data;
-    });
-  }
-
-  getFlatTextArr(path) {
-    return this.getFlatArrDecompressed(path).then((data) => {
-      const text = this.decodeTextArray(data);
-      return text;
+      const {
+        meta: {
+          shape: [length],
+        },
+      } = z;
+      // truncate the filled in values
+      return data.slice(0, length);
     });
   }
 
@@ -132,7 +146,7 @@ export default class BaseAnnDataLoader extends AbstractZarrLoader {
       return this.cellNames;
     }
     this.cellNames = this.getJson('obs/.zattrs')
-      .then(({ _index }) => this.getFlatTextArr(`/obs/${_index}`));
+      .then(({ _index }) => this.getFlatArrDecompressed(`/obs/${_index}`));
     return this.cellNames;
   }
 }
