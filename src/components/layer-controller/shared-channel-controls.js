4c9baa19a3d81cb6e1037e131d6a4ee812187a27
ilan-gold <ilanbassgold@gmail.com>
2021-06-24 01:30:27 -0400

    Begin docs update.

--- src/components/layer-controller/shared-channel-controls.js
+++ src/components/layer-controller/shared-channel-controls.js
@@ -7,6 +7,7 @@ import Select from '@material-ui/core/Select';
  * Dropdown for selecting a channel.
  * @prop {function} handleChange Callback for each new selection.
  * @prop {array} channelOptions List of available selections, like ['DAPI', 'FITC', ...].
+ * @prop {boolean} disabled Whether or not the component is disabled.
  * @prop {number} selectionIndex Current numeric index of a selection.
  */
 export function ChannelSelectionDropdown({
@@ -34,6 +35,7 @@ export function ChannelSelectionDropdown({
  * Checkbox for toggling on/off of a channel.
  * @prop {string} color Current color for this channel.
  * @prop {boolean} checked Whether or not this channel is "on".
+ * @prop {boolean} disabled Whether or not the component is disabled.
  * @prop {function} toggle Callback for toggling on/off.
  */
 export function ChannelVisibilityCheckbox({
