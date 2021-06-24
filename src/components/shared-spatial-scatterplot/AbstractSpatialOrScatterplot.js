4d1bb236f13a6e444101354719f62f2f5fb0bf8e
ilan-gold <ilanbassgold@gmail.com>
2021-06-24 00:24:11 -0400

    `useDevicePixels` setting with 3d

--- src/components/shared-spatial-scatterplot/AbstractSpatialOrScatterplot.js
+++ src/components/shared-spatial-scatterplot/AbstractSpatialOrScatterplot.js
@@ -187,10 +187,10 @@ export default class AbstractSpatialOrScatterplot extends PureComponent {
     const showCellSelectionTools = this.cellsLayer !== null
       || (this.cellsEntries.length && this.cellsEntries[0][1].xy);
     const showPanTool = this.cellsLayer !== null || layerProps.findIndex(l => l.type === 'bitmask' || l.type === 'raster') >= 0;
-    // For large datasets, the visual quality takes only a small
+    // For large datasets or ray casting, the visual quality takes only a small
     // hit in exchange for much better performance by setting this to false:
     // https://deck.gl/docs/api-reference/core/deck#usedevicepixels
-    const useDevicePixels = this.cellsEntries.length < 100000;
+    const useDevicePixels = this.cellsEntries.length < 100000 && !use3d;
 
     return (
       <>
