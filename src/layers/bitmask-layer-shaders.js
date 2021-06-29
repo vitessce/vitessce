e130c3294eebc697fe9e2f8bb8e32a8d94eca5e8
ilan-gold <ilanbassgold@gmail.com>
2021-06-29 22:28:51 +0200

    Change blending.

--- src/layers/bitmask-layer-shaders.js
+++ src/layers/bitmask-layer-shaders.js
@@ -48,19 +48,32 @@ varying vec2 vTexCoord;
 vec4 sampleAndGetColor(sampler2D dataTex, vec2 coord, bool isOn){
   float sampledData = texture(dataTex, coord).r;
   vec4 hoveredColor = float(sampledData == hovered && sampledData > 0. && hovered > 0.) * vec4(0., 0., 1., 1.);
-  vec4 sampledColor = vec4(texture(colorTex, vec2(mod(sampledData, colorTexWidth) / colorTexWidth, floor(sampledData / colorTexWidth) / (colorTexHeight - 1.))).rgb, 1.);
-  return float(isOn) * (sampledColor + hoveredColor);
+  // Colors are laid out corresponding to ids in row-major order in the texture.  So if width of the texture is 10, and you want ID 25,
+  // you need coordinate (2, 4) (i.e 2 rows down, and 5 columns over indexed from 0 for a total of 25 units covered in row major order).
+  vec2 colorTexCoord = vec2(mod(sampledData, colorTexWidth) / colorTexWidth, floor(sampledData / colorTexWidth) / (colorTexHeight - 1.));
+  vec4 sampledColor = vec4(texture(colorTex, colorTexCoord).rgb, 1.);
+  return max(0., min(sampledData, 1.)) * float(isOn) * (sampledColor + hoveredColor);
 }
 
 void main() {
 
-  gl_FragColor += sampleAndGetColor(channel0, vTexCoord, channelIsOn[0]);
-  gl_FragColor += sampleAndGetColor(channel1, vTexCoord, channelIsOn[1]);
-  gl_FragColor += sampleAndGetColor(channel2, vTexCoord, channelIsOn[2]);
-  gl_FragColor += sampleAndGetColor(channel3, vTexCoord, channelIsOn[3]);
-  gl_FragColor += sampleAndGetColor(channel4, vTexCoord, channelIsOn[4]);
-  gl_FragColor += sampleAndGetColor(channel5, vTexCoord, channelIsOn[5]);
+  gl_FragColor = sampleAndGetColor(channel0, vTexCoord, channelIsOn[0]);
+
+  // If the sampled color and the currently stored color (gl_FragColor) are identical, don't blend and use the sampled color,
+  // otherwise just use the currently stored color.
+  vec4 sampledColor = sampleAndGetColor(channel1, vTexCoord, channelIsOn[1]);
+  gl_FragColor = (sampledColor == gl_FragColor || sampledColor == vec4(0.)) ? gl_FragColor : sampledColor;
+  sampledColor = sampleAndGetColor(channel2, vTexCoord, channelIsOn[2]);
+  gl_FragColor = (sampledColor == gl_FragColor || sampledColor == vec4(0.)) ? gl_FragColor : sampledColor;
+  sampledColor = sampleAndGetColor(channel3, vTexCoord, channelIsOn[3]);
+  gl_FragColor = (sampledColor == gl_FragColor || sampledColor == vec4(0.)) ? gl_FragColor : sampledColor;
+  sampledColor = sampleAndGetColor(channel4, vTexCoord, channelIsOn[4]);
+  gl_FragColor = (sampledColor == gl_FragColor || sampledColor == vec4(0.)) ? gl_FragColor : sampledColor;
+  sampledColor = sampleAndGetColor(channel5, vTexCoord, channelIsOn[5]);
+  gl_FragColor = (sampledColor == gl_FragColor || sampledColor == vec4(0.)) ? gl_FragColor : sampledColor;
+  // Apply the opacity if there is pixel data, and if the pixel data is empty i.e no segmentation, use 0 opacity.
   gl_FragColor = vec4(gl_FragColor.rgb, (gl_FragColor.rgb == vec3(0., 0., 0.)) ? 0.0 : opacity);
+
   geometry.uv = vTexCoord;
   DECKGL_FILTER_COLOR(gl_FragColor, geometry);
 }
