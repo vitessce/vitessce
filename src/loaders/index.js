56648bfe671d2d024e7f8fc67533fd13d4c7c05d
ilan-gold <ilanbassgold@gmail.com>
2021-07-08 17:23:38 +0200

    Lint

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -2,4 +2,4 @@ export { default as JsonLoader } from './JsonLoader';
 export { default as MatrixZarrLoader } from './MatrixZarrLoader';
 export { default as GenesJsonAsMatrixZarrLoader } from './GenesJsonAsMatrixZarrLoader';
 export { default as ClustersJsonAsMatrixZarrLoader } from './ClustersJsonAsMatrixZarrLoader';
-export { default as AnnDataLoaders } from './anndata-loaders';
+export { default as AnnDataLoader } from './anndata-loader';
