11dc3f500e3faa0445a13c404f89ebcf504b7c3e
ilan-gold <ilanbassgold@gmail.com>
2021-08-25 15:07:19 -0400

    Add heatmap colormaps.

--- src/layers/heatmap-bitmap-layer-shaders.js
+++ src/layers/heatmap-bitmap-layer-shaders.js
@@ -44,11 +44,21 @@ precision mediump float;
 #endif
 
 #pragma glslify: rdbu = require("glsl-colormap/rdbu")
-#pragma glslify: jet = require("glsl-colormap/jet")
-#pragma glslify: viridis = require("glsl-colormap/viridis")
-#pragma glslify: inferno = require("glsl-colormap/inferno")
-#pragma glslify: magma = require("glsl-colormap/magma")
 #pragma glslify: plasma = require("glsl-colormap/plasma")
+#pragma glslify: viridis = require("glsl-colormap/viridis")
+#pragma glslify: greys = require("glsl-colormap/greys")
+#pragma glslify: magma = require("glsl-colormap/magma")
+#pragma glslify: jet = require("glsl-colormap/jet")
+#pragma glslify: bone = require("glsl-colormap/bone")
+#pragma glslify: copper = require("glsl-colormap/copper")
+#pragma glslify: density = require("glsl-colormap/density")
+#pragma glslify: inferno = require("glsl-colormap/inferno")
+#pragma glslify: cool = require("glsl-colormap/cool")
+#pragma glslify: hot = require("glsl-colormap/hot")
+#pragma glslify: spring = require("glsl-colormap/spring")
+#pragma glslify: summer = require("glsl-colormap/summer")
+#pragma glslify: autumn = require("glsl-colormap/autumn")
+#pragma glslify: winter = require("glsl-colormap/winter")
 
 // The texture (GL.LUMINANCE & Uint8Array).
 uniform sampler2D uBitmapTexture;
