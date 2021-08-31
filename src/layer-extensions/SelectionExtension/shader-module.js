046b5d5af78086bd58d591e632781df882347513
ilan-gold <ilanbassgold@gmail.com>
2021-08-31 17:37:15 +0200

    Remove size filter.

--- src/layer-extensions/SelectionExtension/shader-module.js
+++ src/layer-extensions/SelectionExtension/shader-module.js
@@ -8,9 +8,6 @@ const inject = {
   'vs:DECKGL_FILTER_GL_POSITION': `
     position.z += (1. - isSelected) * .00005; // Add a small z offset for unselected points in the positive direction i.e into the screen.
   `,
-  // 'vs:DECKGL_FILTER_SIZE': `
-  //   size *= bool(isSelected) ? 1. : .6; // Allows changing point size based on selection.
-  // `,
   'fs:#main-start': ` // Gets rid of bad border effects (active after deck.gl 8.5): https://github.com/visgl/deck.gl/pull/6081
     float distToCenterNew = length(unitPosition) * outerRadiusPixels;
     float inCircleNew = step(distToCenterNew, outerRadiusPixels);
