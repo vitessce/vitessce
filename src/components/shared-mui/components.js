1558e95a2d4f0dd28f4e1cd5eb6814a2803b3c11
mkeller <7525285+keller-mark@users.noreply.github.com>
2021-11-01 16:38:40 -0400

    Popper to popover

--- src/components/shared-mui/components.js
+++ src/components/shared-mui/components.js
@@ -1,41 +1,10 @@
 import React, { useRef } from 'react';
 import Box from '@material-ui/core/Box';
-import Paper from '@material-ui/core/Paper';
-import Popper from '@material-ui/core/Popper';
+import Popover from '@material-ui/core/Popover';
 import IconButton from '@material-ui/core/IconButton';
 import MenuList from '@material-ui/core/MenuList';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import { styles } from './styles';
 
-function MuiPopper(props) {
-  const classes = styles();
-  const {
-    anchorEl,
-    open,
-    placement = 'bottom-start',
-    children,
-  } = props;
-  return (
-    <Popper
-      className={classes.popper}
-      open={open}
-      anchorEl={anchorEl.current}
-      placement={placement}
-    >
-      {children}
-    </Popper>
-  );
-}
-
-function MuiPaper(props) {
-  const { children } = props;
-  const classes = styles();
-  return (
-    <Paper className={classes.paper} elevation={4}>
-      {children}
-    </Paper>
-  );
-}
 
 export function MuiSpan(props) {
   const { children } = props;
@@ -49,42 +18,49 @@ export function PopperMenu(props) {
     open,
     setOpen,
     children,
-    placement,
     buttonClassName,
   } = props;
-  const anchorRef = useRef(null);
-  const classes = styles();
+  const anchorRef = useRef();
 
   const handleClick = () => {
     setOpen(prev => !prev);
   };
 
-  const handleClickAway = () => {
+  const handleClose = () => {
     setOpen(false);
   };
 
+  const id = open ? 'v-popover-menu' : undefined;
+
   return (
-    <ClickAwayListener onClickAway={handleClickAway}>
-      <Box style={{ position: 'relative' }}>
-        <IconButton
-          onClick={handleClick}
-          size="small"
-          ref={anchorRef}
-          className={buttonClassName}
-        >
-          {buttonIcon}
-        </IconButton>
-        <MuiPopper
-          className={classes.popper}
-          open={open}
-          anchorEl={anchorRef}
-          placement={placement}
-        >
-          <MuiPaper>
-            <MenuList>{children}</MenuList>
-          </MuiPaper>
-        </MuiPopper>
-      </Box>
-    </ClickAwayListener>
+    <Box style={{ position: 'relative' }}>
+      <IconButton
+        aria-describedby={id}
+        onClick={handleClick}
+        size="small"
+        ref={anchorRef}
+        className={buttonClassName}
+      >
+        {buttonIcon}
+      </IconButton>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorRef && anchorRef.current}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transitionDuration={0}
+        elevation={4}
+      >
+        <MenuList>{children}</MenuList>
+      </Popover>
+    </Box>
   );
 }
