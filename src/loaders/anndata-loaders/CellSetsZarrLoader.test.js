8c8241967981ce396d821f6bd9029dd14657bedb
mkeller <7525285+keller-mark@users.noreply.github.com>
2021-11-17 16:25:54 -0500

    Fix tests

--- src/loaders/anndata-loaders/CellSetsZarrLoader.test.js
+++ src/loaders/anndata-loaders/CellSetsZarrLoader.test.js
@@ -12,6 +12,7 @@ describe('loaders/CellSetsZarrLoader', () => {
           ['CD19+', 'CD19-', 'CD19-', 'Retinal bipolar neuron'],
         ],
       ],
+      [undefined]
     ];
     const options = [
       { groupName: 'Subclass Levels', setName: ['obs/L1', 'obs/L2', 'obs/L3'] },
@@ -77,6 +78,7 @@ describe('loaders/CellSetsZarrLoader', () => {
       [
         ['Immune', 'Immune', 'Immune', 'Neuron'],
       ],
+      [undefined]
     ];
     const options = [
       { groupName: 'Subclass Level 1', setName: 'obs/L1' },
@@ -103,6 +105,49 @@ describe('loaders/CellSetsZarrLoader', () => {
               name: 'Neuron',
               set: [
                 ['cell_4', null],
+              ],
+            },
+          ],
+        },
+      ],
+    });
+  });
+
+  it('dataToCellSetsTree creates a flat tree from a single column with scores', async () => {
+    const data = [
+      ['cell_1', 'cell_2', 'cell_3', 'cell_4'],
+      [
+        ['Immune', 'Immune', 'Immune', 'Neuron'],
+      ],
+      [
+        [0.25, 0.5, 0.6, 0.1],
+      ]
+    ];
+    const options = [
+      { groupName: 'Subclass Level 1', setName: 'obs/L1' },
+    ];
+
+    const tree = dataToCellSetsTree(data, options);
+
+    expect(tree).toEqual({
+      version: '0.1.3',
+      datatype: 'cell',
+      tree: [
+        {
+          name: 'Subclass Level 1',
+          children: [
+            {
+              name: 'Immune',
+              set: [
+                ['cell_1', 0.25],
+                ['cell_2', 0.5],
+                ['cell_3', 0.6],
+              ],
+            },
+            {
+              name: 'Neuron',
+              set: [
+                ['cell_4', 0.1],
               ],
             },
           ],
