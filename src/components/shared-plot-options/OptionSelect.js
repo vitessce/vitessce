8e090691b4d1146c5f453e0e20afffcc4f901491
mkeller <7525285+keller-mark@users.noreply.github.com>
2021-11-17 10:18:45 -0500

    Update props

--- src/components/shared-plot-options/OptionSelect.js
+++ src/components/shared-plot-options/OptionSelect.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Select from '@material-ui/core/Select';
 import { useStyles } from './styles';
 
-
 export default function OptionSelect(props) {
+  const { classes: classesProp = {} } = props
   const classes = useStyles();
   return (
     <Select
       native
-      {...props}
-      classes={{ root: classes.selectRoot }}
       disableUnderline
+      {...props}
+      classes={{
+        root: classes.selectRoot,
+        ...classesProp,
+      }}
     />
   );
 }
