d1f6fae26a13e13f1e98ad3d28ad577f371ba6f3
mkeller <7525285+keller-mark@users.noreply.github.com>
2021-12-14 15:02:37 -0500

    Add custom footer, update react version

---
+++ docs/src/theme/Footer/index.js
@@ -0,0 +1,143 @@
+/* eslint-disable */
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import {useThemeConfig} from '@docusaurus/theme-common';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import isInternalUrl from '@docusaurus/isInternalUrl';
+import styles from './styles.module.css';
+import ThemedImage from '@theme/ThemedImage';
+import IconExternalLink from '@theme/IconExternalLink';
+
+import version from '../../../../src/version.json';
+
+function FooterLink({to, href, label, prependBaseUrlToHref, ...props}) {
+  const toUrl = useBaseUrl(to);
+  const normalizedHref = useBaseUrl(href, {
+    forcePrependBaseUrl: true,
+  });
+  return (
+    <Link
+      className="footer__link-item"
+      {...(href
+        ? {
+            href: prependBaseUrlToHref ? normalizedHref : href,
+          }
+        : {
+            to: toUrl,
+          })}
+      {...props}>
+      {href && !isInternalUrl(href) ? (
+        <span>
+          {label}
+          <IconExternalLink />
+        </span>
+      ) : (
+        label
+      )}
+    </Link>
+  );
+}
+
+function FooterLogo({sources, alt, width, height}) {
+  return (
+    <ThemedImage
+      className="footer__logo"
+      alt={alt}
+      sources={sources}
+      width={width}
+      height={height}
+    />
+  );
+}
+
+function Footer() {
+  const {footer} = useThemeConfig();
+  const {copyright, links = [], logo = {}} = footer || {};
+  const sources = {
+    light: useBaseUrl(logo.src),
+    dark: useBaseUrl(logo.srcDark || logo.src),
+  };
+
+  if (!footer) {
+    return null;
+  }
+
+  return (
+    <footer
+      className={clsx('footer', {
+        'footer--dark': footer.style === 'dark',
+      })}>
+      <div className="container">
+        {links && links.length > 0 && (
+          <div className="row footer__links">
+            {links.map((linkItem, i) => (
+              <div key={i} className="col footer__col">
+                {linkItem.title != null ? (
+                  <div className="footer__title">{linkItem.title}</div>
+                ) : null}
+                {linkItem.items != null &&
+                Array.isArray(linkItem.items) &&
+                linkItem.items.length > 0 ? (
+                  <ul className="footer__items">
+                    {linkItem.items.map((item, key) =>
+                      item.html ? (
+                        <li
+                          key={key}
+                          className="footer__item" // Developer provided the HTML, so assume it's safe.
+                          // eslint-disable-next-line react/no-danger
+                          dangerouslySetInnerHTML={{
+                            __html: item.html,
+                          }}
+                        />
+                      ) : (
+                        <li key={item.href || item.to} className="footer__item">
+                          <FooterLink {...item} />
+                        </li>
+                      ),
+                    )}
+                  </ul>
+                ) : null}
+              </div>
+            ))}
+          </div>
+        )}
+        {(logo || copyright) && (
+          <div className="footer__bottom text--center">
+            {logo && (logo.src || logo.srcDark) && (
+              <div className="margin-bottom--sm">
+                {logo.href ? (
+                  <Link href={logo.href} className={styles.footerLogoLink}>
+                    <FooterLogo
+                      alt={logo.alt}
+                      sources={sources}
+                      width={logo.width}
+                      height={logo.height}
+                    />
+                  </Link>
+                ) : (
+                  <FooterLogo alt={logo.alt} sources={sources} />
+                )}
+              </div>
+            )}
+            {copyright ? (
+              <div className="footer__copyright">
+                <div
+                  // eslint-disable-next-line react/no-danger
+                  dangerouslySetInnerHTML={{
+                    __html: copyright,
+                  }}
+                />
+                <p className="info-section-text">
+                  This deployment: branch=<code>{version.branch}</code>, hash=<code>{version.hash}</code>, date={version.date}
+                </p>
+              </div>
+            ) : null}
+          </div>
+        )}
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
