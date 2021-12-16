f9968c6516ec6e9e2523c7edd4a7a99fff48ba22
mkeller <7525285+keller-mark@users.noreply.github.com>
2021-12-16 08:03:15 -0500

    Try gosling

--- src/components/higlass/HiGlassLazy.js
+++ src/components/higlass/HiGlassLazy.js
@@ -1,42 +1,29 @@
-import React, {
-  useMemo, useEffect, useRef, Suspense, useState,
-} from 'react';
+/* eslint-disable */
+import React, { useRef, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import dynamicImportPolyfill from 'dynamic-import-polyfill';
-import register from 'higlass-register';
-import { ZarrMultivecDataFetcher } from 'higlass-zarr-datafetchers';
 import packageJson from '../../../package.json';
 import { createWarningComponent, asEsModule } from '../utils';
 import { useGridItemSize } from '../hooks';
-import {
-  useCoordination,
-} from '../../app/state/hooks';
+import { useCoordination } from '../../app/state/hooks';
 import { COMPONENT_COORDINATION_TYPES } from '../../app/state/coordination';
 
+import mySpec from './spec.json';
+
 const PIXI_BUNDLE_VERSION = packageJson.dependencies['window-pixi'];
-const HIGLASS_BUNDLE_VERSION = packageJson.dependencies.higlass;
+const GOSLING_BUNDLE_VERSION = '0.9.8';
 const BUNDLE_FILE_EXT = process.env.NODE_ENV === 'development' ? 'js' : 'min.js';
 const PIXI_BUNDLE_URL = `https://unpkg.com/window-pixi@${PIXI_BUNDLE_VERSION}/dist/pixi.${BUNDLE_FILE_EXT}`;
-const HIGLASS_BUNDLE_URL = `https://unpkg.com/higlass@${HIGLASS_BUNDLE_VERSION}/dist/hglib.${BUNDLE_FILE_EXT}`;
-
+const GOSLING_BUNDLE_URL = `https://unpkg.com/gosling.js@${GOSLING_BUNDLE_VERSION}/dist/gosling.js`;
 
 // Initialize the dynamic __import__() function.
 if (dynamicImportPolyfill) {
   dynamicImportPolyfill.initialize();
 }
 
-// Register the zarr-multivec plugin data fetcher.
-// References:
-// https://github.com/higlass/higlass-register
-// https://github.com/higlass/higlass-zarr-datafetchers
-register(
-  { dataFetcher: ZarrMultivecDataFetcher, config: ZarrMultivecDataFetcher.config },
-  { pluginType: 'dataFetcher' },
-);
-
 // Lazy load the HiGlass React component,
 // using dynamic imports with absolute URLs.
-const HiGlassComponent = React.lazy(() => {
+const LazyGoslingComponent = React.lazy(() => {
   if (!window.React) {
     window.React = React;
   }
@@ -47,17 +34,17 @@ const HiGlassComponent = React.lazy(() => {
     const handleImportError = (e) => {
       console.warn(e);
       resolve(asEsModule(createWarningComponent({
-        title: 'Could not load HiGlass',
-        message: 'The HiGlass scripts could not be dynamically imported.',
+        title: 'Could not load Gosling',
+        message: 'The Gosling scripts could not be dynamically imported.',
       })));
     };
       // eslint-disable-next-line no-undef
     __import__(PIXI_BUNDLE_URL).then(() => {
       // eslint-disable-next-line no-undef
-      __import__(HIGLASS_BUNDLE_URL).then(() => {
+      __import__(GOSLING_BUNDLE_URL).then(() => {
         // React.lazy promise must return an ES module with the
         // component as the default export.
-        resolve(asEsModule(window.hglib.HiGlassComponent));
+        resolve(asEsModule(window.gosling.GoslingComponent));
       }).catch(handleImportError);
     }).catch(handleImportError);
   });
@@ -78,7 +65,7 @@ const HG_SIZE = 800;
    * to pass to the TitleInfo component.
    * @prop {function} onReady A callback function to signal that the component is ready.
    */
-export default function HiGlassLazy(props) {
+export default function GoslingLazy(props) {
   const {
     coordinationScopes,
     theme,
@@ -87,6 +74,8 @@ export default function HiGlassLazy(props) {
     genomeSize,
     height,
   } = props;
+
+  const gosRef = useRef();
 
   // Get "props" from the coordination space.
   const [{
@@ -103,119 +92,15 @@ export default function HiGlassLazy(props) {
 
   // eslint-disable-next-line no-unused-vars
   const [width, computedHeight, containerRef] = useGridItemSize();
-  const [hgInstance, setHgInstance] = useState();
-  const isActiveRef = useRef();
-
-  const hgOptions = useMemo(() => ({
-    ...hgOptionsProp,
-    theme,
-  }), [hgOptionsProp, theme]);
-
-  const hgViewConfig = useMemo(() => {
-    // HiGlass needs the start and end absolute genome coordinates
-    const centerX = genomicTargetX;
-    const genomesPerUnitX = genomeSize / (2 ** genomicZoomX);
-    const unitX = width / HG_SIZE;
-    const initialXDomain = [
-      centerX - genomesPerUnitX * unitX / 2,
-      centerX + genomesPerUnitX * unitX / 2,
-    ];
-    const centerY = genomicTargetY;
-    const genomesPerUnitY = genomeSize / (2 ** genomicZoomY);
-    const unitY = height / HG_SIZE;
-    const initialYDomain = [
-      centerY - genomesPerUnitY * unitY / 2,
-      centerY + genomesPerUnitY * unitY / 2,
-    ];
-    return {
-      editable: false,
-      zoomFixed: false,
-      trackSourceServers: [
-        '//higlass.io/api/v1',
-      ],
-      exportViewUrl: '//higlass.io/api/v1/viewconfs',
-      views: [
-        {
-          uid: 'main',
-          ...hgViewConfigProp,
-          initialXDomain,
-          initialYDomain,
-        },
-      ],
-      zoomLocks: {
-        locksByViewUid: {},
-        locksDict: {},
-      },
-      locationLocks: {
-        locksByViewUid: {},
-        locksDict: {},
-      },
-      valueScaleLocks: {
-        locksByViewUid: {},
-        locksDict: {},
-      },
-    };
-  }, [genomicTargetX, genomeSize, genomicZoomX, width, genomicTargetY,
-    genomicZoomY, height, hgViewConfigProp]);
-
-  useEffect(() => {
-    const handleMouseEnter = () => {
-      isActiveRef.current = true;
-    };
-    const handleMouseLeave = () => {
-      isActiveRef.current = false;
-    };
-    const container = containerRef.current;
-    container.addEventListener('mouseenter', handleMouseEnter);
-    container.addEventListener('mouseleave', handleMouseLeave);
-    return () => {
-      container.removeEventListener('mouseenter', handleMouseEnter);
-      container.removeEventListener('mouseenter', handleMouseLeave);
-    };
-  }, [containerRef]);
-
-
-  useEffect(() => {
-    if (!hgInstance) {
-      return () => {};
-    }
-    hgInstance.api.on('viewConfig', (viewConfigString) => {
-      // Only set if the user mouse is over this component ("is active").
-      // Otherwise, this could be an initial on viewConfig change callback from a sibling,
-      // which will cause an infinite loop.
-      if (!isActiveRef.current) {
-        return;
-      }
-      const viewConfig = JSON.parse(viewConfigString);
-      const xDomain = viewConfig.views[0].initialXDomain;
-      const yDomain = viewConfig.views[0].initialYDomain;
-
-      const nextGenomicZoomX = Math.log2(
-        genomeSize / ((xDomain[1] - xDomain[0]) * (HG_SIZE / width)),
-      );
-      const nextGenomicZoomY = Math.log2(
-        genomeSize / ((yDomain[1] - yDomain[0]) * (HG_SIZE / height)),
-      );
-      const nextGenomicTargetX = xDomain[0] + (xDomain[1] - xDomain[0]) / 2;
-      const nextGenomicTargetY = yDomain[0] + (yDomain[1] - yDomain[0]) / 2;
-      setGenomicZoomX(nextGenomicZoomX);
-      setGenomicZoomY(nextGenomicZoomY);
-      setGenomicTargetX(nextGenomicTargetX);
-      setGenomicTargetY(nextGenomicTargetY);
-    });
-    return () => hgInstance.api.off('viewConfig');
-  }, [hgInstance, genomeSize, width, height, setGenomicZoomX, setGenomicZoomY,
-    setGenomicTargetX, setGenomicTargetY]);
 
   return (
     <div className="higlass-wrapper-parent">
       <div className="higlass-wrapper" ref={containerRef} style={{ height: `${height}px` }}>
         <Suspense fallback={<div>Loading...</div>}>
-          <HiGlassComponent
-            ref={setHgInstance}
-            zoomFixed={false}
-            viewConfig={hgViewConfig}
-            options={hgOptions}
+          <LazyGoslingComponent
+            ref={gosRef}
+            spec={mySpec}
+            experimental={{ reactive: true }}
           />
         </Suspense>
       </div>
@@ -223,7 +108,7 @@ export default function HiGlassLazy(props) {
   );
 }
 
-HiGlassLazy.defaultProps = {
+GoslingLazy.defaultProps = {
   hgOptions: {
     bounded: true,
     pixelPreciseMarginPadding: true,
