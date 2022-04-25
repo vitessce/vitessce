d5958c6277dc1dca96153297e86ce9cff2f28722
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-04-25 16:05:43 -0400

    Merge

--- src/app/view-config-versions.js
+++ src/app/view-config-versions.js
@@ -9,12 +9,8 @@ import configSchema1_0_4 from '../schemas/config-1.0.4.schema.json';
 import configSchema1_0_5 from '../schemas/config-1.0.5.schema.json';
 import configSchema1_0_6 from '../schemas/config-1.0.6.schema.json';
 import configSchema1_0_7 from '../schemas/config-1.0.7.schema.json';
-<<<<<<< HEAD
 import configSchema1_0_8 from '../schemas/config-1.0.8.schema.json';
-=======
-// TODO
 import configSchema1_0_9 from '../schemas/config-1.0.9.schema.json';
->>>>>>> 8230d16f (Add console error)
 import cellSetsSchema from '../schemas/cell-sets.schema.json';
 import rasterSchema from '../schemas/raster.schema.json';
 import {
@@ -26,12 +22,8 @@ import {
   upgradeFrom1_0_4,
   upgradeFrom1_0_5,
   upgradeFrom1_0_6,
-<<<<<<< HEAD
   upgradeFrom1_0_7,
-=======
-  // TODO
   upgradeFrom1_0_8,
->>>>>>> 8230d16f (Add console error)
 } from './view-config-upgraders';
 
 /**
@@ -39,11 +31,7 @@ import {
  * Add a new schema and upgrade function here when bumping the view config version.
  * The latest view config version should always have a null value instead of an upgrade function.
  */
-<<<<<<< HEAD
-export const LATEST_VERSION = '1.0.8';
-=======
 export const LATEST_VERSION = '1.0.9';
->>>>>>> 8230d16f (Add console error)
 export const SCHEMA_HANDLERS = {
   '0.1.0': [new Ajv().compile(configSchema0_1_0), upgradeFrom0_1_0],
   '1.0.0': [new Ajv().addSchema(cellSetsSchema).addSchema(rasterSchema).compile(configSchema1_0_0), upgradeFrom1_0_0],
@@ -53,12 +41,7 @@ export const SCHEMA_HANDLERS = {
   '1.0.4': [new Ajv().addSchema(cellSetsSchema).addSchema(rasterSchema).compile(configSchema1_0_4), upgradeFrom1_0_4],
   '1.0.5': [new Ajv().addSchema(cellSetsSchema).addSchema(rasterSchema).compile(configSchema1_0_5), upgradeFrom1_0_5],
   '1.0.6': [new Ajv().addSchema(cellSetsSchema).addSchema(rasterSchema).compile(configSchema1_0_6), upgradeFrom1_0_6],
-<<<<<<< HEAD
   '1.0.7': [new Ajv().addSchema(cellSetsSchema).addSchema(rasterSchema).compile(configSchema1_0_7), upgradeFrom1_0_7],
-  '1.0.8': [new Ajv().addSchema(cellSetsSchema).addSchema(rasterSchema).compile(configSchema1_0_8), null],
-=======
-  '1.0.7': [new Ajv().addSchema(cellSetsSchema).addSchema(rasterSchema).compile(configSchema1_0_7), upgradeFrom1_0_8], // TODO
-  // TODO
+  '1.0.8': [new Ajv().addSchema(cellSetsSchema).addSchema(rasterSchema).compile(configSchema1_0_8), upgradeFrom1_0_8],
   '1.0.9': [new Ajv().addSchema(cellSetsSchema).addSchema(rasterSchema).compile(configSchema1_0_9), null],
->>>>>>> 8230d16f (Add console error)
 };
