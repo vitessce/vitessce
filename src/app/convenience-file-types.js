7cda0bb975742e6fc5608a631824bae1e005116d
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-06-10 13:26:47 -0400

    Add convenience function

--- src/app/convenience-file-types.js
+++ src/app/convenience-file-types.js
@@ -1,8 +1,28 @@
+import { FileType } from './constants';
+
+export function expandCellSetsJson(fileDef) {
+  const {
+    url,
+    requestInit,
+    coordinationValues = {},
+  } = fileDef;
+  const baseCoordinationValues = {
+    obsType: coordinationValues.obsType || 'cell',
+  };
+  return [
+    {
+      fileType: FileType.OBS_SETS_CELL_SETS_JSON,
+      url,
+      requestInit,
+      coordinationValues: baseCoordinationValues,
+    },
+  ];
+}
 
 /**
  * Built-in convenience file type
  * expansion functions.
  */
 export const CONVENIENCE_FILE_TYPES = {
-
+  // [FileType.CELL_SETS_JSON]: expandCellSetsJson,
 };
