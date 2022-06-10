97fd870b287def529aa0771de7afd54eb1980b72
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-06-10 14:15:29 -0400

    Add convenience function

--- src/app/convenience-file-types.js
+++ src/app/convenience-file-types.js
@@ -1,8 +1,34 @@
+import { FileType } from './constants';
 
+export function expandClustersJson(fileDef) {
+  const baseFileDef = {
+    ...fileDef,
+    coordinationValues: {
+      ...fileDef.coordinationValues,
+      obsType: fileDef.coordinationValues?.obsType || 'cell',
+      featureType: fileDef.coordinationValues?.featureType || 'gene',
+    },
+  };
+  delete baseFileDef.type;
+  return [
+    {
+      ...baseFileDef,
+      fileType: FileType.OBS_INDEX_CLUSTERS_JSON,
+    },
+    {
+      ...baseFileDef,
+      fileType: FileType.FEATURE_INDEX_CLUSTERS_JSON,
+    },
+    {
+      ...baseFileDef,
+      fileType: FileType.OBS_FEATURE_MATRIX_CLUSTERS_JSON,
+    },
+  ];
+}
 /**
  * Built-in convenience file type
  * expansion functions.
  */
 export const CONVENIENCE_FILE_TYPES = {
-
+  // [FileType.CLUSTERS_JSON]: expandClustersJson,
 };
--- src/app/convenience-file-types.test.js
+++ src/app/convenience-file-types.test.js
@@ -1,6 +1,40 @@
-/* eslint-disable */
 import expect from 'expect';
+import {
+  expandClustersJson,
+} from './convenience-file-types';
 
 describe('src/app/convenience-file-types.js', () => {
-
+  describe('expandClustersJson', () => {
+    it('expands clusters.json', () => {
+      expect(expandClustersJson({
+        fileType: 'clusters.json',
+        url: 'http://localhost:8000/clusters.json',
+      })).toEqual([
+        {
+          fileType: 'obsIndex.clusters.json',
+          url: 'http://localhost:8000/clusters.json',
+          coordinationValues: {
+            obsType: 'cell',
+            featureType: 'gene',
+          },
+        },
+        {
+          fileType: 'featureIndex.clusters.json',
+          url: 'http://localhost:8000/clusters.json',
+          coordinationValues: {
+            obsType: 'cell',
+            featureType: 'gene',
+          },
+        },
+        {
+          fileType: 'obsFeatureMatrix.clusters.json',
+          url: 'http://localhost:8000/clusters.json',
+          coordinationValues: {
+            obsType: 'cell',
+            featureType: 'gene',
+          },
+        },
+      ]);
+    });
+  });
 });
