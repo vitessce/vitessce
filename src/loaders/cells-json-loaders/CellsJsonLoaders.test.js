24cd3f2cd46a0f1655c786582f920ea41e728dfc
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-06-24 10:48:40 -0400

    Update tests

--- src/loaders/cells-json-loaders/CellsJsonLoaders.test.js
+++ src/loaders/cells-json-loaders/CellsJsonLoaders.test.js
@@ -1,6 +1,8 @@
 import expect from 'expect';
 import CellsJsonAsObsEmbeddingLoader from './CellsJsonAsObsEmbedding';
 import CellsJsonAsObsLabelsLoader from './CellsJsonAsObsLabels';
+import CellsJsonAsObsLocationsLoader from './CellsJsonAsObsLocations';
+import CellsJsonAsObsSegmentationsLoader from './CellsJsonAsObsSegmentations';
 import cellsGoodFixture from '../../schemas/fixtures/cells.good.json';
 import { JsonSource } from '../data-sources';
 import LoaderResult from '../LoaderResult';
@@ -46,6 +48,38 @@ describe.only('loaders/cells-json-loaders', () => {
       expect(Object.keys(payload)).toEqual(['obsIndex', 'obsLabels']);
       expect(payload.obsIndex).toEqual(['778']);
       expect(payload.obsLabels).toEqual(['Inhibitory CP']);
+    });
+  });
+  describe('CellsJsonAsObsLocationsLoader', () => {
+    it('can load obsLocations', async () => {
+      const loader = createLoader(CellsJsonAsObsLocationsLoader, {
+        fileType: 'obsLocations.cells.json',
+      }, cellsGoodFixture);
+      const result = await loader.load();
+      expect(result).toBeInstanceOf(LoaderResult);
+      const payload = result.data;
+      expect(Object.keys(payload)).toEqual(['obsIndex', 'obsLocations']);
+      expect(payload.obsIndex).toEqual(['778']);
+      expect(payload.obsLocations[0]).toEqual([18171.230942376023]);
+      expect(payload.obsLocations[1]).toEqual([24590.795274569497]);
+    });
+  });
+  describe('CellsJsonAsObsSegmentationsLoader', () => {
+    it('can load obsSegmentations', async () => {
+      const loader = createLoader(CellsJsonAsObsSegmentationsLoader, {
+        fileType: 'obsSegmentations.cells.json',
+      }, cellsGoodFixture);
+      const result = await loader.load();
+      expect(result).toBeInstanceOf(LoaderResult);
+      const payload = result.data;
+      expect(Object.keys(payload)).toEqual(['obsIndex', 'obsSegmentationsType', 'obsSegmentations']);
+      expect(payload.obsIndex).toEqual(['778']);
+      expect(payload.obsSegmentationsType).toEqual('polygon');
+      expect(payload.obsSegmentations[0]).toEqual([
+        [1, 2],
+        [3, 4],
+        [5, 6],
+      ]);
     });
   });
 });
