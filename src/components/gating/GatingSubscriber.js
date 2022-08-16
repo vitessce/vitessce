7e7451b666af344aa9b17cc9483c169c2b3299f4
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2022-08-16 09:11:59 -0400

    Release v1.2.1 (#1315)
    * Release for v1.2.1. Commit by create-release.sh
    
    * Demo for v1.2.1. Commit by create-release.sh
    
    * Update changelog
    
    * Bug fix for gating plot
    
    * Comments
    
    * Changelog
    
    * Docs
    
    * Docs
    
    * Update demo
    
    * Remove gating demo from list

--- src/components/gating/GatingSubscriber.js
+++ src/components/gating/GatingSubscriber.js
@@ -112,16 +112,21 @@ export default function GatingSubscriber(props) {
       const updatedCells = {};
       // Iterate through cells and build new cells with added mapping.
       expressionMatrix.rows.forEach((cellId, index) => {
-        // Need to use new cell object reference
-        // to prevent other plots from seeing these
-        // changes to the cells objects.
-        const curCell = { ...cells[cellId] };
+        // Need to use new cell and cell.mappings objects
+        // to prevent other views which use the same
+        // top-level cells object reference from seeing any
+        // modifications.
         const cellMatrixRowOffset = expressionMatrix.cols.length * index;
-        curCell.mappings[mapping] = [
-          transformFunction(expressionMatrix.matrix[cellMatrixRowOffset + selectedGeneCols[0]]),
-          transformFunction(expressionMatrix.matrix[cellMatrixRowOffset + selectedGeneCols[1]]),
-        ];
-        updatedCells[cellId] = curCell;
+        updatedCells[cellId] = {
+          ...cells[cellId],
+          mappings: {
+            ...cells[cellId].mappings,
+            [mapping]: [
+              transformFunction(expressionMatrix.matrix[cellMatrixRowOffset + selectedGeneCols[0]]),
+              transformFunction(expressionMatrix.matrix[cellMatrixRowOffset + selectedGeneCols[1]]),
+            ],
+          },
+        };
       });
 
       return updatedCells;
@@ -132,7 +137,7 @@ export default function GatingSubscriber(props) {
 
   // Puts the mapping values in the cell info tooltip.
   const getCellInfoOverride = (cellId) => {
-    const cell = cells[cellId];
+    const cell = cellsWithGenes[cellId];
     const selectedTransformName = transformOptions.find(
       o => o.value === featureValueTransform,
     )?.name;
