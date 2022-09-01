5308fae920eded7dde3cb891fb7e5f53de428168
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-09-01 13:55:24 -0400

    Update set-node-options script

--- set-node-options.sh
+++ set-node-options.sh
@@ -2,10 +2,10 @@ FULL_VERSION=`node --version`
 MAJOR_VERSION_WITH_V=${FULL_VERSION%.*.*}
 MAJOR_VERSION=${MAJOR_VERSION_WITH_V:1}
 
-V_OPTIONS="--max_old_space_size=4096"
+V_OPTIONS="--max_old_space_size=8192"
 if [ "$MAJOR_VERSION" -gt "17" ]; then
   echo "Set node options for >=17"
-  V_OPTIONS="--max_old_space_size=4096 --openssl-legacy-provider"
+  V_OPTIONS="--max_old_space_size=8192 --openssl-legacy-provider"
 fi
 
 if [[ "$1" == "--action" ]]; then
