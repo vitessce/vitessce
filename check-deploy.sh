1ada9714acf43ec05c2e572c45c9f0cf9ce09139
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-09-20 16:28:33 -0400

    Comment

--- check-deploy.sh
+++ check-deploy.sh
@@ -1,6 +1,7 @@
 REMOTE_VERSION=`npm view vitessce --json | jq .version`
 LOCAL_VERSION=`cat ./package.json | jq .version`
 
+# Reference: https://stackoverflow.com/a/37939589
 function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
 
 if [ $(version $LOCAL_VERSION) -gt $(version $REMOTE_VERSION) ]; then
