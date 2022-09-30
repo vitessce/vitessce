e69e5a0f25be669f3103c68b7f0121b9b3c8fd54
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-09-30 16:14:21 -0400

    Bundle scripts for main package

---
+++ monorepo/packages/main/vite.config.js
@@ -0,0 +1,87 @@
+// References:
+// - https://github.com/gosling-lang/gosling.js/blob/master/vite.config.js
+// - https://github.com/gosling-lang/gosling.js/blob/master/scripts/build-embed.js
+// - https://github.com/gosling-lang/gosling.js/blob/master/scripts/build-umd.js
+// - https://github.com/hms-dbmi/viv/blob/master/scripts/bundle.mjs
+import * as esbuild from 'esbuild';
+import { defineConfig } from 'vite';
+import path from 'path';
+import pkg from './package.json';
+
+
+/**
+ * Bundles vite worker modules during development into single scripts.
+ * see: https://github.com/hms-dbmi/viv/pull/469#issuecomment-877276110
+ * @returns {import('vite').Plugin}
+ */
+ const bundleWebWorker = {
+  name: 'bundle-web-worker',
+  apply: 'serve', // plugin only applied with dev-server
+  async transform(_, id) {
+      if (/\?worker_file$/.test(id)) {
+          // just use esbuild to bundle the worker dependencies
+          const bundle = await esbuild.build({
+              entryPoints: [id],
+              inject: ['./src/alias/buffer-shim.js'],
+              format: 'iife',
+              bundle: true,
+              write: false
+          });
+          if (bundle.outputFiles.length !== 1) {
+              throw new Error('Worker must be a single module.');
+          }
+          return bundle.outputFiles[0].text;
+      }
+  }
+};
+
+// We can't inject a global `Buffer` polyfill for the worker entrypoint using vite alone,
+// so we reuse the `bundle-web-worker` plugin to inject the buffer shim during production.
+const manualInlineWorker = {
+  apply: 'build',
+  async transform(code, id) {
+      if (id.endsWith('bam-worker.ts?worker&inline') || id.endsWith('vcf-worker.ts?worker&inline')) {
+          const bundle = await bundleWebWorker.transform(code, id + '?worker_file');
+          const base64 = Buffer.from(bundle).toString('base64');
+          // https://github.com/vitejs/vite/blob/72cb33e947e7aa72d27ed0c5eacb2457d523dfbf/packages/vite/src/node/plugins/worker.ts#L78-L87
+          return `const encodedJs = "${base64}";
+const blob = typeof window !== "undefined" && window.Blob && new Blob([atob(encodedJs)], { type: "text/javascript;charset=utf-8" });
+export default function() {
+const objURL = blob && (window.URL || window.webkitURL).createObjectURL(blob);
+try {
+  return objURL ? new Worker(objURL) : new Worker("data:application/javascript;base64," + encodedJs, {type: "module"});
+} finally {
+  objURL && (window.URL || window.webkitURL).revokeObjectURL(objURL);
+}
+}`;
+      }
+  }
+};
+
+const alias = {
+  //uuid: path.resolve(__dirname, './node_modules/uuid/dist/esm-browser/index.js')
+};
+
+const skipExt = new Set(['uuid']);
+const external = [
+  ...Object.keys(pkg.dependencies),
+  //...Object.keys(pkg.peerDependencies)
+].filter(dep => !skipExt.has(dep));
+
+export default defineConfig({
+  build: {
+      emptyOutDir: false,
+      outDir: 'dist',
+      minify: false,
+      target: 'es2018',
+      sourcemap: true,
+      lib: {
+          entry: path.resolve(__dirname, 'src/index.js'),
+          formats: ['es'],
+          fileName: 'vitessce'
+      },
+      rollupOptions: { external }
+  },
+  resolve: { alias },
+  plugins: [manualInlineWorker]
+});
--- monorepo/vite.config.js
+++ monorepo/vite.config.js
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+// For tests.
 export default defineConfig({
   plugins: [react({
     jsxRuntime: 'classic',
