4937e7eb6ace3a63be0902811811a72b7488c502
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-10-01 18:12:46 -0400

    Update

--- monorepo/sites/demo/src/index.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { VitessceDemo } from './vitessce-demo';
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(_jsx(VitessceDemo, {}));
