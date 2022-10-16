4bd70c05b34a340930afe2e6196751e2275ba504
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-10-16 00:23:45 -0500

    Add plain html tests

---
+++ sites/html/cypress.config.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3003',
+    chromeWebSecurity: false,
+    specPattern: 'cypress/e2e/*.spec.cy.js',
+    supportFile: false,
+  },
+  video: false,
+});
