fa727f2d5c0a7dbc573a7b832e3fb947263056d7
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-10-25 10:23:19 -0400

    Lint

--- examples/plugins/src/index.js
+++ examples/plugins/src/index.js
@@ -1,7 +1,7 @@
 import { pluginViewType } from './plugins/plugin-view-type';
 import { pluginCoordinationType } from './plugins/plugin-coordination-type';
 import { pluginFileType } from './plugins/plugin-file-type';
-import { pluginImageView } from './plugins/plugin-image-view'
+import { pluginImageView } from './plugins/plugin-image-view';
 
 export const configsWithPlugins = {
   'plugin-view-type': pluginViewType,
