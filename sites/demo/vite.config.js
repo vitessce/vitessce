0327a9c67c73760f2e75cdf8f627da4ba0d856b1
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-11-30 08:40:05 -0500

    Fix demo base path

--- sites/demo/vite.config.js
+++ sites/demo/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { serveTestFixtures } from '../../vite.config';
 
 export default defineConfig({
+  base: './',
   plugins: [
     react({
       jsxRuntime: 'classic',
