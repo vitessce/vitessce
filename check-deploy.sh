8ba19a9e6afeb7aaa7b4711fcdc9b0afb7a50603
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-12-01 20:39:05 -0500

    Fix deploy script

--- check-deploy.sh
+++ check-deploy.sh
@@ -2,7 +2,8 @@ REMOTE_VERSION=`npm view vitessce --json | jq .version`
 LOCAL_VERSION=`cat ./package.json | jq .version`
 
 # Reference: https://stackoverflow.com/a/37939589
-function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
+# Reference: https://stackoverflow.com/a/26314887
+function version { echo "$@" | tr -d '"' | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
 
 if [ $(version $LOCAL_VERSION) -gt $(version $REMOTE_VERSION) ]; then
   echo "::set-output name=needs-deploy::true"
