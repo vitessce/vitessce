4f99b7419b80cf00c1e69975b3e0cdb96143908a
ilan-gold <ilanbassgold@gmail.com>
2022-12-07 21:45:40 +0100

    (fix): pin deck.gl properly

--- .meta-updater/main.mjs
+++ .meta-updater/main.mjs
@@ -7,9 +7,9 @@ const { cloneDeep } = lodash;
 const isDryrun = process.env.META_UPDATER_MODE === 'dryrun';
 const isVersionOnly = process.env.META_UPDATER_MODE === 'versiononly';
 
-const LUMAGL_VERSION = '8.5.16';
+const LUMAGL_VERSION = '~8.5.16';
 const LOADERSGL_VERSION = "^3.0.0";
-const DECKGL_VERSION = '^8.8.6';
+const DECKGL_VERSION = '~8.8.6';
 const TURF_VERSION = "^6.5.0";
 const NEBULAGL_VERSION = "0.23.8";
 const OTHER_VERSIONS = {
