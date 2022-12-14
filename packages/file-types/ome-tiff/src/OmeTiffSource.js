d5491db3731e3b95b385c07d2fd221ee6f66731b
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-12-14 16:52:39 -0500

    WIP

---
+++ packages/file-types/ome-tiff/src/OmeTiffSource.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-underscore-dangle */
+import { DataSourceFetchError } from '@vitessce/vit-s';
+
+export default class OmeTiffSource {
+  constructor({ url, requestInit }) {
+    this.url = url;
+    this.requestInit = requestInit;
+  }
+
+  get data() {
+    if (this._data) return this._data;
+    this._data = fetch(this.url, this.requestInit).then((response) => {
+      if (!response.ok) {
+        return Promise.reject(new DataSourceFetchError('JsonSource', this.url, response.headers));
+      }
+      return response.json();
+    // eslint-disable-next-line no-console
+    }).catch(() => Promise.reject(new DataSourceFetchError('JsonSource', this.url, {})));
+    return this._data;
+  }
+}
---
+++ packages/file-types/ome-tiff/src/index.js
@@ -0,0 +1,3 @@
+export { default as OmeTiffSource } from './OmeTiffSource';
+export { default as RasterJsonAsImageLoader } from './RasterJsonAsImageLoader';
+export { default as RasterJsonAsObsSegmentationsLoader } from './RasterJsonAsObsSegmentationsLoader';
