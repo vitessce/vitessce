8b942ff7c5ad9a413881ed8ebc61f26681ba143d
mkeller <7525285+keller-mark@users.noreply.github.com>
2023-02-23 12:48:19 -0500

    Use tubule with area

--- examples/configs/src/view-configs/kpmp.js
+++ examples/configs/src/view-configs/kpmp.js
@@ -70,7 +70,7 @@ export const kpmp2023 = {
         },
         {
           fileType: 'obsFeatureMatrix.anndata.zarr',
-          url: `${baseUrl}/S-1905-017737/Tubules.adata.zarr`,
+          url: `${baseUrl}/S-1905-017737/Tubules with Area.adata.zarr`,
           options: {
             path: 'X',
           },
