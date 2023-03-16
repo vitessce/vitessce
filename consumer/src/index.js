15e2103e80498b089fe9198f09a13788b7d48202
mkeller <7525285+keller-mark@users.noreply.github.com>
2023-03-15 21:51:49 -0400

    Update

--- consumer/src/index.js
+++ consumer/src/index.js
@@ -10,7 +10,7 @@ const e = React.createElement;
 
 const eng2019 = {
   name: 'Eng et al., Nature 2019',
-  version: '1.0.15',
+  version: '1.0.16', // Must be the latest version because using VitS rather than Vitessce
   description: '',
   datasets: [],
   initStrategy: 'auto',
