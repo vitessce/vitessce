91ab5869827209956e1850422cabcd51377ba8d3
mkeller <7525285+keller-mark@users.noreply.github.com>
2023-04-17 14:50:05 -0400

    README things

--- scripts/changeset-publish.sh
+++ scripts/changeset-publish.sh
@@ -9,19 +9,34 @@ if [[ "$1" != "--action" ]]; then
   fi
 fi
 
+# Temporarily copy the root README.md to packages/main/prod/README.md,
+# since the file at packages/main/prod will be shown for the main 'vitessce'
+# package on NPM.
+mv ./packages/main/prod/README.md ./packages/main/prod/README.temp.md
+cp ./README.md ./packages/main/prod/README.md
+
+# Publish all sub-packages to NPM.
 pnpm publish --filter='./packages/**' --no-git-checks --access public
+
+# Revert the copied README
+rm ./packages/main/prod/README.md
+mv ./packages/main/prod/README.temp.md ./packages/main/prod/README.md
+
+# Tag this as the release commit.
 git tag v$(cat ./package.json | jq -r .version)
+# Push the tag to GitHub.
 git push --follow-tags
+
+# Create RELEASE_NOTES.md from the latest entries in CHANGELOG.md.
 node .changeset/get-release-notes.mjs
 
+# Either indicate to softprops/action-gh-release that there should be a release,
+# or create a release from the command line.
 if [[ "$1" == "--action" ]]; then
-  # If this was running from within the changesets/action action,
-  # then we know that there should be a release.
   echo "should-release=true" >> $GITHUB_OUTPUT
 else
   # Need to create a GitHub release from the command line.
   TAG_NAME=v$(cat ./package.json | jq -r .version)
-
+  # Use the GitHub CLI to create a release.
   gh release create $TAG_NAME -F RELEASE_NOTES.md
-
 fi
