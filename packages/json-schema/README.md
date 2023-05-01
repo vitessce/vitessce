6bdd30e709fc998aa6ae4d2e4104dd4833b99552
mkeller <7525285+keller-mark@users.noreply.github.com>
2023-05-01 12:56:09 -0400

    Add comment

--- packages/json-schema/README.md
+++ packages/json-schema/README.md
@@ -2,3 +2,10 @@ This directory contains NodeJS scripts for converting the Zod schemas from `@vit
 
 These JSON schemas are not intended to be used at runtime within any Vitessce JS sub-package.
 Instead, they are intended to be uploaded to NPM/CDN where they can be accessed from Python or R to perform validation in the absence of JavaScript.
+
+
+From the root of the monorepo, run:
+
+```sh
+pnpm run build-json-schema
+```
--- packages/json-schema/scripts/convert-schemas.mjs
+++ packages/json-schema/scripts/convert-schemas.mjs
@@ -1,7 +1,10 @@
 import fs from 'node:fs';
 import { join } from 'node:path';
 import { zodToJsonSchema } from 'zod-to-json-schema';
-import { VERSIONED_CONFIG_SCHEMAS } from '@vitessce/schemas/dist/index.mjs';
+// We need this explicit import because we want to import from the development package,
+// and Node does not use the PNPM publishConfig property like a consumer would
+// be able to do with the production package.
+import { VERSIONED_CONFIG_SCHEMAS } from '@vitessce/schemas/dist/index.js';
 
 
 const distDir = join('dist');
