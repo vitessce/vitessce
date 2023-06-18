b2386f9e53951c3b3764506a72b4a9edf1a21d83
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2023-06-18 10:06:15 -0400

    Try windows again

--- test.sh
+++ test.sh
@@ -1,28 +1,24 @@
 #!/usr/bin/env bash
 set -o errexit
 
-start() { echo travis_fold':'start:$1; echo $1; }
-end() { echo travis_fold':'end:$1; }
-die() { set +v; echo "$*" 1>&2 ; sleep 1; exit 1; }
-
-start changelog
+# changelog check
 if [[ "$1" == "--action" ]]; then
   if [ "$GITHUB_REF" != 'refs/heads/main' ]; then
     diff CHANGELOG.md <(curl https://raw.githubusercontent.com/vitessce/vitessce/main/CHANGELOG.md) \
       && die 'Update CHANGELOG.md'
   fi
 fi
-end changelog
+# end changelog check
 
-start lint
+# linting
 pnpm run lint || die 'eslint failed; try: pnpm run lint-fix'
-end lint
+# end linting
 
-start test
+# unit tests
 pnpm run test
-end test
+# end unit tests
 
-start cypress
+# end-to-end tests with Cypress
 # Cypress fails randomly on GH Actions so we only run this locally.
 if [[ "$1" != "--action" ]]; then
   cd sites/demo
@@ -31,10 +27,12 @@ if [[ "$1" != "--action" ]]; then
   pnpm run cypress:run
   echo 'NOTE: Server is still running.'
 fi
-end cypress
+# end end-to-end tests
 
-start schema
+# schema checks
 ./packages/vit-s/src/schemas/schema-schema.sh
-end schema
+# end schema checks
 
+# tsconfig checks
 node scripts/check-tsconfig-references.mjs
+# end tsconfig checks
