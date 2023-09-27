66a967f1faefd13a71c62d4b033938351631319b
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2023-09-26 22:43:12 -0400

    Types-only package (#1708)

---
+++ packages/types/src/index.ts
@@ -0,0 +1,74 @@
+// This is a type-only sub-package.
+type MatrixResult = {
+  data: number[] | Float32Array[];
+  shape: number[];
+};
+
+export type ObsFeatureMatrixData = {
+  obsIndex: string[];
+  featureIndex: string[];
+  obsFeatureMatrix: MatrixResult;
+};
+
+export type ObsEmbeddingData = {
+  obsIndex: string[];
+  obsEmbedding: MatrixResult;
+};
+
+export type ObsLocationsData = {
+  obsIndex: string[];
+  obsLocations: MatrixResult;
+};
+
+export type ObsPointsData = {
+  obsIndex: string[];
+  obsPoints: MatrixResult;
+};
+
+export type ObsSpotsDatat = {
+  obsIndex: string[];
+  obsSpots: MatrixResult;
+};
+
+export type ObsSegmentationsPolygons = {
+  obsSegmentations: { data: number[][][], shape: number[] };
+  obsSegmentationsType: 'polygon';
+};
+
+// TODO: bitmask
+export type ObsSegmentationsData = ObsSegmentationsPolygons;
+
+export type FeatureLabelsData = {
+  featureIndex: string[];
+  featureLabels: string[];
+};
+
+export type ObsLabelsData = {
+  obsIndex: string[];
+  obsLabels: string[];
+};
+
+export type SetsTreeNodeLeaf = {
+  name: string;
+  color?: number[];
+  set: Array<[string, number | null]>;
+}
+
+export type SetsTreeNodeNonLeaf = {
+  name: string;
+  color?: number[];
+  children: Array<SetsTreeNodeNonLeaf | SetsTreeNodeLeaf>;
+};
+
+export type SetsTree = {
+  version: '0.1.3';
+  tree: SetsTreeNodeNonLeaf[];
+};
+
+export type ObsSetsData = {
+  obsIndex: string[];
+  obsSets: SetsTree;
+  obsSetsMembership: Map<string, string[][]>;
+};
+
+export {};
