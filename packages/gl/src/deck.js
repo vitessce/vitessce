0db9729fe9e1e0ca55e898c4f3cb70011532316b
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2023-10-06 14:49:44 -0400

    WIP: patching dependencies

--- packages/gl/src/deck.js
+++ packages/gl/src/deck.js
@@ -1,3 +1,3 @@
-export { default as DeckGL } from 'deck.gl';
+export { DeckGL } from '@deck.gl/react';
 export { COORDINATE_SYSTEM, CompositeLayer, OrthographicView, OrbitView } from '@deck.gl/core';
 export { PolygonLayer, TextLayer, ScatterplotLayer } from '@deck.gl/layers';
