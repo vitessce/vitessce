ba7941a11678fbaab69bb46343902a45fbe77906
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2023-11-28 09:45:05 -0500

    Presigned URLs via environment variables

--- examples/configs/src/view-configs/spatial-beta/washu.js
+++ examples/configs/src/view-configs/spatial-beta/washu.js
@@ -4,6 +4,24 @@ import {
   hconcat,
 } from '@vitessce/config';
 
+
+// We do not want to commit any presigned URLs to the repository, so we will set them as environment variables in the terminal.
+
+// Get the presigned URLs for these files on S3:
+// s3://hdv-spatial-data/washu-kidney/Lighsheet_test_data/sample_16/20x/stitched-ome_tiff/Sample_16_20x_2-Stitched.pyramid.offsets.json
+// s3://hdv-spatial-data/washu-kidney/Lighsheet_test_data/sample_16/20x/stitched-ome_tiff/Sample_16_20x_2-Stitched.pyramid.ome.tif
+
+// Set these presigned URLs as environment variables in the terminal by running:
+
+// export VITE_TIFF_URL='{your presigned URL for the OME-TIFF file}'
+// export VITE_JSON_URL='{your presigned URL for the offsets JSON file}'
+
+// Note: You may need to disable 'Terminal may enable paste bracketing' in iTerm2 Settings > Profiles > Terminal.
+// Reference: https://stackoverflow.com/a/75748117
+
+const omeTiffPresignedUrl = import.meta.env.VITE_TIFF_URL;
+const offsetsPresignedUrl = import.meta.env.VITE_JSON_URL;
+
 function generateWashUConfig() {
   const config = new VitessceConfig({
     schemaVersion: '1.0.16',
@@ -11,9 +29,9 @@ function generateWashUConfig() {
   });
   const dataset = config.addDataset('WashU data').addFile({
     fileType: 'image.ome-tiff',
-    url: 'https://hdv-spatial-data.s3.us-east-1.amazonaws.com/washu-kidney/Lighsheet_test_data/sample_16/20x/stitched-ome_tiff/Sample_16_20x_2-Stitched.pyramid.ome.tif',
+    url: omeTiffPresignedUrl,
     options: {
-        offsetsUrl: 'https://hdv-spatial-data.s3.us-east-1.amazonaws.com/washu-kidney/Lighsheet_test_data/sample_16/20x/stitched-ome_tiff/Sample_16_20x_2-Stitched.pyramid.offsets.json'
+        offsetsUrl: offsetsPresignedUrl
     },
     coordinationValues: {
       fileUid: 'Sample_16_20x_2-Stitched',
@@ -43,9 +61,23 @@ function generateWashUConfig() {
           },
           {
             spatialTargetC: 1,
-            spatialChannelColor: [0, 255, 0],
+            spatialChannelColor: [255, 255, 0],
             spatialChannelVisible: true,
             spatialChannelOpacity: 1.0,
+            spatialChannelWindow: null,
+          },
+          {
+            spatialTargetC: 2,
+            spatialChannelColor: [0, 255, 255],
+            spatialChannelVisible: true,
+            spatialChannelOpacity: 1.0,
+            spatialChannelWindow: null,
+          },
+          {
+            spatialTargetC: 3,
+            spatialChannelColor: [255, 255, 255],
+            spatialChannelVisible: true,
+            spatialChannelOpacity: 0.5,
             spatialChannelWindow: null,
           },
         ]),
