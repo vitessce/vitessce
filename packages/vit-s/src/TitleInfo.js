33f62d1f55e220a918b3b01bc2d349909d50b8b0
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2023-11-30 16:57:24 -0500

    UseMemo for TitleInfo icon buttons (#1736)

--- packages/vit-s/src/TitleInfo.js
+++ packages/vit-s/src/TitleInfo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import clsx from 'clsx';
 import { makeStyles, MenuItem, IconButton, Link } from '@material-ui/core';
 import {
@@ -57,11 +57,13 @@ function PlotOptions(props) {
   const { options } = props;
   const [open, setOpen] = useState(false);
   const classes = useStyles();
+
+  const buttonIcon = useMemo(() => (<SettingsIconWithArrow open={open} />), [open]);
   return (options ? (
     <PopperMenu
       open={open}
       setOpen={setOpen}
-      buttonIcon={<SettingsIconWithArrow open={open} />}
+      buttonIcon={buttonIcon}
       buttonClassName={classes.iconButton}
       placement="bottom-end"
       aria-label="Open plot options menu"
@@ -84,17 +86,18 @@ function DownloadOptions(props) {
   const { urls } = props;
   const [open, setOpen] = useState(false);
   const classes = useStyles();
+  const buttonIcon = useMemo(() => (<CloudDownloadIconWithArrow open={open} />), [open]);
   return (urls && urls.length ? (
     <PopperMenu
       open={open}
       setOpen={setOpen}
-      buttonIcon={<CloudDownloadIconWithArrow open={open} />}
+      buttonIcon={buttonIcon}
       buttonClassName={classes.iconButton}
       placement="bottom-end"
       aria-label="Open download options menu"
     >
       {urls.map(({ url, name }) => (
-        <MenuItem dense key={`${url}_${name}`} getArialLabel={() => `Click to download ${name}`}>
+        <MenuItem dense key={`${url}_${name}`} aria-label={`Click to download ${name}`}>
           <Link underline="always" href={url} target="_blank" rel="noopener" className={classes.downloadLink}>
             Download {name}
           </Link>
