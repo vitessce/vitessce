8b78bf5dbe91b3c888cd210454351f68548118ca
Eric Moerth <ericmoerth@g.harvard.edu>
2024-01-21 15:27:32 -0800

    Adding second test dataset

---
+++ examples/configs/src/view-configs/3D-maps/ThreeMinimalLight.js
@@ -0,0 +1,57 @@
+import {
+    VitessceConfig,
+    CoordinationLevel as CL,
+    hconcat, vconcat,
+} from '@vitessce/config';
+
+
+function generateThreeMinimalConfiguration() {
+    const config = new VitessceConfig({
+        schemaVersion: '1.0.16',
+        name: 'Minimal Three',
+    });
+    const dataset = config.addDataset('My dataset').addFile({
+        fileType: 'image.ome-tiff',
+        url: "https://assets.hubmapconsortium.org/30bc1823e0c19be58557fb979499bac2/ometiff-pyramids/data/3D_image_stack.ome.tif?token=",
+        coordinationValues: {
+            fileUid: 'kidney',
+        },
+    });
+
+    const spatialThreeView = config.addView(dataset, 'spatialThree');
+    const lcView = config.addView(dataset, 'layerControllerBeta');
+    config.linkViewsByObject([spatialThreeView, lcView], {
+        spatialTargetZ: 0,
+        spatialTargetT: 0,
+        imageLayer: CL([
+            {
+                fileUid: 'kidney',
+                spatialLayerOpacity: 1,
+                spatialTargetResolution: null,
+                imageChannel: CL([
+                    {
+                        spatialTargetC: 0,
+                        spatialChannelColor: [255, 0, 0],
+                        spatialChannelVisible: true,
+                        spatialChannelOpacity: 1.0,
+                        spatialChannelWindow: null,
+                    },
+                    {
+                        spatialTargetC: 1,
+                        spatialChannelColor: [0, 255, 0],
+                        spatialChannelVisible: true,
+                        spatialChannelOpacity: 1.0,
+                        spatialChannelWindow: null,
+                    },
+                ]),
+            },
+        ])
+    });
+
+    config.layout(hconcat(spatialThreeView, lcView));
+
+    const configJSON = config.toJSON();
+    return configJSON;
+}
+
+export const threeMinimal = generateThreeMinimalConfiguration();
