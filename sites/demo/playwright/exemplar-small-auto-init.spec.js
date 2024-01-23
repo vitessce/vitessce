86f3dcf679ad73bd2a8119d648dbfbc1099dcd63
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2024-01-23 14:24:22 -0500

    Playwright visual comparison

---
+++ sites/demo/playwright/exemplar-small-auto-init.spec.js
@@ -0,0 +1,13 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('auto-initialization should occur for images and segmentations', async ({ page }) => {
+
+  await page.goto('http://localhost:3000/?dataset=exemplar-small');
+
+  // Expect a page title.
+  await expect(page).toHaveTitle("Vitessce");
+
+  // Check the screenshot to determine whether auto-initialization was successful.
+  await expect(page).toHaveScreenshot('exemplar-small-auto-init.png');
+});
