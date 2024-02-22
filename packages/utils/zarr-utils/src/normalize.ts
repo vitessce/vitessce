001a2c1177826ab40ac17d070a7138f535b7c9dc
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2024-02-22 17:45:46 -0500

    Support zipped zarr stores

--- packages/utils/zarr-utils/src/normalize.ts
+++ packages/utils/zarr-utils/src/normalize.ts
@@ -1,10 +1,16 @@
 // Adapted from https://github.com/hms-dbmi/vizarr/blob/5b0e3ea6fbb42d19d0e38e60e49bb73d1aca0693/src/utils.ts#L26
 import type { Readable } from '@zarrita/storage';
 import { root as zarrRoot, FetchStore } from 'zarrita';
+import { ZipFileStore } from '@zarrita/storage';
 
 
 export function zarrOpenRoot(url: string, requestInit: RequestInit) {
-  const store: Readable = new FetchStore(url, { overrides: requestInit });
+  let store: Readable;
+  if(url.endsWith('.zip')) {
+    store = ZipFileStore.fromUrl(url);
+  } else {
+    store = new FetchStore(url, { overrides: requestInit });
+  }
 
   // Wrap remote stores in a cache
   return zarrRoot(store);
