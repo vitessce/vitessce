764375ed082ac92839d90e810dce23aba302e029
Eric Moerth <ericmoerth@g.harvard.edu>
2024-04-30 19:28:01 -0400

    Working on getting the LinkController to handle the Config Update via Websockets

--- packages/view-types/link-controller/src/LinkControllerSubscriber.js
+++ packages/view-types/link-controller/src/LinkControllerSubscriber.js
@@ -1,10 +1,9 @@
 /* eslint-disable dot-notation */
 /* eslint-disable no-unused-vars */
 import React, {
-    useCallback, useRef, forwardRef,
+    useState, useEffect,
 } from 'react';
-import {Grid} from '@material-ui/core';
-import {TitleInfo, useGridItemSize, useReady} from "@vitessce/vit-s";
+import {TitleInfo, useGridItemSize} from "@vitessce/vit-s";
 import LinkController from "./LinkController.js";
 
 export function LinkControllerSubscriber(props) {
