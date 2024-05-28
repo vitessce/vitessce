606558642b3881d89d3570ae8c039c063a002f17
ilan-gold <ilanbassgold@gmail.com>
2024-05-28 15:06:30 +0200

    (feat): add lazy array cache

--- packages/file-types/anndata/src/axis_arrays.ts
+++ packages/file-types/anndata/src/axis_arrays.ts
@@ -1,35 +1,45 @@
 import * as zarr from "zarrita";
 import { LazyCategoricalArray, readSparse } from "./utils";
 
-import type { AxisKey, IntType } from "./types";
+import type { AxisKey, BackedArray, IntType } from "./types";
 import { Readable } from "@zarrita/storage";
 import SparseArray from "./sparse_array";
 
 export default class AxisArrays<S extends Readable> {
   public root: zarr.Location<S>;
+  private cache: Map<string, BackedArray>
 
   public constructor(root: zarr.Group<S>, axisKey: AxisKey) {
     this.root = root.resolve(axisKey);
+    this.cache = new Map();
   }
 
-  public async get(key: string): Promise<zarr.Array<zarr.DataType, Readable> | SparseArray<zarr.NumberDataType> | LazyCategoricalArray<IntType, zarr.DataType, Readable>> {
-    // categories needed for backward compat
-    const keyRoot = this.root.resolve(key);
-    const keyNode = await zarr.open(keyRoot);
-    const { categories, "encoding-type": encodingType } =
-      (await keyNode.attrs) as any;
-    if (categories != undefined) {
-      const cats = await zarr.open(this.root.resolve(categories), { kind: "array" });
-      return new LazyCategoricalArray((keyNode as zarr.Array<IntType, S>), cats);
+  public async get(key: string): Promise<BackedArray> {
+    if (!this.cache.has(key)) {
+      // categories needed for backward compat
+      const keyRoot = this.root.resolve(key);
+      const keyNode = await zarr.open(keyRoot);
+      const { categories, "encoding-type": encodingType } =
+        (await keyNode.attrs) as any;
+      if (categories != undefined) {
+        const cats = await zarr.open(this.root.resolve(categories), { kind: "array" });
+        this.cache.set(key, new LazyCategoricalArray((keyNode as zarr.Array<IntType, S>), cats))
+      }
+      else if (encodingType === "categorical") {
+        const cats = await zarr.open(keyRoot.resolve('categories'), { kind: "array" });
+        const codes = await zarr.open(keyRoot.resolve('codes'), { kind: "array" }) as zarr.Array<IntType, Readable>;
+        this.cache.set(key, new LazyCategoricalArray(codes, cats))
+      }
+      else if (["csc_format", "csr_format"].includes(encodingType)) {
+        this.cache.set(key, await readSparse(keyNode as zarr.Group<Readable>))
+      } else {
+        this.cache.set(key, (keyNode as zarr.Array<zarr.DataType, S>));
+      }
     }
-    if (encodingType === "categorical") {
-      const cats = await zarr.open(keyRoot.resolve('categories'), { kind: "array" });
-      const codes = await zarr.open(keyRoot.resolve('codes'), { kind: "array" }) as zarr.Array<IntType, Readable>;
-      return new LazyCategoricalArray(codes, cats);
+    const val = this.cache.get(key)
+    if (val === undefined) {
+      throw new Error("See https://github.com/microsoft/TypeScript/issues/13086 for why this will never happen")
     }
-    if (["csc_format", "csr_format"].includes(encodingType)) {
-      return readSparse(keyNode as zarr.Group<Readable>)
-    }
-    return (keyNode as zarr.Array<zarr.DataType, S>);
+    return val
   }
 }
--- packages/file-types/anndata/src/types.ts
+++ packages/file-types/anndata/src/types.ts
@@ -1,4 +1,7 @@
+import { Readable } from '@zarrita/storage';
 import * as zarr from 'zarrita';
+import SparseArray from './sparse_array';
+import { LazyCategoricalArray } from './utils';
 
 export const AxisKeys = ["obs", "var", "obsm", "varm", "X", "layers", "obsp", "varp"] as const;
 export type AxisKey = (typeof AxisKeys)[number]
@@ -7,3 +10,4 @@ export type AxisSelection = (number | Slice | null)
 export type FullSelection = AxisSelection[]
 export type IntType = zarr.Int8 | zarr.Int16 | zarr.Int32
 export type ArrayType = Exclude<zarr.DataType, zarr.ObjectType>
+export type BackedArray = zarr.Array<zarr.DataType, Readable> | SparseArray<zarr.NumberDataType> | LazyCategoricalArray<IntType, zarr.DataType, Readable>
