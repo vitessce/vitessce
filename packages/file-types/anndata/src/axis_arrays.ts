1f644365dc2506d6615171f5aa35349e846ccae8
ilan-gold <ilanbassgold@gmail.com>
2024-05-28 14:47:27 +0200

    (fix): lazy categorical typing

--- packages/file-types/anndata/src/axis_arrays.ts
+++ packages/file-types/anndata/src/axis_arrays.ts
@@ -1,7 +1,7 @@
 import * as zarr from "zarrita";
 import { LazyCategoricalArray, readSparse } from "./utils";
 
-import type { AxisKey } from "./types";
+import type { AxisKey, IntType } from "./types";
 import { Readable } from "@zarrita/storage";
 import SparseArray from "./sparse_array";
 
@@ -12,7 +12,7 @@ export default class AxisArrays<S extends Readable> {
     this.root = root.resolve(axisKey);
   }
 
-  public async get(key: string): Promise<zarr.Array<zarr.DataType, Readable> | SparseArray<zarr.NumberDataType> | LazyCategoricalArray<zarr.NumberDataType, zarr.DataType, Readable>> {
+  public async get(key: string): Promise<zarr.Array<zarr.DataType, Readable> | SparseArray<zarr.NumberDataType> | LazyCategoricalArray<IntType, zarr.DataType, Readable>> {
     // categories needed for backward compat
     const keyRoot = this.root.resolve(key);
     const keyNode = await zarr.open(keyRoot);
@@ -20,11 +20,11 @@ export default class AxisArrays<S extends Readable> {
       (await keyNode.attrs) as any;
     if (categories != undefined) {
       const cats = await zarr.open(this.root.resolve(categories), { kind: "array" });
-      return new LazyCategoricalArray((keyNode as zarr.Array<zarr.NumberDataType, S>), cats);
+      return new LazyCategoricalArray((keyNode as zarr.Array<IntType, S>), cats);
     }
     if (encodingType === "categorical") {
       const cats = await zarr.open(keyRoot.resolve('categories'), { kind: "array" });
-      const codes = await zarr.open(keyRoot.resolve('codes'), { kind: "array" }) as zarr.Array<zarr.NumberDataType, Readable>;
+      const codes = await zarr.open(keyRoot.resolve('codes'), { kind: "array" }) as zarr.Array<IntType, Readable>;
       return new LazyCategoricalArray(codes, cats);
     }
     if (["csc_format", "csr_format"].includes(encodingType)) {
