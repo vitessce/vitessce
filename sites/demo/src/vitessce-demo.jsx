dc4e01e233409d71ef167773c6d4ca0fbd39726b
Eric Moerth <ericmoerth@g.harvard.edu>
2024-06-11 17:52:01 -0400

    Enroute to get to AWS Websocket Service

--- sites/demo/src/vitessce-demo.jsx
+++ sites/demo/src/vitessce-demo.jsx
@@ -1,35 +1,36 @@
 /* eslint-disable no-console */
 import React, {
-    useEffect, useRef, useState, useMemo,
+  useEffect, useRef, useState, useMemo,
 } from 'react';
-import {Vitessce} from 'vitessce';
+import { Vitessce } from 'vitessce';
 
-import {getConfig, listConfigs, getPlugins} from './api.js';
-import {Welcome} from './welcome.jsx';
-import {Warning} from './warning.jsx';
+import { getConfig, listConfigs, getPlugins } from './api.js';
+import { Welcome } from './welcome.jsx';
+import { Warning } from './warning.jsx';
 import PieSocket from 'piesocket-js';
 
 import './index.scss';
-import {setConfig} from "isomorphic-git";
+import { setConfig } from 'isomorphic-git';
 
 function AwaitResponse(props) {
-    const {
-        response,
-        theme,
-    } = props;
-    const [isLoading, setIsLoading] = useState(true);
-    const responseRef = useRef();
-    useEffect(() => {
-        response.then((c) => {
-            responseRef.current = c;
-            setIsLoading(false);
-        });
-    }, [response]);
-    return (!isLoading ? React.createElement(responseRef.current) : <Warning title="Loading..." theme={theme}/>);
+  const {
+    response,
+    theme,
+  } = props;
+  const [isLoading, setIsLoading] = useState(true);
+  const responseRef = useRef();
+  useEffect(() => {
+    response.then((c) => {
+      responseRef.current = c;
+      setIsLoading(false);
+    });
+  }, [response]);
+  return (!isLoading ? React.createElement(responseRef.current) :
+    <Warning title="Loading..." theme={theme}/>);
 }
 
 function preformattedDetails(response) {
-    return `
+  return `
     ok: ${response.ok}
     status: ${response.status}
     statusText: ${response.statusText}
@@ -39,48 +40,49 @@ function preformattedDetails(response) {
 }
 
 function logConfigUpgrade(prevConfig, nextConfig) {
-    // eslint-disable-next-line no-console
-    console.log(`Upgrade view config schema from ${prevConfig.version} to ${nextConfig.version}`);
-    // eslint-disable-next-line no-console
-    console.log(prevConfig);
-    // eslint-disable-next-line no-console
-    console.log(nextConfig);
+  // eslint-disable-next-line no-console
+  console.log(`Upgrade view config schema from ${prevConfig.version} to ${nextConfig.version}`);
+  // eslint-disable-next-line no-console
+  console.log(prevConfig);
+  // eslint-disable-next-line no-console
+  console.log(nextConfig);
 }
 
 function checkResponse(response, theme, debug) {
-    if (!response.ok) {
-        return Promise.resolve(
-            () => (
-                <Warning
-                    title="Fetch response not OK"
-                    preformatted={preformattedDetails(response)}
-                    theme={theme}
-                />
-            ),
-        );
-    }
-    return response.text().then((text) => {
-        try {
-            const config = JSON.parse(text);
-            return Promise.resolve(() => (
-                <Vitessce
-                    config={config}
-                    theme={theme}
-                    onConfigChange={debug ? console.log : undefined}
-                    onConfigUpgrade={debug ? logConfigUpgrade : undefined}
-                    validateOnConfigChange={debug}
-                />
-            ));
-        } catch (e) {
-            return Promise.resolve(() => (
-                <Warning
-                    title="Error parsing JSON"
-                    preformatted={preformattedDetails(response)}
-                    unformatted={`${e.message}: ${text}`}
-                    theme={theme}
-                />
-            ));
-        }
+  if (!response.ok) {
+    return Promise.resolve(
+      () => (
+        <Warning
+          title="Fetch response not OK"
+          preformatted={preformattedDetails(response)}
+          theme={theme}
+        />
+      ),
+    );
+  }
+  return response.text()
+    .then((text) => {
+      try {
+        const config = JSON.parse(text);
+        return Promise.resolve(() => (
+          <Vitessce
+            config={config}
+            theme={theme}
+            onConfigChange={debug ? console.log : undefined}
+            onConfigUpgrade={debug ? logConfigUpgrade : undefined}
+            validateOnConfigChange={debug}
+          />
+        ));
+      } catch (e) {
+        return Promise.resolve(() => (
+          <Warning
+            title="Error parsing JSON"
+            preformatted={preformattedDetails(response)}
+            unformatted={`${e.message}: ${text}`}
+            theme={theme}
+          />
+        ));
+      }
     });
 }
 
@@ -90,118 +92,162 @@ function checkResponse(response, theme, debug) {
  * @returns {string} A valid theme name.
  */
 function validateTheme(theme) {
-    return (['light', 'dark'].includes(theme) ? theme : 'dark');
+  return (['light', 'dark'].includes(theme) ? theme : 'dark');
 }
 
 export function VitessceDemo() {
-    //Saving Config and sharing Via Websockets
-    const [ws, setWS] = useState(undefined);
-    const [channel, setChannel] = useState(undefined);
-    const [config, setConfig] = useState(undefined);
+  //Saving Config and sharing Via Websockets
+  const [socket, setSocket] = useState(undefined);
+  const [channel, setChannel] = useState(undefined);
+  const [socketOpen, setSocketOpen] = useState(false);
+  const [config, setConfig] = useState(undefined);
 
+  const urlParams = new URLSearchParams(window.location.search);
+  const websocket = urlParams.get('ws') === 'true';
+  const spaceID = urlParams.get('code');
+  const id = 'id_' + (Math.floor(Math.random() * 1000));
+  const send = urlParams.get('send') === 'true';
+
+
+  const connection = useRef(null);
+  if (websocket) {
+    var authToken = 'mr-vitessce';
+    useEffect(() => {
+      const socket = new WebSocket('wss://irrmj4anbk.execute-api.us-east-1.amazonaws.com/production', ['Authorization',authToken,spaceID]);
+      console.log(socket);
+      // Connection opened
+      socket.addEventListener('open', (event) => {
+        console.log('Connection established');
+        socket.send('test');
+        setSocketOpen(true);
+      });
+      socket.addEventListener('error', (event) => {
+        console.log("Error ", event)
+      });
+      socket.addEventListener('close', (event) => {
+        console.log("Close ", event)
+      });
+      // Listen for messages
+      socket.addEventListener('message', (event) => {
+        if (event.data.includes(';')) {
+          if (event.data.split(';')[0] == id) {
+            console.log('Message from ourselves');
+          } else {
+            console.log('Message from server ');
+            setConfig({
+              ...JSON.parse(atob(event.data.split(';')[1])),
+              uid: 'id' + (Math.floor(Math.random() * 1000))
+            });
+          }
+        }
+      });
+      connection.current = socket;
+      return () => connection.current.close();
+    }, []);
+  }
+
+  //if (websocket) {
+  //   useEffect(() => {
+  //      if (socket === undefined) {
+  //
+  //         //document.cookie = 'Authorization: ' + authToken + '; path=/';
+  //         let socketInit = new WebSocket("wss://irrmj4anbk.execute-api.us-east-1.amazonaws.com/production", authToken);
+  //         setSocket(socketInit);
+  //        socketInit.onopen = (event) => {
+  //           console.log("Socket is open", event)
+  //        };
+  //       socketInit.onmessage = (event) => {
+  //           console.log(event.data);
+  //       };
+  //   }
+  //}, [socket])
+
+  //useEffect(() => {
+
+  //     ws?.onmessage = (event) => {
+  //         console.log(event.data);
+  //     };
+  //     ws?.subscribe(channelID).then((chan) => {
+  //         // console.log("Channel is ready")
+  //         chan.listen("new_message", (data, meta) => {
+  //             // console.log(data.sender, ws?.options.userId)
+  //             if (data.sender !== ws?.options.userId) {
+  //                 // console.log("New Message:", data);
+  //                 setConfig({...data.message, uid: "id" + (Math.floor(Math.random() * 1000))});
+  //             }
+  //         })
+  //         chan.listen("system:member_joined", function (data) {
+  //             console.log("New member joined the chat " + data.member.user);
+  //         })
+  //         setChannel(chan);
+  //     })
+  // }, [channel, socket])
+  //}
+
+  const result = useMemo(() => {
+    const { rowHeight = null } = {};
     const urlParams = new URLSearchParams(window.location.search);
-    const websocket = urlParams.get('ws') === 'true';
-    const channelID = urlParams.get('code');
-    const send = urlParams.get('send') === 'true';
+    const datasetId = urlParams.get('dataset');
+    const debug = urlParams.get('debug') === 'true';
+    const datasetUrl = urlParams.get('url');
+    const showAll = urlParams.get('show') === 'all';
+    const theme = validateTheme(urlParams.get('theme'));
+    const isBounded = urlParams.get('isBounded') === 'true';
+    const strictMode = urlParams.get('strictMode') === 'true';
 
-    if (websocket) {
-        useEffect(() => {
-            if (ws === undefined) {
-                var authToken = 'mr-vitessce';
-                document.cookie = 'Authorization: ' + authToken + '; path=/';
-                let socket = new WebSocket("wss://irrmj4anbk.execute-api.us-east-1.amazonaws.com/production");
-                setWS(socket);
-                socket.onopen = (event) => {
-                    socket.send("Here's some text that the server is urgently awaiting!");
-                };
-            }
-        }, [ws])
+    const ContainerComponent = strictMode ? React.StrictMode : React.Fragment;
 
-        // useEffect(() => {
-        //     ws?.subscribe(channelID).then((chan) => {
-        //         // console.log("Channel is ready")
-        //         chan.listen("new_message", (data, meta) => {
-        //             // console.log(data.sender, ws?.options.userId)
-        //             if (data.sender !== ws?.options.userId) {
-        //                 // console.log("New Message:", data);
-        //                 setConfig({...data.message, uid: "id" + (Math.floor(Math.random() * 1000))});
-        //             }
-        //         })
-        //         chan.listen("system:member_joined", function (data) {
-        //             console.log("New member joined the chat " + data.member.user);
-        //         })
-        //         setChannel(chan);
-        //     })
-        //}, [channel, ws])
+    if (datasetId) {
+      const configFromDataSetId = getConfig(datasetId);
+      if (config === undefined) {
+        setConfig(configFromDataSetId);
+      }
+      const pluginProps = getPlugins(datasetId);
+      // console.log("Getting in with a new Config ", config)
+      return (
+        <ContainerComponent>
+          <Vitessce
+            config={config !== undefined ? config : configFromDataSetId}
+            rowHeight={rowHeight}
+            theme={theme}
+            onConfigChange={(configValue) => {
+              if (send && socketOpen) {
+                let configString = id + ';' + btoa(JSON.stringify(configValue));
+                connection.current?.send('{"action": "sendMessage","message": "' + configString + '"}');
+              }
+            }}
+            onConfigUpgrade={(configValue) => {
+              //console.log("Upgrade of Config inside:", configValue)
+            }}
+            validateOnConfigChange={debug}
+            isBounded={isBounded}
+            {...pluginProps}
+          />
+        </ContainerComponent>
+      );
     }
-
-    const result = useMemo(() => {
-        const {rowHeight = null} = {};
-        const urlParams = new URLSearchParams(window.location.search);
-        const datasetId = urlParams.get('dataset');
-        const debug = urlParams.get('debug') === 'true';
-        const datasetUrl = urlParams.get('url');
-        const showAll = urlParams.get('show') === 'all';
-        const theme = validateTheme(urlParams.get('theme'));
-        const isBounded = urlParams.get('isBounded') === 'true';
-        const strictMode = urlParams.get('strictMode') === 'true';
-
-        const ContainerComponent = strictMode ? React.StrictMode : React.Fragment;
-
-        if (datasetId) {
-            const configFromDataSetId = getConfig(datasetId);
-            if (config === undefined) {
-                setConfig(configFromDataSetId);
-            }
-            const pluginProps = getPlugins(datasetId);
-            // console.log("Getting in with a new Config ", config)
-            return (
-                <ContainerComponent>
-                    <Vitessce
-                        config={config !== undefined ? config : configFromDataSetId}
-                        rowHeight={rowHeight}
-                        theme={theme}
-                        onConfigChange={(configValue) => {
-                            if(send) {
-                                // console.log("Sending Config after Inside Change:", configValue);
-                                channel?.publish("new_message", {
-                                    sender: ws ? ws.options.userId : 0,
-                                    message: configValue,
-                                })
-                            }
-                        }}
-                        onConfigUpgrade={(configValue) => {
-                            //console.log("Upgrade of Config inside:", configValue)
-                        }}
-                        validateOnConfigChange={debug}
-                        isBounded={isBounded}
-                        {...pluginProps}
-                    />
-                </ContainerComponent>
-            );
-        }
-        if (datasetUrl) {
-            const responsePromise = fetch(datasetUrl)
-                .then(response => checkResponse(response, theme, debug))
-                .catch(error => Promise.resolve(() => (
-                    <Warning
-                        title="Error fetching"
-                        unformatted={error.message}
-                        theme={theme}
-                    />
-                )));
-            return (
-                <ContainerComponent>
-                    <AwaitResponse response={responsePromise} theme={theme}/>
-                </ContainerComponent>
-            );
-        }
-        const configs = listConfigs(showAll);
-        return (<Welcome configs={configs} theme={theme}/>);
-    }, [window.location.search, channel, config, ws]);
-    return (
-        <>
-            <style>{`
+    if (datasetUrl) {
+      const responsePromise = fetch(datasetUrl)
+        .then(response => checkResponse(response, theme, debug))
+        .catch(error => Promise.resolve(() => (
+          <Warning
+            title="Error fetching"
+            unformatted={error.message}
+            theme={theme}
+          />
+        )));
+      return (
+        <ContainerComponent>
+          <AwaitResponse response={responsePromise} theme={theme}/>
+        </ContainerComponent>
+      );
+    }
+    const configs = listConfigs(showAll);
+    return (<Welcome configs={configs} theme={theme}/>);
+  }, [window.location.search, channel, config, socket, socketOpen]);
+  return (
+    <>
+      <style>{`
       html, body {
         height: 100%;
       }
@@ -225,8 +271,8 @@ export function VitessceDemo() {
         overflow: hidden;
       }
       `}
-            </style>
-            {result}
-        </>
-    );
+      </style>
+      {result}
+    </>
+  );
 }
