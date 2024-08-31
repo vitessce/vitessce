f30c1dd23f012b25950c789d8bf5e88d8b41b588
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2024-08-31 17:15:42 -0400

    Update

--- packages/schemas/src/view-config-utils.test.ts
+++ packages/schemas/src/view-config-utils.test.ts
@@ -34,13 +34,13 @@ describe('src/app/view-config-utils.js', () => {
       expect(upgradeFrom1_0_15(implicitPerDatasetCoordinations))
         .toEqual(explicitPerDatasetCoordinations);
     });
-    it('upgrades more than once', () => {
-      const latestConfig = upgradeAndParse(legacyViewConfig1_0_0);
-      expect(latestConfig.version).toEqual('1.0.16');
-    });
     it('upgrade view config from v1.0.16 to v1.0.17', () => {
       expect(upgradeFrom1_0_16(annDataOptionsArrays))
         .toEqual(annDataOptionsObjects);
+    });
+    it('upgrades more than once', () => {
+      const latestConfig = upgradeAndParse(legacyViewConfig1_0_0);
+      expect(latestConfig.version).toEqual('1.0.17');
     });
   });
 });
