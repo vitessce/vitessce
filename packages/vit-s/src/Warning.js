9e6095e95c9dc3d5601471ec23af7d9e74c57861
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2025-01-29 22:11:42 -0500

    convert warning styles

--- packages/vit-s/src/Warning.js
+++ packages/vit-s/src/Warning.js
@@ -1,43 +1,44 @@
-import clsx from 'clsx';
-import { makeStyles } from '@mui/styles';
 import { VITESSCE_CONTAINER } from './classNames.js';
+import { styled } from '@mui/material-pigment-css'
 
-const useStyles = makeStyles(theme => ({
-  warningLayout: {
-    backgroundColor: theme.palette.gridLayoutBackground,
-    position: 'absolute',
-    width: '100%',
-    height: '100vh',
-  },
-  containerFluid: {
-    width: '100%',
-    padding: '15px',
-    marginRight: 'auto',
-    marginLeft: 'auto',
-    boxSizing: 'border-box',
-    display: 'flex',
-  },
-  row: {
-    flexGrow: '1',
-  },
-  warningCard: {
-    border: `1px solid ${theme.palette.cardBorder}`,
-    flex: '1 1 auto',
-    minHeight: '1px',
-    padding: '12px',
-    marginTop: '8px',
-    marginBottom: '8px',
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'column',
-    minWidth: '0',
-    wordWrap: 'break-word',
-    backgroundClip: 'border-box',
-    borderRadius: '4px',
-    backgroundColor: theme.palette.primaryBackground,
-    color: theme.palette.primaryForeground,
-  },
+const WarningLayout = styled('div')({
+  backgroundColor: theme.palette.gridLayoutBackground,
+  position: 'absolute',
+  width: '100%',
+  height: '100vh',
+});
+
+const FluidWarningLayout = styled(WarningLayout)({
+  width: '100%',
+  padding: '15px',
+  marginRight: 'auto',
+  marginLeft: 'auto',
+  boxSizing: 'border-box',
+  display: 'flex',
+})
+
+const Row = styled('div')({
+  flexGrow: '1',
+});
+
+const WarningCard = styled('div')(({ theme }) => ({
+  border: `1px solid ${theme.palette.cardBorder}`,
+  flex: '1 1 auto',
+  minHeight: '1px',
+  padding: '12px',
+  marginTop: '8px',
+  marginBottom: '8px',
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  minWidth: '0',
+  wordWrap: 'break-word',
+  backgroundClip: 'border-box',
+  borderRadius: '4px',
+  backgroundColor: theme.palette.primaryBackground,
+  color: theme.palette.primaryForeground,
 }));
+
 
 export function Warning(props) {
   const {
@@ -45,18 +46,17 @@ export function Warning(props) {
     preformatted,
     unformatted,
   } = props;
-  const classes = useStyles();
   return (
     <div className={VITESSCE_CONTAINER}>
-      <div className={clsx(classes.warningLayout, classes.containerFluid)}>
-        <div className={classes.row}>
-          <div className={classes.warningCard}>
+      <FluidWarningLayout>
+        <Row>
+          <WarningCard>
             <h1>{title}</h1>
             {preformatted ? (<pre>{preformatted}</pre>) : null}
             <p>{unformatted}</p>
-          </div>
-        </div>
-      </div>
+          </WarningCard>
+        </Row>
+        </FluidWarningLayout>
     </div>
   );
 }
