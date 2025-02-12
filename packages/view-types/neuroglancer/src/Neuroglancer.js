479ea3af8bd64708ccf04c145150b082072d9eda
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-02-12 14:52:46 -0500

    Styling refactoring

--- packages/view-types/neuroglancer/src/Neuroglancer.js
+++ packages/view-types/neuroglancer/src/Neuroglancer.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { NeuroglancerViewer } from 'vitessce-react-neuroglancer';
+import { useStyles } from './styles.js';
 
-export function Neuroglancer({ viewerState, onViewerStateChanged }) {
+export function Neuroglancer(props) {
+  const { viewerState, onViewerStateChanged } = props;
+  const classes = useStyles();
   const [updatedState, setUpdatedState] = useState(viewerState);
-  function handleStateChanged(newState) {
+  
+  const handleStateChanged = useCallback((newState) => {
     if (JSON.stringify(newState) !== JSON.stringify(updatedState)) {
-      onViewerStateChanged(newState);
+      if(onViewerStateChanged) {
+        onViewerStateChanged(newState);
+      }
     }
-  }
+  }, [onViewerStateChanged, updatedState]);
   const changeLayout = () => {
     setUpdatedState(prevState => ({
       ...prevState,
@@ -16,12 +22,19 @@ export function Neuroglancer({ viewerState, onViewerStateChanged }) {
   };
   return (
     <>
+      <style>{`
+      .neuroglancer-segment-list-header-label {
+        display: none !important;
+      }
+      `}</style>
       {/* Test button to change the layout and get the updated state */}
       <button type="button" onClick={changeLayout} style={{ width: '10%', color: '#333' }}>Change layout</button>
-      <NeuroglancerViewer
-        viewerState={updatedState}
-        onViewerStateChanged={handleStateChanged}
-      />
+      <div className={classes.neuroglancerWrapper}>
+        <NeuroglancerViewer
+          viewerState={updatedState}
+          onViewerStateChanged={handleStateChanged}
+        />
+      </div>
     </>
   );
 }
