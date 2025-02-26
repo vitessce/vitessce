f27bdd037ca67e4b620d0bbc5590f86f5f91f7ad
tkakar <tabassum_kakar@hms.harvard.edu>
2025-02-26 16:05:26 -0500

    Tkakar/CAT-1107-Create-Neuroglancer-View (#2033)
    Co-authored-by: Mark Keller <7525285+keller-mark@users.noreply.github.com>

---
+++ .changeset/chatty-geckos-hunt.md
@@ -0,0 +1,9 @@
+---
+"@vitessce/neuroglancer": patch
+"@vitessce/constants-internal": patch
+"@vitessce/all": patch
+"@vitessce/example-configs": patch
+"demo": patch
+---
+
+Added Neuroglancer component as vitessce view
--- examples/configs/src/index.js
+++ examples/configs/src/index.js
@@ -70,6 +70,7 @@ import { humanLiver } from './view-configs/3d-maps/human-liver.js';
 import { bloodVessel } from './view-configs/3d-maps/blood-vessel.js';
 import { bloodVesselNamed } from './view-configs/3d-maps/blood-vessel-named.js';
 import { bloodVesselNeighborhood } from './view-configs/3d-maps/blood-vessel-neighborhood.js';
+import { melanomaNeuroglancer } from './view-configs/3d-maps/melanoma-neuroglancer.js';
 import { sorgerBiggerNeighborhood } from './view-configs/3d-maps/sorger-bigger.js';
 import { cellNeighborhood } from './view-configs/3d-maps/cell-neighborhood-named.js';
 import { threeMinimal } from './view-configs/3d-maps/three-minimal.js';
@@ -176,7 +177,7 @@ export const configs = {
   'hakimian-2021': threeMinimalLight,
   'link-controller': linkControllerDemo,
   'link-controller-minimal': linkControllerMinimal,
-
+  'melanoma-neuroglancer': melanomaNeuroglancer,
   // Nature Methods figures
   'figure-cite-seq': citeSeq,
   'figure-codex': codex,
