1da4f049f0da5d0d6690805537225554e67a323b
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-03-05 09:33:13 -0500

    Update

--- packages/utils/zarr-utils/src/normalize.ts
+++ packages/utils/zarr-utils/src/normalize.ts
@@ -1,12 +1,35 @@
 // Adapted from https://github.com/hms-dbmi/vizarr/blob/5b0e3ea6fbb42d19d0e38e60e49bb73d1aca0693/src/utils.ts#L26
-import { root as zarrRoot, FetchStore, type Readable } from 'zarrita';
+import { root as zarrRoot, FetchStore, type Readable, type AbsolutePath } from 'zarrita';
 import ZipFileStore from '@zarrita/storage/zip';
 import ReferenceStore from '@zarrita/storage/ref';
+import { getDebugMode } from '@vitessce/globals';
 
 type ZarrOpenRootOptions = {
   requestInit?: RequestInit,
   refSpecUrl?: string,
 };
+
+class RelaxedFetchStore extends FetchStore {
+  // This allows returning `undefined` for 403 responses,
+  // as opposed to completely erroring.
+  // Needed due to https://github.com/manzt/zarrita.js/pull/212
+  // In the future, perhaps we could contribute a way to pass a
+  // custom error handling function or additional options
+  // to the zarrita FetchStore so that a subclass is not required.
+  async get(
+		key: AbsolutePath,
+		options: RequestInit = {},
+	): Promise<Uint8Array | undefined> {
+    try {
+      return await super.get(key, options);
+    } catch(e: any) {
+      if(getDebugMode()) {
+        throw e;
+      }
+      return undefined;
+    }
+	}
+}
 
 export function zarrOpenRoot(url: string, fileType: null | string, opts?: ZarrOpenRootOptions) {
   let store: Readable;
@@ -33,7 +56,7 @@ export function zarrOpenRoot(url: string, fileType: null | string, opts?: ZarrOp
     store = ReferenceStore.fromSpec(referenceSpecPromise,
       { target: url, overrides: opts?.requestInit });
   } else {
-    store = new FetchStore(url, { overrides: opts?.requestInit });
+    store = new RelaxedFetchStore(url, { overrides: opts?.requestInit });
   }
 
   // Wrap remote stores in a cache
