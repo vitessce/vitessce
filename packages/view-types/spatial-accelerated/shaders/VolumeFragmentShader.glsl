993df4387f9b63cd5140156c2312b515bc807050
alexandra27i <74194845+alexandra27i@users.noreply.github.com>
2025-04-23 10:50:58 -0400

    minimal cleanup

--- packages/view-types/spatial-accelerated/shaders/VolumeFragmentShader.glsl
+++ packages/view-types/spatial-accelerated/shaders/VolumeFragmentShader.glsl
@@ -176,7 +176,6 @@ ivec4 getBrickLocation(vec3 location, int targetRes, int channel) {
     // if resident, return x,y,z,resolution AND ADD TO LRU
 
     // return x,y,z,resolution (for size) -> maybe add negative values for nonres etc.
-    // gl_FragColor = vec4(float(currentRes),0,0,1);
     return ivec4(0,0,0,-1);
 }
 
@@ -211,16 +210,6 @@ void main(void) {
     }
     t_hit.x = max(t_hit.x, 0.0);
 
-    // how to query 
-    // uint ptint = texture(pageTableTex, vec3(0,0,0)).r;
-    // float pt = float(ptint) / 4294967295.0;
-    // float bc = texture(brickCacheTex, vec3(16.0/2048.0,16.0/2048.0,16.0/128.0)).r;
-
-    // gl_FragColor = vec4(0.1*pt, 0 , 0.1*bc, 1.0);
-    // gl_FragColor = vec4(u_color, 1.0);
-    // gl_FragColor = vec4(u_clim[0], u_clim[1], 0.0, 1.0);
-    // return;
-
     //STEP 3: Compute the step size to march through the volume grid
     ivec3 volumeTexSize = textureSize(brickCacheTex, 0);
     volumeTexSize = ivec3(voxelExtents);
@@ -231,6 +220,7 @@ void main(void) {
     // dt = max(0.5, dt);
     vec3 p = cameraCorrected + (t_hit.x + dt) * rayDir;
     // Most browsers do not need this initialization, but add it to be safe
+    gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
 
     // t_hit is between 0 and 3000
     // dt is around 0.0015
@@ -243,38 +233,16 @@ void main(void) {
 
     bool debug = false;
 
-    if (!debug) {
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
-    } else if (dt >= 0.1) {
-        gl_FragColor = vec4(worldSpaceCoords, 1.0);
-        return;
-    } else {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-        return;
-    }
 
     // Initialization of some variables
-    float max_val = 0.0;
     vec3 rgbCombo = vec3(0.0);
     float total = 0.0;
-    float x = gl_FragCoord.x/u_window_size.x;
-    float y = gl_FragCoord.y/u_window_size.y;
 
     ivec4 currentBrickLocation = ivec4(0,0,0,0);
-    float currentDt = dt;
-    int currentRes = 0;
-    float currentScale = 1.0;
-    int currentPositionIndex = 0;
-    float distanceToNextBrick = 0.0;
     ivec3 currentTargetResPTCoord = ivec3(0,0,0);
-    bool queryNewBrick = false;
 
     float alphaMultiplicator = 1.0;
     vec3 localPos = vec3(0.0);
-
-    vec3 brickStep = step * pow(2.0, float(lowestRes + targetResC0));
-
-    ivec3 currentBrickLocationPTCoord = normalizedToPTCoord(p, targetResC0);
 
     for (float t = t_hit.x; t < t_hit.y; t += dt) {
 
@@ -284,16 +252,6 @@ void main(void) {
         // p goes from 0 to 1
         p = min(p, vec3(1.0 - 0.00000028));
         p = max(p, vec3(0.00000028));
-
-        // add an if clause to adapt step sizes
-        // calculate p in target res pt coord
-        if (false) {
-            // query new getBrickLocation
-            // if brick location is the same, continue
-            // if different
-            // update currentBrickLocation
-            // update currentDt
-        }
 
         ivec4 brickCacheOffset = getBrickLocation(p, targetResC0, 0);
         
@@ -313,16 +271,9 @@ void main(void) {
         } else if (brickCacheOffset.w == -2) {
             // render minimum maximum
         }
-
-        // currentDt = dt;
-        // currentRes = brickCacheOffset.w;
-        // currentScale = pow(2.0, float(lowestRes) - float(brickCacheOffset.w));
         
         float scale = pow(2.0, float(lowestRes) - float(brickCacheOffset.w));
         localPos = fract(p * scale);
-
-        // gl_FragColor = vec4(localPos, 1.0);
-        // return;
 
         vec3 brickCacheCoord = vec3(
             (float(brickCacheOffset.x) * 32.0 + localPos.x * 32.0) / 2048.0,
@@ -344,7 +295,6 @@ void main(void) {
             && currentVoxelInBrick == newVoxelInBrick
             && reps >= 0) {
         
-            //rgbCombo += rgbComboAdd;
             total = val;
 
             total = clamp(total, 0.0, 1.0);
@@ -370,20 +320,10 @@ void main(void) {
             newBrickLocationPTCoord = normalizedToPTCoord(p, targetResC0);
             newVoxelInBrick = ivec3(fract( p * scale) * 32.0);
         }
-
-        vec3 brickSwitch = vec3(abs(newBrickLocationPTCoord - currentTargetResPTCoord));
-        vec3 voxelSwitch = vec3(abs(newVoxelInBrick - currentVoxelInBrick));
-        float bs = max(max(brickSwitch.x, brickSwitch.y), brickSwitch.z);
-        float vs = max(max(voxelSwitch.x, voxelSwitch.y), voxelSwitch.z);
-        // gl_FragColor = vec4(bs, vs, 0.0, 1.0);
-        // return;
         
         if (gl_FragColor.a > 0.99) {
             break;
         }
-
-        // check if a new p would exceed the distance to the next brick in target res
-        // if so -- set flag to true and dont increment p
     }
 
     // gl_FragDepth = distance(worldSpaceCoords,p)*u_physical_Pixel;
@@ -391,113 +331,3 @@ void main(void) {
     gl_FragColor.g = linear_to_srgb(gl_FragColor.g);
     gl_FragColor.b = linear_to_srgb(gl_FragColor.b);
 }
-
-
-/*
-    // For finding the settings for the MESH
-    //gl_FragColor = vec4(worldSpaceCoords.xyz,1.0); // COLORED BOX
-    //gl_FragColor = vec4(cameraCorrected.x,cameraCorrected.y,cameraCorrected.z,1.0); // CLOSEST PLANE
-    //gl_FragColor = vec4(rayDirUnnorm.x,rayDirUnnorm.y,rayDirUnnorm.z,1.0); // RAY DIRECTION (MAKES PLUS SHAPE)
-    //gl_FragColor = vec4(t_hit.x/1000.0, 0.0, t_hit.y/1000.0, 1.0); // intersect hits
-    //float depth_hit = t_hit.y - t_hit.x;
-    //gl_FragColor = vec4(depth_hit/100.0, 0.0, 0.0, 1.0); // intersect hits
-    //gl_FragColor = vec4(u_renderstyle==0?1.0:0.0, u_renderstyle==1?1.0:0.0, u_renderstyle==2?1.0:0.0, 1.0); // intersect hits
-    //gl_FragColor = vec4(alphaScale, dtScale, 0.0, 1.0);
-    //return;
-    // NOTE: image opacity scales dtScale, not alphaScale
-
-    //STEP 3: Compute the step size to march through the volume grid',
-    ivec3 volumeTexSize = textureSize(volumeTex, 0);
-    vec3 dt_vec = 1.0 / (vec3(volumeTexSize) * abs(rayDir));
-    float dt = min(dt_vec.x, min(dt_vec.y, dt_vec.z));
-    dt = max(0.5, dt);
-    float offset = wang_hash(int(gl_FragCoord.x + 640.0 * gl_FragCoord.y));
-    vec3 p = cameraCorrected + (t_hit.x + offset + dt) * rayDir;
-    // Most browsers do not need this initialization, but add it to be safe.',
-    gl_FragColor = vec4(0.0);
-    p = p / boxSize + vec3(0.5);
-    vec3 step = (rayDir * dt) / boxSize;
-
-    // Initialization of some variables.',
-    float max_val = 0.0;
-    float max_val2 = 0.0;
-    float max_val3 = 0.0;
-    float max_val4 = 0.0;
-    float max_val5 = 0.0;
-    float max_val6 = 0.0;
-    vec3 rgbCombo = vec3(0.0);
-    float total = 0.0;
-    int max_i = 30000;
-    int i = 0;
-    float x = gl_FragCoord.x/u_window_size.x;
-    float y = gl_FragCoord.y/u_window_size.y;
-    //vec3 meshPos = texture2D(u_stop_geom, vec2(x,y)).xyz;
-    // "  vec3 meshPos = texture2D(u_stop_geom, vec2(gl_FragCoord.x,gl_FragCoord.y)).xyz;",
-    //  "  gl_FragColor = vec4(meshPos,1.0);",
-    //  "  return;",
-    float alphaMultiplicator = 1.0;
-    for (float t = t_hit.x; t < t_hit.y; t += dt) {
-        vec3 rgbCombo = vec3(0.0);
-        float total   = 0.0;
-
-        float val = texture(pageTableTex, vec3(0,0,0)).r;
-        val = max(0.0, (val - u_clim[0]) / (u_clim[1] - u_clim[0]));
-        rgbCombo += max(0.0, min(1.0, val)) * u_color;
-        total    += val;
-
-        if (volumeCount > 1.0) {
-            float val2 = texture(volumeTex2, p).r;
-            val2 = max(0.0, (val2 - u_clim2[0]) / (u_clim2[1] - u_clim2[0]));
-            rgbCombo += max(0.0, min(1.0, val2)) * u_color2;
-            total    += val2;
-        }
-        if (volumeCount > 2.0) {
-            float val3 = texture(volumeTex3, p).r;
-            val3 = max(0.0, (val3 - u_clim3[0]) / (u_clim3[1] - u_clim3[0]));
-            rgbCombo += max(0.0, min(1.0, val3)) * u_color3;
-            total    += val3;
-        }
-        if (volumeCount > 3.0) {
-            float val4 = texture(volumeTex4, p).r;
-            val4 = max(0.0, (val4 - u_clim4[0]) / (u_clim4[1] - u_clim4[0]));
-            rgbCombo += max(0.0, min(1.0, val4)) * u_color4;
-            total    += val4;
-        }
-        if (volumeCount > 4.0) {
-            float val5 = texture(volumeTex5, p).r;
-            val5 = max(0.0, (val5 - u_clim5[0]) / (u_clim5[1] - u_clim5[0]));
-            rgbCombo += max(0.0, min(1.0, val5)) * u_color5;
-            total    += val5;
-        }
-        if (volumeCount > 5.0) {
-            float val6 = texture(volumeTex6, p).r;
-            val6 = max(0.0, (val6 - u_clim6[0]) / (u_clim6[1] - u_clim6[0]));
-            rgbCombo += max(0.0, min(1.0, val6)) * u_color6;
-            total    += val6;
-        }
-
-        total = clamp(total, 0.0, 1.0);
-        float sliceAlpha = total * dtScale * dt;
-        vec3 sliceColor  = rgbCombo;
-
-        // DEBUG -- right now if we have 1 channel we add the color multiplied with the alpha.
-        // however we should add the full color and then just add the alpha. also we would have
-        // to weigh the colors by the alpha if adding multiple.
-
-        gl_FragColor.rgb += sliceAlpha * alphaMultiplicator * sliceColor;
-        gl_FragColor.a   += sliceAlpha * alphaMultiplicator;
-
-        alphaMultiplicator *= (1.0 - sliceAlpha);
-
-        if (gl_FragColor.a > 0.99) {
-            break;
-        }
-        p += step;
-    }
-
-    gl_FragDepth = distance(worldSpaceCoords,p)*u_physical_Pixel;
-    gl_FragColor.r = linear_to_srgb(gl_FragColor.r);
-    gl_FragColor.g = linear_to_srgb(gl_FragColor.g);
-    gl_FragColor.b = linear_to_srgb(gl_FragColor.b);
-}
-*/
