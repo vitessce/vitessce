f7eec300a5df5f8de221ab6e7efdf8ee501c92ef
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-05-31 13:27:12 -0400

    Lint

--- packages/view-types/heatmap/src/Heatmap.test.jsx
+++ packages/view-types/heatmap/src/Heatmap.test.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable func-names */
 import React from 'react';
 import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import Heatmap from './Heatmap.js';
 import { expressionMatrix, cellColors } from './Heatmap.test.fixtures.js';
