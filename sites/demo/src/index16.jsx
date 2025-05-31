49de533e5ecbdbef3c9051a1aaf75aa705fd267a
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-05-31 14:08:35 -0400

    Test with v16.8.0 of react

---
+++ sites/demo/src/index16.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { VitessceDemo } from './vitessce-demo.jsx';
+
+const container = document.getElementById('root');
+ReactDOM.render(<VitessceDemo />, container);
