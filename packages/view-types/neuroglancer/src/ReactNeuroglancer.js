f76a906d48bb346f1ec2a1f7a55dbc9c331fe602
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-06-03 17:38:46 -0400

    Lint

--- packages/view-types/neuroglancer/src/ReactNeuroglancer.js
+++ packages/view-types/neuroglancer/src/ReactNeuroglancer.js
@@ -4,7 +4,7 @@ import ReactNeuroglancer from '@janelia-flyem/react-neuroglancer';
 const Component = typeof ReactNeuroglancer.default === 'function' ? ReactNeuroglancer.default : ReactNeuroglancer;
 
 const ReactNeuroglancerWrapper = React.forwardRef((props, ref) => (
-    <Component ref={ref} {...props} />
+  <Component ref={ref} {...props} />
 ));
 
 export default ReactNeuroglancerWrapper;
