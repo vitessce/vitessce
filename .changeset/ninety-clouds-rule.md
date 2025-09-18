2136ec53fc7bbdec0f94e55f8a960be18d6cbc0d
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-09-18 09:56:01 -0400

    Fix coordination type docs

---
+++ .changeset/ninety-clouds-rule.md
@@ -0,0 +1,5 @@
+---
+"@vitessce/constants-internal": patch
+---
+
+Fix coordination type docs and add test to ensure every viewType is mapped to an array of coordinationTypes.
--- packages/constants-internal/src/constant-relationships.test.ts
+++ packages/constants-internal/src/constant-relationships.test.ts
@@ -1,8 +1,9 @@
 import { describe, it, expect } from 'vitest';
-import { DataType } from './constants.js';
+import { DataType, ViewType } from './constants.js';
 import {
   DATA_TYPE_COORDINATION_VALUE_USAGE,
 } from './constant-relationships.js';
+import { COMPONENT_COORDINATION_TYPES } from './coordination.js';
 
 describe('src/app/constant-relationships.js', () => {
   describe('DataType-to-CoordinationType usage mapping', () => {
@@ -10,6 +11,13 @@ describe('src/app/constant-relationships.js', () => {
       const dataTypes = Object.values(DataType).sort();
       const mappedDataTypes = Object.keys(DATA_TYPE_COORDINATION_VALUE_USAGE).sort();
       expect(dataTypes.length).toEqual(mappedDataTypes.length);
+    });
+  });
+  describe('ViewType-to-CoordinationType usage mapping', () => {
+    it('every view type is mapped to an array of coordination types', () => {
+      const viewTypes = Object.values(ViewType).sort();
+      const mappedViewTypes = Object.keys(COMPONENT_COORDINATION_TYPES).sort();
+      expect(viewTypes.length).toEqual(mappedViewTypes.length);
     });
   });
 });
