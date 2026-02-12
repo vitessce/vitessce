33bc36237887b8ef10458fd029eb1677aa5f3f33
Nikolay Akhmetov <nick.akhmetov@gmail.com>
2026-02-12 09:49:08 -0500

    e2e test fixes

--- sites/demo/playwright/react-19/non-3d-views.spec.js
+++ sites/demo/playwright/react-19/non-3d-views.spec.js
@@ -4,7 +4,7 @@ const { test, expect } = require('@playwright/test');
 test('demo homepage renders with React 19', async ({ page }) => {
   await page.goto('http://localhost:3000/?show=all');
   await expect(page).toHaveTitle('Vitessce');
-  await expect(page.getByText('Vitessce')).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Vitessce', exact: true })).toBeVisible();
 });
 
 test('non-3D spatialBeta config renders with React 19', async ({ page }) => {
