a4533bf7e0deac66f44fcb1bdf5b5812c8b8e9d7
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2026-02-23 09:49:19 -0500

    Comment

--- packages/file-types/spatial-zarr/src/neuroglancer-loaders/NgPrecomputedMeshDataLoader.js
+++ packages/file-types/spatial-zarr/src/neuroglancer-loaders/NgPrecomputedMeshDataLoader.js
@@ -8,6 +8,9 @@ export default class PrecomputedMeshDataLoader extends AbstractTwoStepLoader {
         obsIndex: null,
         obsSegmentations: {},
         obsSegmentationsType: 'mesh',
+        // TODO: the "Right way" to do this would be to pass these options via the coordination space,
+        // but that would require defining new coordination types or aligning with existing ones,
+        // and we need to think carefully about that.
         neuroglancerOptions: options,
       },
       url,
