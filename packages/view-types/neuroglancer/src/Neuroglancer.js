5909d0b9fa3b0f0e9884a7ec5895b9709f2f6260
Tabassum Kakar <tkakar@wpi.edu>
2026-03-25 14:19:06 -0400

    Fixed click during rotation

--- packages/view-types/neuroglancer/src/Neuroglancer.js
+++ packages/view-types/neuroglancer/src/Neuroglancer.js
@@ -5,6 +5,8 @@ import { NeuroglancerGlobalStyles } from './styles.js';
 
 const LazyReactNeuroglancer = React.lazy(() => import('./ReactNeuroglancer.js'));
 
+const DRAG_THRESHOLD = 4;
+
 function createWorker() {
   return new ChunkWorker();
 }
@@ -18,6 +20,11 @@ export class NeuroglancerComp extends PureComponent {
     this.prevClickHandler = null;
     this.prevMouseStateChanged = null;
     this.prevHoverHandler = null;
+    this.mouseDownHandler = null;
+    this.mouseMoveHandler = null;
+    this.isDragging = false;
+    this.mouseStartX = 0;
+    this.mouseStartY = 0;
     this.onViewerStateChanged = this.onViewerStateChanged.bind(this);
     this.onRef = this.onRef.bind(this);
     // To avoid closure for onSegmentClick(), to update the selection
@@ -36,16 +43,32 @@ export class NeuroglancerComp extends PureComponent {
       this.prevMouseStateChanged = viewer.mouseState.changed;
       viewer.inputEventBindings.sliceView.set('at:dblclick0', () => {});
       viewer.inputEventBindings.perspectiveView.set('at:dblclick0', () => {});
+
+      // Compare mousedown vs mouseup position to detect drag
+      // Avoids relying on mousemove which can fire spuriously in Neuroglancer
+      this.mouseStartX = 0;
+      this.mouseStartY = 0;
+      this.mouseDownHandler = (e) => {
+        this.mouseStartX = e.clientX;
+        this.mouseStartY = e.clientY;
+      };
+
       this.prevClickHandler = (event) => {
         if (event.button === 0) {
-          // Wait for mouseState to update
-          requestAnimationFrame(() => {
-            const { pickedValue, pickedRenderLayer } = viewer.mouseState;
-            // Only trigger selection when a segment is clicked rather than any click on the view
-            if (pickedValue && pickedValue.low !== undefined && pickedRenderLayer) {
-              this.latestOnSegmentClick?.(pickedValue.low);
-            }
-          });
+          const dx = event.clientX - this.mouseStartX;
+          const dy = event.clientY - this.mouseStartY;
+          // To differentiate between between drag (for rotation) and click
+          const wasDrag = Math.sqrt(dx * dx + dy * dy) > DRAG_THRESHOLD;
+
+          if (!wasDrag) {
+            requestAnimationFrame(() => {
+              const { pickedValue, pickedRenderLayer } = viewer.mouseState;
+              // Only trigger selection when a segment is clicked rather than any click on the view
+              if (pickedValue && pickedValue.low !== undefined && pickedRenderLayer) {
+                this.latestOnSegmentClick?.(pickedValue.low);
+              }
+            });
+          }
         }
       };
       this.prevHoverHandler = () => {
@@ -54,13 +77,17 @@ export class NeuroglancerComp extends PureComponent {
           this.latestOnSelectHoveredCoords?.(pickedSegment?.low);
         }
       };
-      viewer.element.addEventListener('mouseup', this.prevClickHandler);
+
+      viewer.element.addEventListener('mousedown', this.mouseDownHandler, { capture: true });
+      viewer.element.addEventListener('mouseup', this.prevClickHandler, { capture: true });
       viewer.mouseState.changed.add(this.prevHoverHandler);
     } else {
       // Unmount (viewerRef is null)
-      if (this.prevElement && this.prevClickHandler) {
-        this.prevElement.removeEventListener('mouseup', this.prevClickHandler);
+      if (this.prevElement) {
+        this.prevElement.removeEventListener('mouseup', this.prevClickHandler, { capture: true });
+        this.prevElement.removeEventListener('mousedown', this.mouseDownHandler, { capture: true });
         this.prevClickHandler = null;
+        this.mouseDownHandler = null;
       }
       if (this.prevMouseStateChanged && this.prevHoverHandler) {
         this.prevMouseStateChanged.remove(this.prevHoverHandler);
