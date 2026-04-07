c4ea9b76eab3e6455fbf819b76a8b2c17f7718fb
Tabassum Kakar <tkakar@wpi.edu>
2026-04-07 11:21:44 -0400

    Fixed patch to invoke async_comp worker

--- packages/neuroglancer-workers/src/index.js
+++ packages/neuroglancer-workers/src/index.js
@@ -3,4 +3,4 @@
 export { default as ChunkWorker } from 'web-worker:@janelia-flyem/neuroglancer/dist/module/chunk_worker.bundle.js';
 
 // TODO: is the AsyncComputationWorker needed? The react-neuroglancer component seems to render without it.
-// export { default as AsyncComputationWorker } from 'web-worker:@janelia-flyem/neuroglancer/dist/module/async_computation.bundle.js';
+export { default as AsyncComputationWorker } from 'web-worker:@janelia-flyem/neuroglancer/dist/module/async_computation.bundle.js';
