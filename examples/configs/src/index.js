bd7c878a0995d606a8dbed4577564869a1600dac
Tabassum Kakar <tkakar@wpi.edu>
2026-08-11 10:47:04 -0400

    Configs for interscellar and cell-only

--- examples/configs/src/index.js
+++ examples/configs/src/index.js
@@ -96,6 +96,8 @@ import { linkControllerMinimal } from './view-configs/3d-maps/link-controller-mi
 import { neuroglancerTwoLayers } from './view-configs/3d-maps/neuroglancer-two-layers.js';
 import { neuroglancerSorger } from './view-configs/3d-maps/neuroglancer-sorger.js';
 import { neuroglancerInvasive } from './view-configs/3d-maps/neuroglancer-invasive.js';
+import { neuroglancerMisCellOnly } from './view-configs/3d-maps/neuroglancer-mis-cell-only.js';
+import { neuroglancerInterscellar } from './view-configs/3d-maps/neuroglancer-mis-interscellar.js';
 import { neuroglancerMacosko } from './view-configs/3d-maps/neuroglancer-macosko.js';
 // Nature Methods figures
 import { citeSeq } from './view-configs/paper-figures/cite-seq.js';
@@ -219,6 +221,8 @@ export const configs = {
   'neuroglancer-two-layers': neuroglancerTwoLayers,
   'neuroglancer-sorger': neuroglancerSorger,
   'neuroglancer-invasive': neuroglancerInvasive,
+  'neuroglancer-mis-cell-only': neuroglancerMisCellOnly,
+  'neuroglancer-interscellar': neuroglancerInterscellar,
   'neuroglancer-macosko': neuroglancerMacosko,
   // Spatial Accelerated
   's-a-lsp1': saLsp1,
